89bc6affb099aea09717c2290991f8e45e562312
Gunnar Eyal Wolf Iszaevich <gwolf@gwolf.org>
2005-06-24 18:46:58 +0000

    Bye bye, _logs tables and rules!

--- trunk/sql/printeddocs.sql
+++ trunk/sql/printeddocs.sql
@@ -17,38 +17,3 @@ CREATE TABLE printeddocs( -- No. 1
     PRIMARY KEY (id),
     UNIQUE (uid, ipaddress, did, timestamp)
 );
-
-
-----------------
--- Log tables --
-----------------
-
-CREATE TABLE printeddocs_logs ( -- No. 2
-    id SERIAL,
-    uid int4 NOT NULL,
-    did int4 NOT NULL,
-    ipaddress inet NOT NULL,
-    timestamp timestamp NOT NULL,
-    dbuser text DEFAULT CURRENT_USER,
-    dbtimestamp timestamp DEFAULT CURRENT_TIMESTAMP,
-    dbmodtype char(1)
-);
-
------------
--- Rules --
------------
-
-CREATE RULE printeddocs_update AS	-- UPDATE rule
-ON UPDATE TO printeddocs
-DO
-INSERT INTO printeddocs_logs ( id, uid, did, ipaddress, timestamp, dbmodtype )
-		       VALUES( old.id, old.uid, old.did, old.ipaddress, 
-			       old.timestamp, 'U' );
-
-CREATE RULE printeddocs_delete AS	-- DELETE rule
-ON DELETE TO printeddocs
-DO
-INSERT INTO printeddocs_logs ( id, uid, did, ipaddress, timestamp, dbmodtype )
-		       VALUES( old.id, old.uid, old.did, old.ipaddress, 
-			       old.timestamp, 'D' );
-
