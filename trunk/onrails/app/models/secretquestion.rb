ede8e82e1d6bc322f708f0830f90511f6e277b2b
Developer at IFUNAM <dev@fisica.unam.mx>
2005-10-28 22:24:26 +0000

    New methods added: per_pages, order_by create_msg, destroy_msg, update_msg

--- trunk/onrails/app/models/secretquestion.rb
+++ trunk/onrails/app/models/secretquestion.rb
@@ -1,5 +1,24 @@
 class Secretquestion < ActiveRecord::Base
   validates_length_of :name, :message => "La pregunta debe contener de 1 a 80 caracteres", :within => 1..80
   validates_uniqueness_of :name, :message => "La pregunta ya existe"
-  
+
+  def self.per_pages
+    10
+  end
+
+  def self.order_by
+    'name'
+  end
+
+  def self.create_msg
+    'La nueva pregunta ha sido agregada'
+  end
+
+  def self.destroy_msg
+    'La pregunta ha sido borrada'
+  end
+
+  def self.update_msg
+    'La pregunta ha sido actualizada'
+  end
 end
