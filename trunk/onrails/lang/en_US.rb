dee925df90c98f2c51e6c1d2acbd6c00ca7e10aa
Developer at IFUNAM <dev@fisica.unam.mx>
2005-10-30 10:54:51 +0000

    Integrando el soporte de varios lenguajes

---
+++ trunk/onrails/lang/en_US.rb
@@ -0,0 +1,6 @@
+Localization.define('en_US') do |l|
+    l.store 'Usuario', 'User'
+    l.store 'Ayuda', 'Help'
+    l.store 'Salir', 'Logout'
+    l.store 'Informar de un error', 'Sendbug'
+end
---
+++ trunk/onrails/lang/es_MX.rb
@@ -0,0 +1,6 @@
+Localization.define('es_MX') do |l|
+    l.store 'Usuario', 'Usuario'
+    l.store 'Ayuda', 'Ayuda'
+    l.store 'Salir', 'Salir'
+    l.store 'Informar de un error', 'Informar de un error'
+end
