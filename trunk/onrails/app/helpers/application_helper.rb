29b49ebdb2bac0aad7ed5c0528e37a5ce34e36fe
Developer at IFUNAM <dev@fisica.unam.mx>
2005-11-01 09:09:48 +0000

    Agregando helpers para *limpiar* las vistas

--- trunk/onrails/app/helpers/application_helper.rb
+++ trunk/onrails/app/helpers/application_helper.rb
@@ -1,3 +1,43 @@
-# Methods added to this helper will be available to all templates in the application.
+ # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # used
+  def copyright
+    @copyright || 'Universidad Nacional Aut&oacute;noma de M&eacute;xico'
+  end
+
+  def charset
+    @charset || 'iso-8859-1'
+  end
+
+  def icon
+    @icon || '/images/favicon.ico'
+  end
+
+  def style
+    @style || 'form'
+  end
+
+  def help
+    @help || "Ayuda"
+  end
+
+  def logout
+    @logout || 'Salir'
+  end
+
+  def logout_link
+    @logout_link || '/logout/'
+  end
+
+  def sendbug
+    @sendbug || 'Informar de un error'
+  end
+  
+  def sendbug_link
+    @sendbug_link || '/sendbug/'
+  end
+
+  def user_label
+    @user_label || 'Usuario'
+  end
 end
