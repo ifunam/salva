fda773c212ba1e83dae4d677c96e06123f6440e2
Developer at IFUNAM <dev@fisica.unam.mx>
2005-11-01 09:13:44 +0000

    quitando localization

--- trunk/onrails/app/controllers/application.rb
+++ trunk/onrails/app/controllers/application.rb
@@ -2,6 +2,4 @@
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   helper :application
-
-  Localization.lang = 'es_MX'
 end
