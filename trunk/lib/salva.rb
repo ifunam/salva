8ff615414d45d7a38f133c040d0eca76f8c601e8
Developer at IFUNAM <dev@fisica.unam.mx>
2005-11-19 22:37:27 +0000

    Quitamos la *cadenizacion*

--- trunk/lib/salva.rb
+++ trunk/lib/salva.rb
@@ -3,27 +3,12 @@ module Salva
   SALVA = {}
   
   def get(symbol, section)
-    if section == 'strings'
-      SALVA[section][symbol] || 'Check your file lang/salva.yml: '+symbol.to_s+' is undefined.'
-    elsif section == 'config' 
+    if section == 'config'
       SALVA[section][symbol] || 'Check your file config/salva.yml: '+symbol.to_s+' is undefined.'
     else
       'The section:'+section.to_s+' is undefined.'
     end
   end   
-  
-  def self.load_strings
-    filename =  File.join(RAILS_ROOT, 'lang', 'es.yml')
-    strings = YAML::parse(File.open(filename))
-    tree = strings.transform
-    
-    symbol = Hash.new
-    # convert string keys to symbols
-    tree.each do |key,legend|
-      symbol[key.to_sym] = legend 
-    end
-    SALVA['strings'] = symbol
-  end
 
   def self.load_config_settings
     filename =  File.join(RAILS_ROOT, 'config', 'salva.yml')
