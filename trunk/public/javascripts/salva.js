e6caa8f149f255d04e31d19491123df7325478c9
Developer at IFUNAM <dev@fisica.unam.mx>
2005-11-24 07:59:02 +0000

    JavaScript inicial para el pinche salva

---
+++ trunk/public/javascripts/salva.js
@@ -0,0 +1,11 @@
+function handleKeyPress(keyEvent)
+{
+ // Below handler Navigator/IE differences
+ var keyEvent = (keyEvent ? keyEvent : event);
+ var keyPressed = (keyEvent.which ? keyEvent.which : keyEvent.keyCode);
+
+ if (keyPressed == 9 || keyPressed == 13) // 9 = TAB key and 13 = ENTER key
+ {
+  return 1;
+ } 
+}
