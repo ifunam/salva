ce098bcd953ac580a91451cbf0f02725b94334c0
Developer at IFUNAM <dev@fisica.unam.mx>
2005-11-29 18:20:20 +0000

    Correcciones ortográficas

--- branches/salva_dev/db/catalogs/sectors.sql
+++ branches/salva_dev/db/catalogs/sectors.sql
@@ -1,7 +1,7 @@
-INSERT INTO sectors (name) VALUES ('Investigación cientifica');
-INSERT INTO sectors (name) VALUES ('Investigación humanistica');
+INSERT INTO sectors (name) VALUES ('Investigación científica');
+INSERT INTO sectors (name) VALUES ('Investigación humanística');
 INSERT INTO sectors (name) VALUES ('Docencia');
-INSERT INTO sectors (name) VALUES ('Difusión extensión');
+INSERT INTO sectors (name) VALUES ('Difusión y extensión');
 INSERT INTO sectors (name) VALUES ('Servicios');
 INSERT INTO sectors (name) VALUES ('Salud');
 INSERT INTO sectors (name) VALUES ('Energéticos');
