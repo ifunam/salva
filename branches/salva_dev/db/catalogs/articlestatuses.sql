ba0580f7681d467bb9e3dd72467d7f83021f7cf9
Developer at IFUNAM <dev@fisica.unam.mx>
2005-11-29 18:21:24 +0000

    Agregamos los estados de los articulos y las categorías de las publicaciones

---
+++ branches/salva_dev/db/catalogs/articlestatuses.sql
@@ -0,0 +1,5 @@
+INSERT INTO articlestatuses (name) VALUES ('Aceptado');
+INSERT INTO articlestatuses (name) VALUES ('Enviado');
+INSERT INTO articlestatuses (name) VALUES ('En prensa');
+INSERT INTO articlestatuses (name) VALUES ('En proceso');
+INSERT INTO articlestatuses (name) VALUES ('Publicado');
---
+++ branches/salva_dev/db/catalogs/publicationcategories.sql
@@ -0,0 +1,4 @@
+INSERT INTO publicationcategories (name) VALUES ('Ciencias');
+INSERT INTO publicationcategories (name) VALUES ('Ciencias naturales');
+INSERT INTO publicationcategories (name) VALUES ('Ciencias sociales, artes y humanidades');
+
