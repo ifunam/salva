8b74eee9cdd38e54fe237ec935d347defca802c2
Developer at IFUNAM <dev@fisica.unam.mx>
2005-12-06 22:57:44 +0000

    actualizamos descripciones de las columnas y clarificamos los metodos

--- branches/salva_dev/app/helpers/salva_helper.rb
+++ branches/salva_dev/app/helpers/salva_helper.rb
@@ -1,17 +1,23 @@
 module SalvaHelper 
-
-  def get_cfg(key)
-    labels = { 
+  
+  def get_cfg(name)
+    cfg = { 
       'institution_name' => 'Instituto de Física - UNAM',
       'institution_url' => 'http://www.fisica.unam.mx',
       'institution_administrative_key' => '-clave presupuestal-'
     }
-
-    labels[key] if labels[key]
+    cfg[name] ? cfg[name] : name
   end
   
-  def salva_column(key)
-    column_name = { 
+  def salva_column(name)
+    column = { 
+      # users
+      'login' => 'Usuario',
+      'passwd' => 'Contraseña',
+      'userstatus_id' => 'Estado del usuario',
+      'email' => 'Correo electrónico',
+      'pkcs7' => 'Certificado para firma digital',
+      # personals
       'firstname' => 'Nombre',
       'lastname1' => 'Apellido paterno',
       'lastname2' => 'Apellido materno',
@@ -22,10 +28,20 @@ module SalvaHelper
       'birth_state_id' => 'Estado',
       'maritalstatu_id' => 'Estado civil',
       'photo' => 'Fotografía',
-      'other' => 'Otra información o comentarios adicionales'
+      'other' => 'Otra información o comentarios adicionales',
+      # addresses
+      'addresstype_id' => 'Tipo de dirección',
+      'country_id' => 'País',
+      'postaddress' => 'Dirección',
+      'state_id' => 'Estado',
+      'city' => 'Ciudad',
+      'zipcode' => 'Código postal',
+      'phone' => 'Teléfono(s)',
+      'fax' => 'Fax',
+      'movil' => 'Celular o radio',
     }
     
-    column_name[key] if column_name[key]
+    column[name] ? column[name] : name
   end
   
 end
