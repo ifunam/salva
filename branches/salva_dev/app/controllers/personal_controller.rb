83143b1e8f89ce6bcdb2ee4e85482d80aaac84c1
Developer at IFUNAM <dev@fisica.unam.mx>
2005-12-07 06:27:05 +0000

    varios cambios;

--- branches/salva_dev/app/controllers/personal_controller.rb
+++ branches/salva_dev/app/controllers/personal_controller.rb
@@ -11,10 +11,26 @@ class PersonalController < ApplicationController
 
   def show
     @edit = Personal.find(@session[:user])
-#    render :action => 'show'
+  end
+  def edit 
+    @edit = Personal.find(@session[:user])
   end
   def new
     @edit = Personal.new
-#    render :action => 'new'
+  end
+
+  def create 
+    @edit = Personal.new(params[:edit])
+    @edit.id = @session[:user] 
+
+    if @edit.save
+
+      flash[:notice] = @create_msg
+      redirect_to :action => 'show'
+    else
+      logger.info "*** Algo esta mal <<wey>>, checalo! ***"
+      logger.info @edit.errors.full_messages
+      redirect_to :action => 'edit'
+    end
   end
 end
