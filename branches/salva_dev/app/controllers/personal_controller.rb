ec63a1f9f41880958b5e3d6ac2380e2d28ac5233
Developer at IFUNAM <dev@fisica.unam.mx>
2005-12-07 22:11:56 +0000

    Primeras pruebas con ajax upload progress bar

--- branches/salva_dev/app/controllers/personal_controller.rb
+++ branches/salva_dev/app/controllers/personal_controller.rb
@@ -1,7 +1,9 @@
 class PersonalController < ApplicationController
+  upload_status_for :create
+  upload_status_for :status => :custom_status
 
   def index
-    @edit = Personal.find(@session[:user])
+    @edit = Personal.find(:first, @session[:user])
     if @edit then
      redirect_to :action => 'show'
     else
@@ -10,27 +12,35 @@ class PersonalController < ApplicationController
   end
 
   def show
-    @edit = Personal.find(@session[:user])
+    @edit = Personal.find(:first, @session[:user])
   end
+
   def edit 
-    @edit = Personal.find(@session[:user])
+    @edit = Personal.find(:first, @session[:user])
   end
+
   def new
     @edit = Personal.new
   end
 
   def create 
-    @edit = Personal.new(params[:edit])
-    @edit.id = @session[:user] 
-
-    if @edit.save
-
-      flash[:notice] = @create_msg
-      redirect_to :action => 'show'
-    else
-      logger.info "*** Algo esta mal <<wey>>, checalo! ***"
-      logger.info @edit.errors.full_messages
-      redirect_to :action => 'edit'
-    end
+#    @edit = Personal.new(params[:edit])
+#    @edit.id = @session[:user] 
+    upload_progress.message = "Enviando su fotografía..."
+#    if @edit.save
+#      flash[:notice] = 'Sus datos personales han sido guardados'
+#      redirect_to :action => 'show'
+#    else
+#      flash[:notice] = 'Wey, hay errores al guardar esta información'
+#      logger.info @edit.errors.full_messages
+#      redirect_to :action => 'index'
+#    end
   end
+
+  def custom_status
+#    render :inline => "<%= upload_progress.completed_percent rescue 0 %> % complete", :layout => false
+    render :inline => '<%= upload_progress_status %> <div>Updated at <%= Time.now %></div>', :layout => false
+  end
+
+
 end
--- branches/salva_dev/app/models/personal.rb
+++ branches/salva_dev/app/models/personal.rb
@@ -1,4 +1,18 @@
 class Personal < ActiveRecord::Base
   set_primary_key "user_id"
+ # validates_format_of :photo, :with => /^image/, :message => "Usted solamente debe subir imágenes"
 
+ # before_create :prepare_new_record
+  
+ # def prepare_new_record
+    #  def photo=(photo) 
+    #self.name = base_part_of(photo_field.original_filename)
+    #self.content_type = photo_field.content_type.chomp
+#    self.photo = 
+  # end
+
+# def base_part_of(file_name)
+#  name = File.basename(file_name)
+#  name.gsub(/[^\w._-]/, ' ' )
+# end
 end
