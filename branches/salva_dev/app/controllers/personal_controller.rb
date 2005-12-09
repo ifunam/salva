dfdf25af5fc023f7306ea9faed37e21c452bdc85
Developer at IFUNAM <dev@fisica.unam.mx>
2005-12-09 04:35:50 +0000

    Traducciones, cambios varios en el controlador de informacion personal

--- branches/salva_dev/app/controllers/personal_controller.rb
+++ branches/salva_dev/app/controllers/personal_controller.rb
@@ -1,3 +1,4 @@
+require 'RMagick'
 class PersonalController < ApplicationController
   upload_status_for :create
 
@@ -13,27 +14,30 @@ class PersonalController < ApplicationController
   def show
      @edit = Personal.find(:first, @session[:user])
   end
-  
-  def photo
-    @edit = Personal.find(:first, @session[:user])
-    send_data (@edit.photo, :filename => @edit.photo_filename, :type => "image/"+@edit.photo_content_type.to_s, :disposition => "inline")
+
+  def new
+    @edit = Personal.new
   end
   
   def edit 
     @edit = Personal.find(:first, @session[:user])
-  end
+  end  
 
-  def new
-    @edit = Personal.new
+  def photo
+    @edit = Personal.find(:first, @session[:user])
+    send_data (@edit.photo, :filename => @edit.photo_filename, :type => "image/"+@edit.photo_content_type.to_s, :disposition => "inline")
   end
 
   def create 
     @edit = Personal.new(params[:edit])
     @edit.id = @session[:user] 
-    @edit.photo_filename = base_part_of(@params[:edit]['photo'].original_filename)
-    @edit.photo_content_type = base_part_of(@params[:edit]['photo'].content_type.chomp)
-    @edit.photo = @params[:edit]['photo'].read
-    upload_progress.message = "Enviando su fotografía..."
+    if @edit.photo
+      @edit.photo_filename = base_part_of(params[:edit]['photo'].original_filename)
+      @edit.photo_content_type = base_part_of(params[:edit]['photo'].content_type.chomp)
+      @edit.photo = params[:edit]['photo'].read
+      upload_progress.message = "Enviando su fotografía..."
+    end
+
     if @edit.save
       flash[:notice] = 'Sus datos personales han sido guardados'
       redirect_to :action => 'show'
@@ -46,10 +50,12 @@ class PersonalController < ApplicationController
 
   def update
     @edit = Personal.find(@session[:user])
-    @edit.photo_filename = base_part_of(@params[:edit]['photo'].original_filename)
-    @edit.photo_content_type = base_part_of(@params[:edit]['photo'].content_type.chomp)
-    @edit.photo = @params[:edit]['photo'].read
-    upload_progress.message = "Enviando su fotografía..."
+    if @edit.photo
+      @edit.photo_filename = base_part_of(params[:edit]['photo'].original_filename)
+      @edit.photo_content_type = base_part_of(params[:edit]['photo'].content_type.chomp)
+      @edit.photo = params[:edit]['photo'].read
+      upload_progress.message = "Actualizando su fotografía..."
+    end
     if @edit.update_attributes(params[:edit])
       flash[:notice] = 'Sus datos personales han sido actualizados'
       redirect_to :action => 'show'
