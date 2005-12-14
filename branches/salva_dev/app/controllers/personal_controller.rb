52808e3096b47e1fb02cc3ee8fbec820f8893a0f
Developer at IFUNAM <dev@fisica.unam.mx>
2005-12-14 02:31:29 +0000

    La barra de progreso funciona con create y update

--- branches/salva_dev/app/controllers/personal_controller.rb
+++ branches/salva_dev/app/controllers/personal_controller.rb
@@ -1,7 +1,7 @@
 require 'RMagick'
 class PersonalController < ApplicationController
   include Magick
-  upload_status_for :create
+  upload_status_for :create, :update
 
   def index
     get_personal
@@ -31,53 +31,59 @@ class PersonalController < ApplicationController
     send_data (@edit.photo, :filename => @edit.photo_filename, :type => "image/"+@edit.photo_content_type.to_s, :disposition => "inline")
   end
   
+  
   def create 
-    get_personal
-    @edit.id = @session[:user] 
-    @edit.moduser_id = @session[:user] if @session[:user]
-
-    if @edit.photo
-      @edit.photo_filename = base_part_of(params[:edit]['photo'].original_filename)
-      @edit.photo_content_type = base_part_of(params[:edit]['photo'].content_type.chomp)
-      @edit.photo = transform_photo(params[:edit]['photo'])
-      @edit.photo_content_type = 'png'
-    end
-    
-    if @edit.save
-      flash[:notice] = 'Sus datos personales han sido guardados'
-      redirect_to :action => 'show'
-    else
-      flash[:notice] = 'Wey, hay errores al guardar esta información'
-      logger.info @edit.errors.full_messages
-      redirect_to :action => 'index'
-    end
+    @edit = Personal.new(params[:edit])
+    save_personal('create')
   end
-
+  
   def update
-    get_personal
+    @edit = Personal.new(params[:edit])
+    save_personal('update')
+  end
+  
+  protected
+  
+  def save_personal(transaction)
     @edit.id = @session[:user] 
     @edit.moduser_id = @session[:user] if @session[:user]
+    
     if @edit.photo
       @edit.photo_filename = base_part_of(params[:edit]['photo'].original_filename)
       @edit.photo_content_type = base_part_of(params[:edit]['photo'].content_type.chomp)
       @edit.photo = transform_photo(params[:edit]['photo'])
       @edit.photo_content_type = 'png'
     end
-    if @edit.save
-      flash[:notice] = 'Sus datos personales han sido actualizados'
-      redirect_to :action => 'show'
+
+    if ( transaction == 'create') then
+      if @edit.save
+        msg ='Sus datos personales han sido guardados'
+      else
+        errmsg = 'Hay errores al guardar esta información'
+      end
     else
-      flash[:notice] = 'Wey, hay errores al guardar esta información'
+      if @edit.update
+        msg = 'Sus datos personales han sido actualizados'
+      else
+        errmsg = 'Hay errores al actualizar esta información'
+      end
+    end  
+
+    if errmsg then
+      flash[:notice] = 'Hay errores al actualizar esta información'
       logger.info @edit.errors.full_messages
       redirect_to :action => 'index'
+    else
+      flash[:notice] = msg 
+      redirect_to :action => 'show'
     end
   end
-
+  
   def base_part_of(file_name)
     name = File.basename(file_name)
     name.gsub(/[^\w._-]/, ' ' )
   end
-
+  
   def transform_photo(photo)
     photo = Magick::Image.from_blob(photo.read).first
     maxwidth = 80
@@ -96,7 +102,4 @@ class PersonalController < ApplicationController
     @edit = Personal.find(:first, :conditions => [ "user_id=?", @session[:user]])
   end
 
-#  def upload_status
-#    render :inline => "<%= upload_progress.completed_percent rescue 0 %> % complete", :layout => false
-#  end
 end
