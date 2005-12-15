7e0c7fa76398556272495017ef6eca305eda6360
Developer at IFUNAM <dev@fisica.unam.mx>
2005-12-15 06:17:05 +0000

    Adios

--- branches/salva_dev/db/catalogs/maritalstatus.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO maritalstatus (name) VALUES ('Soltero(a)');
-INSERT INTO maritalstatus (name) VALUES ('Casado(a)');
-INSERT INTO maritalstatus (name) VALUES ('Divorciado(a)');
-INSERT INTO maritalstatus (name) VALUES ('Unión libre');
-INSERT INTO maritalstatus (name) VALUES ('Viudo(a)');
