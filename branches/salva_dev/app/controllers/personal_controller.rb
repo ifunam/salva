e0087932c7bb49bdd4642bc12b619883afcf05ab
Developer at IFUNAM <dev@fisica.unam.mx>
2005-12-15 03:14:30 +0000

    Mas detalles con la pinche foto y validaciones

--- branches/salva_dev/app/controllers/personal_controller.rb
+++ branches/salva_dev/app/controllers/personal_controller.rb
@@ -26,57 +26,52 @@ class PersonalController < ApplicationController
 
   def photo
     get_personal
-    @headers['Pragma'] = 'no-cache'
-    @headers['Cache-Control'] = 'no-cache, must-revalidate'
-    send_data (@edit.photo, :filename => @edit.photo_filename, :type => "image/"+@edit.photo_content_type.to_s, :disposition => "inline")
+    if  @edit.photo then
+      @headers['Pragma'] = 'no-cache'
+      @headers['Cache-Control'] = 'no-cache, must-revalidate'
+      send_data (@edit.photo, :filename => @edit.photo_filename, :type => "image/"+@edit.photo_content_type.to_s, :disposition => "inline")
+    else
+      redirect_to "/images/comodin.png"
+    end
   end
-  
   
   def create 
     @edit = Personal.new(params[:edit])
-    save_personal('create')
+    @edit.id = @session[:user] 
+    @edit.moduser_id = @session[:user] if @session[:user]
+    save_photo if @edit.photo.size > 0
+
+    if @edit.save 
+      flash[:notice] = 'Sus datos personales han sido guardados'
+      render :action => 'show'
+    else
+      flash[:notice] = 'Hay errores al guardar esta información'
+      render :action => 'new'
+    end
   end
   
   def update
     @edit = Personal.new(params[:edit])
-    save_personal('update')
+    @edit.id = @session[:user] 
+    @edit.moduser_id = @session[:user] if @session[:user]
+    save_photo if @edit.photo.size > 0
+
+    if @edit.update 
+      flash[:notice] = 'Sus datos personales han sido actualizados'
+      render :action => 'show'
+    else
+      flash[:notice] = 'Hay errores al actualizar esta información'
+      render :action => 'edit'
+    end
   end
   
   protected
   
-  def save_personal(transaction)
-    @edit.id = @session[:user] 
-    @edit.moduser_id = @session[:user] if @session[:user]
-    
-    if @edit.photo
-      @edit.photo_filename = base_part_of(params[:edit]['photo'].original_filename)
-      @edit.photo_content_type = base_part_of(params[:edit]['photo'].content_type.chomp)
-      @edit.photo = transform_photo(params[:edit]['photo'])
-      @edit.photo_content_type = 'png'
-    end
-
-    if ( transaction == 'create') then
-      if @edit.save
-        msg ='Sus datos personales han sido guardados'
-      else
-        errmsg = 'Hay errores al guardar esta información'
-      end
-    else
-      if @edit.update
-        msg = 'Sus datos personales han sido actualizados'
-      else
-        errmsg = 'Hay errores al actualizar esta información'
-      end
-    end  
-
-    if errmsg then
-      flash[:notice] = 'Hay errores al actualizar esta información'
-      logger.info @edit.errors.full_messages
-      redirect_to :action => 'index'
-    else
-      flash[:notice] = msg 
-      redirect_to :action => 'show'
-    end
+  def save_photo
+    @edit.photo_filename = base_part_of(params[:edit]['photo'].original_filename)
+    @edit.photo_content_type = base_part_of(params[:edit]['photo'].content_type.chomp)
+    @edit.photo = transform_photo(params[:edit]['photo'])
+    @edit.photo_content_type = 'png'
   end
   
   def base_part_of(file_name)
--- branches/salva_dev/app/models/personal.rb
+++ branches/salva_dev/app/models/personal.rb
@@ -1,5 +1,11 @@
 class Personal < ActiveRecord::Base
   set_primary_key "user_id"
-#  validates_format_of  :photo_content_type,  :with => /^image/, :message => "Bestia peluda, solamente debe subir imágenes"
- 
+  
+  validates_presence_of :firstname,  :lastname1, :sex,
+  :dateofbirth, :birth_country_id, :birth_state_id, :birthcity, 
+  :maritalstatu_id, :message => 'wey, estos campos son obligatorios'
+  validates_numericality_of :birth_country_id, :birth_state_id, 
+  :maritalstatu_id
+  validates_inclusion_of :sex, :in=>%w( true false ), :message=>"wow! Entonces qué pinche genero tienes, pinche hermafrodita? Es lógica difusa, valen madres el 0 y el 1 ..."
+  validates_format_of  :photo_content_type,  :with => /^image/
 end
