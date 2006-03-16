bf175672e65234a32079cdbfad169c0ea6382b01
Developer at IFUNAM <dev@fisica.unam.mx>
2006-03-16 00:09:53 +0000

    Cambios menores

--- trunk/app/helpers/application_helper.rb
+++ trunk/app/helpers/application_helper.rb
@@ -16,27 +16,30 @@ module ApplicationHelper
   def sendbug_link
     '/sendbug/'
   end
-  
-  def action_link(opts={}, html_opts ={})
+
+  def action_link(action, id, alt, question=nil)
     image ='/images/invisible_16x16.png'
-    onmouseover = "return overlib('#{opts[:alt]}', WIDTH, 20, HEIGHT, 20, RIGHT, BELOW, SNAPX, 2, SNAPY, 2)"
-    onmouseout = "return nd()" 
-    link_options = { :action => opts[:action], :id => opts[:id] }
-    html_options = { :class => opts[:action], :onmouseover => onmouseover, :onmouseout => onmouseout }
-    html_options[:confirm] = html_opts[:question] if html_opts[:question] != nil
-    link_to(image_tag(image, :size => '16x16', :border => 0, :alt => opts[:alt]), link_options, html_options)
+    miceover = "return overlib('#{alt}', WIDTH, 20, HEIGHT, 20, RIGHT, BELOW, "
+    miceover += "SNAPX, 2, SNAPY, 2)"
+    miceout = "return nd()" 
+    link_options = { :action => action, :id => id}
+    html_options = { :class => action, :onmouseover => miceover, :onmouseout => miceout }
+    html_options[:confirm] = question if question != nil
+    
+    link_to(image_tag(image, :size => '16x16', :border => 0, :alt => alt]), 
+            link_options, html_options )
   end
   
-  def show_link(options={})
-    action_link({:action => 'show', :id => options[:id], :alt => 'Mostrar'})
+  def show_link(id)
+    action_link('show', id, 'Mostrar')
   end
   
-  def edit_link(options={})
-    action_link({:action => 'edit', :id => options[:id], :alt => 'Modificar'})
+  def edit_link(id)
+    action_link('edit', id, 'Modificar')
   end
   
-  def purge_link(options={})
-    action_link({:action => 'purge', :id => options[:id], :alt => 'Borrar'}, {:question => options[:question]})
+  def purge_link(id,question)
+    action_link('purge', id, 'Borrar', question)
   end
   
   def check_box(object, id, checked=false, prefix='item_id')
@@ -73,20 +76,25 @@ module ApplicationHelper
                 :order => [:day, :month, :year])
   end
   
-  def remote_function_tag(div, with, prefix=nil)
-    prefix ?  with = "'prefix=#{prefix}&#{with}" :  with = "'#{with}" 
-    remote_function(:update => div, :with => with, :url => {:action => :upgrade_select_dest},  
-                    :loading => "Toggle.display('#{div}_note')",
-                    :success => "new Effect.BlindUp('#{div}_note', {duration: 0.4}); return false;")
-  end
   
-  def select_simple(object, model, model_id, ref_model=nil, condition_id=nil, rparams=nil)
-    params = [ object, model_id,  model.find(:all, :order => 'name ASC').collect {|p| [ p.name, p.id ]}, {:prompt => '-- Seleccionar --'} ]
-    if ref_model and condition_id then
-      params = [ object, model_id, model.find(:all, :order => 'name ASC', :conditions => [ ref_model + ' = ?', condition_id ]).collect {|p| [ p.name, p.id ]}, {:prompt => '-- Seleccionar --'} ]
-    end
+  def select_simple(object, model, model_id, rparams=nil)
+    params = [ object, model_id,  
+               model.find(:all, :order => 'name ASC').collect {|p| [ p.name, p.id ]}, 
+               {:prompt => '-- Seleccionar --'} ]
+   
     params.push({:onchange => remote_function_tag(*rparams.to_a)}) if rparams.length > 0
-    select(*params.to_a)
+    select(*params.to_a)    
+  end
+
+  def select_condition(object, model, model_id, condition_id, ref_model, rparams=nil)
+    condition = ref_model + ' = ?'
+    params = [ object, model_id, 
+               model.find(:all, :order => 'name ASC', 
+                          :conditions => [ condition,condition_id ]
+                          ).collect {|p| [ p.name, p.id ]}, 
+               {:prompt => '-- Seleccionar --'} ]
+    params.push({:onchange => remote_function_tag(*rparams.to_a)}) if rparams.length > 0
+    select(*params.to_a)    
   end
   
   def select_selected(object, model, model_id, id, ref_model=nil, condition_id=nil, rparams=nil) 
@@ -135,6 +143,13 @@ module ApplicationHelper
     ckbox_group += "</ul>\n"
     ckbox_group
   end
+
+  def remote_function_tag(div, with, prefix=nil)
+    prefix ?  with = "'prefix=#{prefix}&#{with}" :  with = "'#{with}" 
+    remote_function(:update => div, :with => with, :url => {:action => :upgrade_select_dest},  
+                    :loading => "Toggle.display('#{div}_note')",
+                    :success => "new Effect.BlindUp('#{div}_note', {duration: 0.4}); return false;")
+  end
   
   def remote_upgrade_select(model, name, question='¿Desea agregar este elemento?', options={}) 
     options = options.stringify_keys
@@ -149,7 +164,7 @@ module ApplicationHelper
       "if (handleKeyPress(event) && checkString(document.forms[0].#{field}_#{name}.value)) { var agree=confirm('#{question}'); if (agree) {new Ajax.Updater('#{div2upgrade}', '/wizard/upgrade_select?class=#{model}&prefix=#{prefix}&name='+document.forms[0].#{field}_#{name}.value, {asynchronous:true, evalScripts:true}); return false;} }"
     end
   end
-
+  
   def quickpost(partial)
     link_to_function(image_tag('add.gif', {:border=>0, :valign => 'middle', :alt=> 'Agregar'}), toggle_effect(partial, 'Effect.BlindUp', "duration:0.4", "Effect.BlindDown", "duration:0.4"))    
   end
@@ -157,7 +172,7 @@ module ApplicationHelper
   def toggle_effect(domid, true_effect, true_opts, false_effect, false_opts)
     "$('#{domid}').style.display == 'none' ? new #{false_effect}('#{domid}', {#{false_opts}}) : new #{true_effect}('#{domid}', {#{true_opts}}); return false;"
   end
-
+  
   # This can be moved into application_helper.rb
   def loading_indicator_tag(scope,id)
     "<img src=\"/images/indicator.gif\" style=\"display: none;\" id=\"#{scope}-#{id}-loading-indicator\" alt=\"loading indicator\" class=\"loading-indicator\" />"
