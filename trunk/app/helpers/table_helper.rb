5942d549d8e86601225ffe76ef9d8b4a93f19ab0
Developer at IFUNAM <dev@fisica.unam.mx>
2006-03-29 16:39:01 +0000

    Metodos table_show y table_list mas simples, agregamos modificaciones a las vistas de address y salva_scaffold con el objetivo de presentar de manera ordenada las opciones Guardad y Cancelar de los formularios. Ademas, se agrego la leyenda que permite al usuario conocer el significado del *asterisco* :P

--- trunk/app/helpers/table_helper.rb
+++ trunk/app/helpers/table_helper.rb
@@ -1,50 +1,48 @@
 module TableHelper
   def table_list(collection, options = {} )
-    options = options.stringify_keys
-    header = options['header']
-    columns = options['columns']
+    header = options[:header]
+    columns = options[:columns]
     
     list = []
-    collection.each { |item|
+    collection.each { |row|
       cell = []
       if columns.is_a?Array then
         columns.each { |attr| 
-          if item.send(attr) != nil then
+          if row.send(attr) != nil then
             if is_id?(attr) then
               (model, field) = set_belongs_to(attr)
-              cell << item.send(model).send(field) if item.send(model) != nil
+              cell << row.send(model).send(field) if row.send(model) != nil
             else
-              cell << item.send(attr) if item.send(attr) != nil
+              cell << row.send(attr) if row.send(attr) != nil
             end
           end
         } 
       else
-        item.attributes().each { |key, value| cell << value if key != 'id' and value != nil } 
+        row.attributes().each { |key, value| 
+          cell << value if key != 'id' and value != nil 
+        } 
       end
       cell_content = cell.join(', ').to_s+'.'
-      list.push({'id' => item.id, 'cell_content' => cell_content })
+      list.push({'id' => row.id, 'cell_content' => cell_content })
     }
-    render(:partial => '/salva/list', :locals => { :header => header, :list => list })
+    render(:partial => '/salva/list', 
+           :locals => { :header => header, :list => list })
   end
   
   def table_show(collection, options = {})
-    options = options.stringify_keys
-    belongs_to = options['belongs_to']
-    
     default_hidden = %w(id dbtime moduser_id user_id created_on updated_on) 
-    hidden = options['hidden']    
+    hidden = options[:hidden]    
     hidden = [ hidden ] unless hidden.is_a?Array
-    
     hidden.each { |attr| default_hidden << attr } if hidden != nil
     
     body = []
     collection.each { |column| 
       attr = column.name
-      
       if !default_hidden.include?(attr) then
         if is_id?(attr) then
           (model, field) = set_belongs_to(attr)
-          body << [ attr, @edit.send(model).send(field) ] if @edit.send(model) != nil 
+          body << [ attr, @edit.send(model).send(field) ] unless
+            @edit.send(model) == nil 
         else
           case attr 
           when 'sex' 
@@ -55,7 +53,6 @@ module TableHelper
         end
       end
     }
-    
     render(:partial => '/salva/show', :locals => { :body => body})
   end
   
