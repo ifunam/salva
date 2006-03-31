0e3ed1b7f941f8139f1bbe0bea588b1667eef272
Developer at IFUNAM <dev@fisica.unam.mx>
2006-03-31 03:29:36 +0000

    Agregamos datos de los catalogos correspondientes a la  modalidad de la nacionalidad, el estado migratorio y las identificaciones....

---
+++ trunk/db/catalogs/citizenmodalities.sql
@@ -0,0 +1,4 @@
+-- http://www.sre.gob.mx/tramites/nacionalidad/default.htm
+INSERT INTO citizenmodalities (name) VALUES ('Por nacimiento');
+INSERT INTO citizenmodalities (name) VALUES ('Por naturalización');
+INSERT INTO citizenmodalities (name) VALUES ('Otro');
---
+++ trunk/db/catalogs/identifications.sql
@@ -0,0 +1,6 @@
+INSERT INTO identifications(name) VALUES ('Pasaporte');
+INSERT INTO identifications(name) VALUES ('RFC');
+INSERT INTO identifications(name) VALUES ('CURP');
+INSERT INTO identifications(name) VALUES ('No. credencial de elector');
+INSERT INTO identifications(name) VALUES ('VISA');
+
