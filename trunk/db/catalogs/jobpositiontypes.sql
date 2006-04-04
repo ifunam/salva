8b39cef8fa9992ded901d3959ad3c566a859e969
Developer at IFUNAM <dev@fisica.unam.mx>
2006-04-04 04:41:58 +0000

    informacion para tipos de empleo en la UNAM y su nivel...

---
+++ trunk/db/catalogs/jobpositiontypes.sql
@@ -0,0 +1,4 @@
+INSERT INTO jobpositiontypes (name) VALUES ('Personal académico para docencia');
+INSERT INTO jobpositiontypes (name) VALUES ('Personal académico para investigación');
+INSERT INTO jobpositiontypes (name) VALUES ('Personal administrativo de base');
+INSERT INTO jobpositiontypes (name) VALUES ('Personal administrativo de confianza');
