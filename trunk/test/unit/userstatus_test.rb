5e047cc1424ae4771a75580fb90e31029a8375c6
Developer at IFUNAM <dev@fisica.unam.mx>
2006-05-04 17:15:20 +0000

    Just a CRUD unit test for Userstatus..

--- trunk/test/unit/userstatus_test.rb
+++ trunk/test/unit/userstatus_test.rb
@@ -1,14 +1,45 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
+# grab the user model
+require 'userstatus'
+
 class UserstatusTest < Test::Unit::TestCase
-  fixtures :userstatus
+  fixtures :userstatuses
 
-  def setup
-    @userstatus = Userstatus.find(1)
+  # CRUD test
+
+  def test_create
+    # create a brand new userstatus
+    @userstatus = userstatuses(:example)
+
+    # save him
+    assert @userstatus.save
+  end  
+  
+  def test_read
+    # read him back
+    @userstatus = Userstatus.find(userstatuses(:example).id)
+    
+    # compare the names
+    assert_equal @userstatus.name, userstatuses(:example).name
+    
+    # compare the ids
+    assert_equal @userstatus.id, userstatuses(:example).id
   end
-
-  # Replace this with your real tests.
-  def test_truth
-    assert_kind_of Userstatus,  @userstatus
+  
+  def test_update
+    @userstatus = Userstatus.find(userstatuses(:example).id)
+    
+    # change the name for the user status by using hi-tech encryption ;)
+     @userstatus.name = @userstatus.name.reverse
+    
+    # save the changes
+    assert @userstatus.update
+  end
+  
+  def test_delete
+    @userstatus = Userstatus.find(userstatuses(:example).id)
+    # the userstatus gets killed
+    assert @userstatus.destroy
   end
 end
