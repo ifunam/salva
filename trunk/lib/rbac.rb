897bc7b59916dc72cb45c1bb6641c07545e2947e
Developer at IFUNAM <dev@fisica.unam.mx>
2006-06-23 03:17:27 +0000

    cambios menores

--- trunk/lib/rbac.rb
+++ trunk/lib/rbac.rb
@@ -1,13 +1,18 @@
 module Rbac
   def rbac_required
-    authorized = false
-    roles = UserRoleingroups.find(:all, :conditions => [ "'user_id' = ? ", session[:user]))
+    roles = UserRoleingroup.find_by_user_id(session[:user])
+   
+    unless check_permission(roles, @controller.controller_name, 
+                            @controller.action_name)
+      access_denied and return false 
+    end
+  end
+  
+  def check_permission(roles,controller_name,action_name)
     roles.each { | rol |
-      permission = Permission.find(:first, :conditions => [ "'roleingroup_id = ? AND controller_id = ? AND action_id = ? ", rol.id, @controller.controller_class_name, @controller.action_name])
-      authorized = true and last if permission
+      permission = Permission.find(:conditions => ['roleingroup_id = ? AND controller_id = ? AND action_id = ?', rol.id, controller_name, action_name])
+      return true and break if permission
     }
-    access_denied and return false if authorized == false
   end
 end
 
-  
