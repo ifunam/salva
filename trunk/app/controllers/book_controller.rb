9187bef7ae98d115db5b30cb58fb2b62d2413498
Developer at IFUNAM <dev@fisica.unam.mx>
2006-06-27 06:34:04 +0000

    Omitimos temporalmente el uso de rbac en books...

--- trunk/app/controllers/book_controller.rb
+++ trunk/app/controllers/book_controller.rb
@@ -1,10 +1,9 @@
 class BookController < SalvaController
-
+  skip_before_filter :rbac_required
   def initialize
     super
     @model = Book
     @sequence = [ Book, Bookedition, [BookeditionRoleinbook, BookeditionComment]]
-#, [Chapterinbook, ChapterinbookRoleinbook, ChapterinbookComment ]] ]
     @per_pages = 10
     @order_by = 'title DESC'
   end
