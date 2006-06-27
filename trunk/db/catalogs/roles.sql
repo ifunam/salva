6938805d72aefee1811298c991fe8cfde43314a1
Developer at IFUNAM <dev@fisica.unam.mx>
2006-06-27 05:44:19 +0000

    Actualizamos catalogo con roles...

--- trunk/db/catalogs/roles.sql
+++ trunk/db/catalogs/roles.sql
@@ -1,3 +1,6 @@
 INSERT INTO roles (name) VALUES ('Administrador');
 INSERT INTO roles (name) VALUES ('Usuario');
+INSERT INTO roles (name) VALUES ('Secretarío académico');
+INSERT INTO roles (name) VALUES ('Jefe de departamento');
+INSERT INTO roles (name) VALUES ('Secretaria');
 INSERT INTO roles (name) VALUES ('Visitante');
