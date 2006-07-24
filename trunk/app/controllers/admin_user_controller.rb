ad2904a501d55e73683e0db44af2ef6de8597c2a
Developer at IFUNAM <dev@fisica.unam.mx>
2006-07-24 19:16:32 +0000

    Cambios menores para el controlador admin_user

--- trunk/app/controllers/admin_user_controller.rb
+++ trunk/app/controllers/admin_user_controller.rb
@@ -1,22 +1,11 @@
 class AdminUserController < SalvaController
-  helper :user
-
   def initialize
     super
     @model = User
-    @create_msg = 'El usuario se ha guardado'
-    @update_msg = 'El usuario se ha actualizado'
-    @purge_msg = 'El usuario se ha borrado'
+    @create_msg = 'La información se ha guardado'
+    @update_msg = 'La información ha sido actualizada'
+    @purge_msg = 'La información se ha borrado'
     @per_pages = 10
     @order_by = 'id'
   end
-
-  def index
-    list
-  end
-  
-  def list
-      @user_pages, @users = paginate :user, :per_page => 10
-  end
-  
 end
