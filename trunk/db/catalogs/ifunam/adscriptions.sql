4e2e58616d17e8d7fca032c63dd7ad35658f1008
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-12 17:11:20 +0000

    Agregamos entidades de adscripcion del instituto de fisica

---
+++ trunk/db/catalogs/ifunam/adscriptions.sql
@@ -0,0 +1,7 @@
+INSERT INTO adscriptions (name, abbrev, descr, institution_id, administrative_key) VALUES ('Física Teórica', '', '', 30, '');
+INSERT INTO adscriptions (name, abbrev, descr, institution_id, administrative_key) VALUES ('Física Experimental', '', '', 30, '');
+INSERT INTO adscriptions (name, abbrev, descr, institution_id, administrative_key) VALUES ('Física Experimentla', '', '', 30, '');
+INSERT INTO adscriptions (name, abbrev, descr, institution_id, administrative_key) VALUES ('Física Química', '', '', 30, '');
+INSERT INTO adscriptions (name, abbrev, descr, institution_id, administrative_key) VALUES ('Materia Condensada', '', '', 30, '');
+INSERT INTO adscriptions (name, abbrev, descr, institution_id, administrative_key) VALUES ('Estado Sólido', '', '', 30, '');
+INSERT INTO adscriptions (name, abbrev, descr, institution_id, administrative_key) VALUES ('Apoyo', '', '', 30, '');
