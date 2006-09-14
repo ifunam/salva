b1e9bc98580fe326f4c6bc88f51655511a5f887b
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-14 22:38:34 +0000

    Medio -> Intermedio...

--- trunk/db/catalogs/languagelevels.sql
+++ trunk/db/catalogs/languagelevels.sql
@@ -1,3 +1,3 @@
 INSERT INTO languagelevels (name) VALUES ('Básico');
-INSERT INTO languagelevels (name) VALUES ('Medio');
+INSERT INTO languagelevels (name) VALUES ('Intermedio');
 INSERT INTO languagelevels (name) VALUES ('Avanzado');
