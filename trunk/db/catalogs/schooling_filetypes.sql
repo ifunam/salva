22035ad3571dc4fbbd1a0d2717001112cc28df76
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-21 17:37:00 +0000

    Catalogo de documentos probatorios de los estudiantes, thx Martin

---
+++ trunk/db/catalogs/schooling_filetypes.sql
@@ -0,0 +1,10 @@
+INSERT INTO schooling_filetypes(name)VALUES('Historia académica');
+INSERT INTO schooling_filetypes(name)VALUES('Carta de servicio social');
+INSERT INTO schooling_filetypes(name)VALUES('Constancia de grado');
+INSERT INTO schooling_filetypes(name)VALUES('Carta de créditos');
+INSERT INTO schooling_filetypes(name)VALUES('Título o acta de examen');
+INSERT INTO schooling_filetypes(name)VALUES('Certificado oficial de estudios');
+INSERT INTO schooling_filetypes(name)VALUES('Historial académico');
+
+--INSERT INTO schooling_filetypes(name)VALUES('Copia de acta de nacimiento');
+-- INSERT INTO schooling_filetypes(name)VALUES('Curriculum vitae');
