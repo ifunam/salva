c333d7cb0d93906219b59bb051f7d69ff7b9d7e3
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-22 16:36:35 +0000

    Se incluye soporte para actualizaciones en el  modelo de llaves compuestas y otros cambios menores...

--- trunk/app/models/model_composed_keys.rb
+++ trunk/app/models/model_composed_keys.rb
@@ -1,7 +1,10 @@
+require 'sql'
 class ModelComposedKeys < ActiveRecord::Base
-
+  
   # Class Methods
   class << self
+    include Sql
+
     def set_primary_keys(*keys)
       cattr_accessor :primary_keys, :reserved_attributes 
       self.primary_keys = keys.collect {|key| key.to_s}
@@ -11,20 +14,18 @@ class ModelComposedKeys < ActiveRecord::Base
     def find(*args)
       options = extract_options_from_args!(args)
       case args.first      
-      when :first then find_first_composed_keys(options)
-      when :all   then find_every_composed_keys(options)
-      else             find_composed_keys(args.first)
+      when :first         then find_first_composed_keys(options)
+      when :first_nogroup then first_nogroup_composed_keys(options)
+      when :all           then find_every_composed_keys(options)
+      when :all_nogroup   then every_nogroup_composed_keys(options)
+      else                find_composed_keys(args.first)
       end
-    end
-
-    def set_conditions_for_primary_keys(ids)
-      [ set_query(self.primary_keys) ] + ids.split(':') 
     end
 
     private
     def find_composed_keys(ids)
-      set_ids(ids) # Used for destroy
-      conditions = set_conditions_for_primary_keys(ids)
+      set_ids(ids) 
+      conditions = set_conditions(self.primary_keys,ids.split(':'))
       record = find_initial({:conditions => conditions})
       attributes = (record.attribute_names - self.reserved_attributes) - self.primary_keys
       grouped_records = find_every({:select => attributes.join(','), :conditions => conditions})
@@ -33,15 +34,34 @@ class ModelComposedKeys < ActiveRecord::Base
       record
     end
 
-    def set_query(keys)
-      keys.collect { |key|  key.to_s + ' = ?' }.join(' AND ')
+    def find_every_composed_keys(options)
+      grouped_records = find_every({:select => self.primary_keys.join(','), :group => self.primary_keys.join(',')})
+      grouped_records.collect { |grouped_record| find_composed_keys(get_ids_from_record(grouped_record)) }
+    end
+
+    def find_first_composed_keys(options)
+      record = find_initial({:conditions =>  options[:conditions]})
+      if record != nil
+        grouped_records = find_every({:conditions => options[:conditions]})
+        attributes = (record.attribute_names - self.reserved_attributes) - self.primary_keys
+        record = set_arrays_in_record(record, grouped_records, attributes)
+        record
+      end
+    end
+
+    def first_nogroup_composed_keys(options)
+      find_initial({:conditions =>  options[:conditions]})
+    end
+
+    def every_nogroup_composed_keys(options)
+      find_every({:conditions => options[:conditions]})
     end
     
     def set_ids(ids)
       cattr_accessor :ids
       self.ids = ids
     end
-    
+
     def set_arrays_in_record (record, records, attributes)
       attributes.each { |attribute|
         array = records.collect { |myrecord| myrecord.send(attribute) }
@@ -50,93 +70,104 @@ class ModelComposedKeys < ActiveRecord::Base
       record
     end
 
-    def find_every_composed_keys(options)
-      grouped_records = find_every({:select => self.primary_keys.join(','), 
-                                     :group => self.primary_keys.join(',')})
-      records = []
-      grouped_records.each { |grouped_record|
-        records << find_composed_keys(get_ids_from_record(grouped_record))
-      }
-      records
-    end
-
-    def find_first_composed_keys(options)
-      record = find_initial({:conditions =>  options[:conditions]})
-      grouped_records = find_every({:conditions => options[:conditions]})
-      attributes = (record.attribute_names - self.reserved_attributes) - self.primary_keys
-      record = set_arrays_in_record(record, grouped_records, attributes)
-      record
-    end
-
     def get_ids_from_record(record)
       self.primary_keys.collect { |key| record.send(key) }.join(':')
     end
-    
   end
 
   # Instance Methods
+  include Sql
+
   def destroy
-    conditions = self.class.set_conditions_for_primary_keys(self.ids) 
-    self.class.find(:all, :conditions => conditions).each { |object|
-      super
-      object.destroy
+    conditions = set_conditions(self.primary_keys,self.ids.split(':')) 
+    self.class.find(:all_nogroup, :conditions => conditions).each { |object|
+      self.class.delete(object.attributes['id'])
     }
   end
 
   def save 
-    attributes = set_uncomposed_attributes
+    (self.public_methods.include? 'ids') ? my_update : my_save
+  end
+  
+  private
+
+  def my_save
+    attributes = get_uncomposed_attributes
     models = fill_models(attributes)
     return false unless are_valid_models?(models)
-    models.each { |model|
-      model.create_or_update
-    }
+    models.each { |model|  model.create }
     return true
   end
-
-  def update_attributes(attributes)
-    self.save
+  
+  def my_update
+    attributes  = get_uncomposed_attributes
+    array_keys  = get_array_keys(attributes) 
+    (0..get_maxsize_from_array_attributes(attributes)).each { |i|
+      conditions = set_conditions_for_find_first_nogroup(attributes,array_keys,i)
+      model = self.class.find(:first_nogroup, :conditions => conditions)
+      my_create_or_update(attributes,i,model)
+    }
+    # Here is missing the destroy method to delete the unselected records.
+    return true
+  end
+  
+  def my_create_or_update(attributes, i, model=nil)
+    if model != nil
+      self.attributes = fill_model_to_update(attributes, i)
+      model.update
+    else
+      new_model = fill_model(attributes, i) 
+      new_model.create
+    end
   end
 
-  private
-  def set_uncomposed_attributes
-    attributes = Hash.new
-    ((self.attribute_names - self.reserved_attributes) - self.primary_keys).each { |key|
-      attributes[key] = self.attributes_before_type_cast[key]
-    }
-    return attributes
+  def get_uncomposed_attributes
+    keys = (self.attribute_names - self.reserved_attributes) - self.primary_keys
+    keys.inject({}) do |params, key|  
+      params[key] = self.attributes_before_type_cast[key] 
+      params
+    end
   end
 
   def fill_models(attributes)
-    models = []
-    i = 0
-    while  i <= get_max_array_size(attributes)
-      models << fill_model(attributes, i)
-      i = i + 1
-    end
-    models
+    (0..get_maxsize_from_array_attributes(attributes)).collect { |i| fill_model(attributes,i) }
   end
   
-  def get_max_array_size(uncomposed_attributes)
-    array = []
-    uncomposed_attributes.map { |key,value|  array << value.size if value.is_a?(Array) }
+  def get_maxsize_from_array_attributes(attributes)
+    array = attributes.values.collect! { |value| value.size if value.is_a?(Array) }.compact
     array.empty? ? 0 : array.sort.last - 1
   end 
-
-  def fill_model(uncomposed_attributes, i)
+  
+  def fill_model(uattributes, i)
     model = self.class.new
     self.primary_keys.each { |key| model.[]=(key, self.attributes[key]) }
-    uncomposed_attributes.keys.each { |key| 
-      if uncomposed_attributes[key].is_a? Array
-        model.[]=(key, uncomposed_attributes[key][i])
-      else
-        model.[]=(key, uncomposed_attributes[key])
-      end
+    uattributes.keys.each { |key| 
+      next if key == 'id'
+      (uattributes[key].is_a? Array) ? (model.[]=(key, uattributes[key][i])) : (model.[]=(key, uattributes[key]))
     }
     return model
+  end
+
+  def fill_model_to_update(attributes, i)
+    attributes.keys.inject({}) do |params, key|  
+      (attributes[key].is_a? Array) ? (params[key] = attributes[key][i]) : (params[key] = attributes[key])
+      params
+    end
   end
   
   def are_valid_models?(models)
     models.each { |model| return false unless model.valid? }
     return true
   end
+  
+  def get_array_keys(attributes)
+    attributes.keys.collect! { |key| key if attributes[key].is_a? Array }.compact!
+  end
+
+  def set_conditions_for_find_first_nogroup(attributes, array_keys, i)
+    keys = self.primary_keys + array_keys
+    ids = self.ids.split(':')
+    set_conditions(keys, ids) + array_keys.collect { |key| attributes[key][i] } 
+  end
 end
+
