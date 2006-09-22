3a4273eed92ed73aa62db38275050a7afe75e7aa
Alejandro Aguilar Sierra <algsierra@gmail.com>
2006-09-22 22:27:23 +0000

    AcadVisit scaffoldeado

---
+++ trunk/app/models/acadvisit.rb
@@ -0,0 +1,8 @@
+class Acadvisit < ActiveRecord::Base
+validates_presence_of :institution_id, :country_id, :acadvisittype_id, :name, :startyear
+validates_numericality_of :institution_id, :country_id, :acadvisittype_id, :visitor_id
+belongs_to :institution
+belongs_to :country
+belongs_to :acadvisittype
+belongs_to :visitor
+end
---
+++ trunk/app/models/acadvisittype.rb
@@ -0,0 +1,3 @@
+class Acadvisittype < ActiveRecord::Base
+validates_presence_of :name
+end
