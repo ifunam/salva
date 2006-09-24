01dbd5b57bcafc389557382a4596bc2ed2cc4ca6
Alejandro Aguilar Sierra <algsierra@gmail.com>
2006-09-24 16:56:18 +0000

    Catalogo para cursos

---
+++ trunk/db/catalogs/courses.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO coursetypes (name) VALUES ('Regular');
+INSERT INTO coursetypes (name) VALUES ('Especial');
+
+INSERT INTO courseduration (name, days) VALUES ('Semanal', 5);
+INSERT INTO courseduration (name, days) VALUES ('Mensual', 20);
+INSERT INTO courseduration (name, days) VALUES ('Trimestral', 90);
+INSERT INTO courseduration (name, days) VALUES ('Semestral', 180);
+INSERT INTO courseduration (name, days) VALUES ('Anual', 365);
+
+INSERT INTO coursegrouptypes (name) VALUES ('Actualización');
+INSERT INTO coursegrouptypes (name) VALUES ('Certificación');
+INSERT INTO coursegrouptypes (name) VALUES ('Diplomado');
+INSERT INTO coursegrouptypes (name) VALUES ('Especialización');
+
+INSERT INTO roleincourses (name) VALUES ('Autor');
+INSERT INTO roleincourses (name) VALUES ('Asistente');
+INSERT INTO roleincourses (name) VALUES ('Instructor');
