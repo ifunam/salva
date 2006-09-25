bd8fbeb12fbd8c790e9f57bc5084beaf09cfd41c
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-25 06:32:05 +0000

    Refactoramos el helper para el navigator, aun queda pendiente el defecto de la etiqueta del controlador al usar wizard..

--- trunk/app/helpers/navigator_helper.rb
+++ trunk/app/helpers/navigator_helper.rb
@@ -43,45 +43,45 @@ module NavigatorHelper
   def link_tag_navtab(label,counter)
     link_to(get_label(label), { :controller => 'navigator', :item => counter})
   end
-  
+
+  def get_controller_name
+    @controller_name ||= @controller.class.name.sub(/Controller$/, '').underscore
+  end
+
   def title
-    controller = @controller.class.name.sub(/Controller/,'').downcase
+    controller = get_controller_name
     imgpath = RAILS_ROOT + "/public/images/" + controller + ".png"
     img = controller
     label = controller
-    if controller == 'navigator' then
+    if controller == 'navigator' or controller == 'wizard'then
       tree = @session[:navtree]
       img = tree.data
       label = tree.data
       imgpath = RAILS_ROOT + "/public/images/" + tree.data + ".png"
     end
-    #img = "comodin_salva" if !File.exists?(imgpath) 
+    img = "comodin_salva" if !File.exists?(imgpath) 
     img_tag(img) + get_label(label)
   end
   
   def navbar_icons
+    controller = get_controller_name
     tree = get_tree
-    controller = @controller.class.name.sub(/Controller/,'').downcase
     path = tree.path
-    counter = path.length 
-    counter -= 1
-    
-    if controller == 'navigator' then
-      path.delete_at(0) 
-    end
-    
-    maxsize = 28
-    maxicons = 4
-    imgsize = maxsize - ((path.length - 1) * maxicons) if path.length > 0
-    
+    counter = (path.length - 1)
+    path.delete_at(0) if controller == 'navigator' or controller == 'wizard'
+    imgsize = get_image_size(path.length)
     links = []
     path.reverse.each {  |image|
-      links << link_to(img_tag(image, imgsize), 
-                       {:controller => 'navigator', :depth => counter})
+      links << link_to(img_tag(image, imgsize), {:controller => 'navigator', :depth => counter})
       imgsize += 4
       counter -= 1
-      
     }
     links.join(' ')
+  end
+  
+  def get_image_size(path_length)
+    maxsize = 28
+    maxicons = 4
+    imgsize = maxsize - ((path_length - 1) * maxicons) if path_length > 0
   end
 end  
