1e7afaa926b44ae08c37df97253a6e0acf339ca4
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-27 03:32:53 +0000

    Los quickposts ya funcionan, aun queda pendiente que actualizen los selects. Ademas regresamos a la version 0.3 de zebda..

--- trunk/app/helpers/quickpost_helper.rb
+++ trunk/app/helpers/quickpost_helper.rb
@@ -1,7 +1,7 @@
 require 'salva_helper'
 module QuickpostHelper 
   include SalvaHelper
-
+  
   def quickpost(partial,mode='simple')
     if mode  == 'simple'
       link_to_function(image_tag('add.gif', { :border=>0, :valign => 'middle', :alt=> get_label('add') }), 
@@ -17,41 +17,31 @@ module QuickpostHelper
       " {#{false_opts}}) : new #{true_effect}('#{domid}', {#{true_opts}}); " + 
       "return false;"
   end
+
+  def form_remote_tag_for_quickpost(controller, partial, attribute)
+    form_remote_tag(:success => "new Effect.BlindUp('"+controller+"', {duration:0.4});; ",
+                    404 => "alert('Not found...? Wrong URL...?')", 
+                    :loading => "AjaxScaffold.newOnLoading(request,'"+controller+"');", 
+                    :failure => "alert('HTTP Error ' + request.status + '!');", 
+                    :url  => { :controller => controller, :action => 'create' }
+                    )
+  end
+
+  def update_select_from_quickpost(partial, attrdiv_id=nil)
+    partial_note = partial + '_note'
+    params = "'partial=#{partial}&id='+$F('#{attrdiv_id}')" if attrdiv_id
+    remote_function(:url => {:action => :update_select},
+                    :success => "Effect.BlindUp('#{partial}_note}', {duration: 0.4);;")
+  end
   
   def loading_indicator_tag(scope,id)
     render(:partial => '/salva/loading_indicator_tag', 
            :locals => { :scope => scope, :id => id })
   end  
   
-#  def form_remote_tag_for_quickpost(controller, partial, attribute)
-#    success = update_select_from_quickpost(partial,attribute) if partial
-#    form_remote_tag (:url => {:controller => controller, :action => "create"},
-#                     :update => {:success => "alert('Cargando esta mierda');"},
-#                     :loading => "alert('Cargando esta mierda');",
-#                     :complete => "alert('Esta mierda esta completa')",
-#                     :failure => "alert('Not found...? Wrong URL...?')")
-#  end
-   def form_remote_tag_for_quickpost(controller, partial, attribute)
-     success = update_select_from_quickpost(partial,attribute) 
-     form_remote_tag(:update => {:success => "alert('Cargando esta mierda');",
-                                 :loading => "alert('Cargando esta mierda');",
-                                  :complete => "alert('Esta mierda esta completa')"},
-                     :url => { :controller => controller, :action => 'create' })
-   end
-   
-   def submit_remote_tag_for_quickpost(controller, partial, attribute)
-     success = update_select_from_quickpost(partial,attribute) 
-   end
-
-   def update_select_from_quickpost(partial,attribute)
-     params = "'partial=#{partial}&id='+$F('#{attribute}')" 
-     partial_note = partial + '_note'
-     success_msg = "Effect.BlindUp('#{partial}', {duration: 0.4}); "
-     success_msg += "return false;"
-     loading_msg = "Toggle.display('#{partial_note}');"
-     remote_function(:update => partial, :with => params, :url => {:action => :update_select},
-                     :loading => loading_msg, :success => success_msg)
-   end
+  def submit_remote_tag_for_quickpost(controller, partial, attribute)
+    success = update_select_from_quickpost(partial,attribute) 
+  end
   
   def link_to_cancel_for_quickpost(controller)
     "<a href=\"#\" onclick=\"new Effect.BlindUp('#{controller}', {duration: 0.4}); " +
--- trunk/app/models/user_language.rb
+++ trunk/app/models/user_language.rb
@@ -1,7 +1,10 @@
 class UserLanguage < ActiveRecord::Base
-validates_presence_of :language_id, :spoken_languagelevel_id, :written_languagelevel_id
-validates_numericality_of :language_id, :spoken_languagelevel_id, :written_languagelevel_id
-belongs_to :language
-belongs_to :spoken_languagelevel, :class_name => 'Languagelevel', :foreign_key => 'spoken_languagelevel_id'
-belongs_to :written_languagelevel, :class_name => 'Languagelevel', :foreign_key => 'written_languagelevel_id'
+  attr_accessor :name
+
+  validates_presence_of :language_id, :spoken_languagelevel_id, :written_languagelevel_id
+  validates_numericality_of :language_id, :spoken_languagelevel_id, :written_languagelevel_id
+
+  belongs_to :language
+  belongs_to :spoken_languagelevel, :class_name => 'Languagelevel', :foreign_key => 'spoken_languagelevel_id'
+  belongs_to :written_languagelevel, :class_name => 'Languagelevel', :foreign_key => 'written_languagelevel_id'
 end
