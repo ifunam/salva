6bb863aecd71614452b3cd9a1d3ddb835c836ddb
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-27 04:42:53 +0000

    Arreglamos detalles con la usabilidad del formulario de cursos...

--- trunk/app/models/user_course.rb
+++ trunk/app/models/user_course.rb
@@ -1,4 +1,7 @@
 class UserCourse < ActiveRecord::Base
+  attr_accessor :degree_id
+
+
 validates_presence_of :roleincourse_id, :country_id, :courseduration_id, :modality_id, :year
 validates_numericality_of :courses_id, :coursegroup_id, :roleincourse_id, :institution_id, :country_id, :courseduration_id, :coursemodality_id
 belongs_to :courses
