3107699892732bfbaa96a3515ef1b5fa715b0cd0
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-28 04:50:06 +0000

    Agregamos el filtro de instituciones por pais al formulario de experiencia laboral externa...

--- trunk/app/models/jobposition.rb
+++ trunk/app/models/jobposition.rb
@@ -1,11 +1,10 @@
 class Jobposition < ActiveRecord::Base
-  attr_accessor :jobpositiontype_id
+  attr_accessor :jobpositiontype_id, :country_id
   
   validates_presence_of :institution_id, :startyear
   validates_numericality_of :institution_id #:jobpositioncategory_id, :contracttype_id, 
+
   belongs_to :jobpositioncategory
   belongs_to :contracttype
   belongs_to :institution
-
-
 end
