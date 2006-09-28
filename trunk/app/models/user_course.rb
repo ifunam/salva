4d389a01be5715e23209ca796c71e685cc75090b
Alejandro Aguilar Sierra <algsierra@gmail.com>
2006-09-28 05:51:45 +0000

    validaciones invalidas en user_course

--- trunk/app/models/user_course.rb
+++ trunk/app/models/user_course.rb
@@ -2,7 +2,7 @@ class UserCourse < ActiveRecord::Base
   attr_accessor :degree_id
 
   validates_presence_of :roleincourse_id, :country_id, :courseduration_id, :modality_id, :year
-  validates_numericality_of :courses_id, :coursegroup_id, :roleincourse_id, :institution_id, :country_id, :courseduration_id, :coursemodality_id
+  validates_numericality_of :coursegroup_id, :roleincourse_id, :institution_id, :country_id, :courseduration_id
 
   belongs_to :courses
   belongs_to :coursegroup
@@ -10,5 +10,4 @@ class UserCourse < ActiveRecord::Base
   belongs_to :institution
   belongs_to :country
   belongs_to :courseduration
-  belongs_to :coursemodality
 end
