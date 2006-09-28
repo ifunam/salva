49d3d582d2ec77ad18df3f363d3d2d34c74fca41
Developer at IFUNAM <dev@fisica.unam.mx>
2006-09-28 22:28:39 +0000

    Agregamos el catalogo de tipos de premios...

---
+++ trunk/db/catalogs/prizetypes.sql
@@ -0,0 +1,5 @@
+INSERT INTO prizetypes (name) VALUES ('Diploma');
+INSERT INTO prizetypes (name) VALUES ('Medalla');
+INSERT INTO prizetypes (name) VALUES ('Premio');
+INSERT INTO prizetypes (name) VALUES ('Distinción');
+INSERT INTO prizetypes (name) VALUES ('Otro');
