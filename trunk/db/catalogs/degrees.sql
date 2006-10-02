43d99ed474830ce7c31d1bda1a6afe47651fff2c
Developer at IFUNAM <dev@fisica.unam.mx>
2006-10-02 16:40:14 +0000

    Dejamos nivel Licenciatura solamente...

--- trunk/db/catalogs/degrees.sql
+++ trunk/db/catalogs/degrees.sql
@@ -1,7 +1,7 @@
 -- http://www.unesco.org/iau/onlinedatabases/systems_data/mx.rtf
 INSERT INTO degrees (name) VALUES ('Bachillerato');
 INSERT INTO degrees (name) VALUES ('Técnico');
-INSERT INTO degrees (name) VALUES ('Licenciatura | Ingeniería');
+INSERT INTO degrees (name) VALUES ('Licenciatura');
 INSERT INTO degrees (name) VALUES ('Especialización');
 INSERT INTO degrees (name) VALUES ('Maestría');
 INSERT INTO degrees (name) VALUES ('Doctorado');
