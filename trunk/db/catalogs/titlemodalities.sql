1bc13f720e3f2384b7897678f051ec5bde94a5e5
Developer at IFUNAM <dev@fisica.unam.mx>
2006-10-18 20:24:26 +0000

    Actualizamos las modalidades de titulacion

--- trunk/db/catalogs/titlemodalities.sql
+++ trunk/db/catalogs/titlemodalities.sql
@@ -1,5 +1,8 @@
 INSERT INTO titlemodalities (name) VALUES ('Tesis');
 INSERT INTO titlemodalities (name) VALUES ('Tesina');
+INSERT INTO titlemodalities (name) VALUES ('Ensayo');
 INSERT INTO titlemodalities (name) VALUES ('Ceneval');
 INSERT INTO titlemodalities (name) VALUES ('Reporte técnico');
+INSERT INTO titlemodalities (name) VALUES ('Informe de práctica profesional');
+INSERT INTO titlemodalities (name) VALUES ('Informe de servicio social');
 INSERT INTO titlemodalities (name) VALUES ('Por promedio');
