b422906a247fb047e91bf624dc658189c691a5ce
Developer at IFUNAM <dev@fisica.unam.mx>
2006-11-01 03:26:53 +0000

    Agregamos areas y lineas de investigacion para la fcpys...

---
+++ trunk/db/catalogs/fcpys/researchareas.sql
@@ -0,0 +1,161 @@
+INSERT INTO researchareas (name) VALUES ('Administración Pública');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Neo institucionalismo, administración pública y desarrollo', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Servicio profesional de carrera', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Políticas públicas', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Políticas públicas y organizaciones no gubernamentales', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Democracia y servicio profesional de carrera', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Gobernabilidad', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Evaluación de programas y políticas de rendición de cuentas y transparencia  gubernamental', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Federalismo y órdenes de gobierno', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Nuevas tecnologías en la administración pública', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Gerencia  - Gestión Pública', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Gestión del cambio en las organizaciones', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Ciudadanos, espacios públicos y democracia', 1);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Ética pública', 1);
+
+
+INSERT INTO researchareas (name) VALUES ('Ciencias de la Comunicación');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Teoría y práctica de la comunicación', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación y periodismo', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Periodismo especializado ', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación y género ', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación política', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación organizacional', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación y medios audiovisuales ', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación y publicidad', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación y semiótica', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación y áreas multidisciplinarias', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación e innovaciones tecnológicas', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación, innovaciones tecnológicas y educación', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación, ciencia y tecnología', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Estrategias de comunicación y tolerancia', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Análisis del discurso', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Metodología de la investigación periodística', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Industria cultural', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Investigación para la docencia', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Ciencia social y filosofía', 2);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Nuevas tendencias y campos de estudio en ciencias de la comunicación', 2);
+
+
+INSERT INTO researchareas (name) VALUES ('Ciencia Política');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Sistema político mexicano', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Democracia, instituciones y procesos políticos', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Transición política, alternancia y proceso de gobierno en México', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Partidos políticos', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Instituciones y procesos electorales', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Reforma del Estado', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('El poder judicial federal', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Proceso legislativo ', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Federalismo', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Multiculturalismo, identidades culturales y ciudadanía', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Participación política y derecho a la diferencia', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Derecho y cultura indígenas', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Filosofía y teoría política', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Comunicación política, opinión pública y cultura política', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Historia política y social ', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Metodología aplicada a las ciencias sociales', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Organización sindical y política laboral', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Política social y desarrollo ', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Política y cultura', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Prospectiva política', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Seguridad nacional', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Movimientos, actores y organizaciones sociales y políticas', 3);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Legalidad y violencia', 3);
+
+
+INSERT INTO researchareas (name) VALUES ('Relaciones Internacionales');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Relaciones de México con Estados Unidos', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('México y el TLCAN', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Aspectos teórico-metodológicos de la política exterior de México', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Desafíos de la política exterior de México', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Políticas comparadas', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('México y la Unión Europea', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('La Unión Europea y el TLCAN', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('México y el TLCUE', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Estrategias de aprendizaje para las materias sobre economía ', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Modelos de desarrollo económico', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('El Tratado de Libre Comercio México-Japón', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Geopolítica', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES (' teoría y práctica', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Diccionario de política internacional', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Pensamiento político estadounidense',4);
+INSERT INTO researchlines (name, researcharea_id) VALUES (' pragmatismo y globalización', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Seguridad internacional y seguridad hemisférica', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Negociaciones internacionales', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Medio ambiente y cooperación internacional', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Paz y seguridad y desarrollo', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Teoría de las relaciones internacionales, los universos culturales, los procesos globales y  la renovación del conocimiento', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('El impacto de las revoluciones globales en las Relaciones Internacionales', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Pensamiento internacional latinoamericano', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Diversidad cultural y globalización', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Derecho penal internacional (las adecuaciones y reformas legales del Estado Mexicano)', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Derecho Diplomático', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Reforma de la ONU', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Desarrollos temáticos actuales para la enseñanza del derecho internacional', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Estudios africanos', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Procesos de integración', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('la Unión Europea, MERCOSUR, TLCAN y  ALCA', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Migración Coreana a México', 4); 
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Prospectiva global en los escenarios regionales', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('México y la Cuenca del Pacífico', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Los empresarios mexicanos en  Asia Pacífico', 4);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Estados Unidos y Canadá', 4);
+
+
+INSERT INTO researchareas (name) VALUES ('Sociología');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Cultura e identidad', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Metodología de la investigación', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Movimientos sociales', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Políticas sociales', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Problemas nacionales', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Sociología ambiental', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Sociología de género', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Sociología de la educación', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Sociología del cine', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Sociología del trabajo', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Sociología jurídica', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Teoría sociológica', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Diversidad e identidad (género, plurietnia, valores, cultura, infancia)', 5);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Investigación para la docencia', 5);
+
+
+INSERT INTO researchareas (name) VALUES ('Centro de Estudios Latinoamericanos');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Desarrollo y constitución estructural de América Latina y el Caribe', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Estado y Actores sociopolíticos en América Latina y el Caribe', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Estudios de la cultura en América Latina y el Caribe', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Democratización en América Latina y el Caribe', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Ecología política en América Latina y el Caribe', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Gobernabilidad y políticas públicas en América Latina y el Caribe', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Migración y movimientos sociales en América Latina y el Caribe', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Sociología e historia de América Latina y el Caribe', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Estudios de género', 6);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Globalización e integración en América Latina y el Caribe', 6);
+
+
+INSERT INTO researchareas (name) VALUES ('Centro de Estudios Europeos');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Relaciones México- Unión Europea: inclusión social y gobernabilidad',7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Regionalismos comparados: TLCAN-UE', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Regionalismos y Seguridad Internacional', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Prospectiva de la relación México-Unión Europea: escenarios futuros', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('La Doctrina de la Política Exterior de la Unión Europea ', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('El modelo social europeo un ejemplo para México', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('La Unión Europea y su política de vecindad', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('La Unión Europea en la prevención de conflictos y gestión de crisis', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Las instituciones promotoras del desarrollo en el ámbito supranacional y nacional en el proceso de integración europeo', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('La ampliación de la Unión Europea', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Impactos internos e internacionales', 7);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('El futuro institucional de la UE y la construcción de una identidad europea', 7);
+
+
+INSERT INTO researchareas (name) VALUES ('Centro de Estudios de Opinión Pública');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Opinión Pública', 8);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Estudios Electorales', 8);
+
+
+INSERT INTO researchareas (name) VALUES ('Centro de Estudios Básicos en Teoría Social');
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Educación', 9);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Derechos humanos', 9);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Migración', 9);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Riesgo y desarrollo sustentable', 9);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Género', 9);
+INSERT INTO researchlines (name, researcharea_id) VALUES ('Cultura', 9);
