13ff7d5db78cfc96f039f055f39dcf8c3c487aea
Alejandro Aguilar Sierra <algsierra@gmail.com>
2006-11-24 00:42:34 +0000

    correcciones varias

--- trunk/app/models/journal.rb
+++ trunk/app/models/journal.rb
@@ -1,6 +1,6 @@
 class Journal < ActiveRecord::Base
-validates_presence_of :name, :mediatype_id, :publisher_id, :country_id
-validates_numericality_of :mediatype_id, :publisher_id, :country_id
+validates_presence_of :name, :mediatype_id, :country_id
+validates_numericality_of :mediatype_id, :country_id
 belongs_to :mediatype
 belongs_to :publisher
 belongs_to :country
--- trunk/app/models/user_genericwork.rb
+++ trunk/app/models/user_genericwork.rb
@@ -4,6 +4,7 @@ class UserGenericwork  < ModelComposedKeys
 
   validates_presence_of :genericwork_id, :userrole_id
   validates_numericality_of :genericwork_id, :userrole_id
+  validates_uniqueness_of :user_id, :genericwork_id, :userrole_id, :message=> "Dato duplicado"
   belongs_to :genericwork
   belongs_to :userrole
 end
