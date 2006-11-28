b7aee84f32ad280af48b18e06ff8ed478763eb6f
Alejandro Aguilar Sierra <algsierra@gmail.com>
2006-11-28 22:03:27 +0000

    Areas de investigacion CCA

---
+++ trunk/db/catalogs/cca/researcharea.sql
@@ -0,0 +1,16 @@
+INSERT INTO researchareas (name) VALUES ('Física del Clima');
+INSERT INTO researchareas (name) VALUES ('Cambio y Variabilidad Climática');
+INSERT INTO researchareas (name) VALUES ('Climatología Dinámica');
+INSERT INTO researchareas (name) VALUES ('Meteorología Tropical');
+INSERT INTO researchareas (name) VALUES ('Climatología Urbana');
+INSERT INTO researchareas (name) VALUES ('Transferencia de Radiación');
+INSERT INTO researchareas (name) VALUES ('Física de Nubes y de Aerosoles');
+INSERT INTO researchareas (name) VALUES ('Interacción Micro y Mesoescala');
+INSERT INTO researchareas (name) VALUES ('Modelación Atmosférica');
+INSERT INTO researchareas (name) VALUES ('Bioclimatología y Electricidad Atmosférica');
+INSERT INTO researchareas (name) VALUES ('Química Atmosférica'); 
+INSERT INTO researchareas (name) VALUES ('Físico-Química Atmosférica');
+INSERT INTO researchareas (name) VALUES ('Contaminación Ambiental'); 
+INSERT INTO researchareas (name) VALUES ('Aerobiología'); 
+INSERT INTO researchareas (name) VALUES ('Citogenética Ambiental');
+INSERT INTO researchareas (name) VALUES ('Mutagénesis Ambiental');
