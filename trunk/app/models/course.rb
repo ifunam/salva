44b852a4691a7870dc4eb90d69c38975536bc97e
Developer at IFUNAM <dev@fisica.unam.mx>
2006-11-29 18:47:49 +0000

    Actualizamos los cambios de la tabla courses en sus respectivas vista y modelo

--- trunk/app/models/course.rb
+++ trunk/app/models/course.rb
@@ -1,11 +1,10 @@
 class Course < ActiveRecord::Base
-validates_presence_of :name, :coursetype_id, :country_id, :courseduration_id, :modality_id, :startyear
-validates_numericality_of :coursetype_id, :degree_id, :country_id, :institution_id, :courseduration_id, :modality_id
-belongs_to :coursetype
-belongs_to :degree
-belongs_to :country
-belongs_to :institution
-belongs_to :coursegroup
-belongs_to :courseduration
-belongs_to :modality
+  validates_presence_of :name, :country_id, :courseduration_id, :modality_id, :startyear
+  validates_numericality_of :country_id, :institution_id, :courseduration_id, :modality_id
+
+  belongs_to :country
+  belongs_to :institution
+  belongs_to :coursegroup
+  belongs_to :courseduration
+  belongs_to :modality
 end
