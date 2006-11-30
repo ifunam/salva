6f8dea2e04a0d97b03f8c7fbdfef79551154e97c
Developer at IFUNAM <dev@fisica.unam.mx>
2006-11-30 23:27:50 +0000

    Agregamos catalogo de periodos escolares...

---
+++ trunk/db/catalogs/periods.sql
@@ -0,0 +1,14 @@
+-- http://www.dgae.unam.mx/noticias/calendar/calendar.html
+INSERT INTO periods (name, startdate, enddate) VALUES ('2003-1', '2002-09-12', '2004-01-24');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2003-2', '2003-02-24', '2003-06-20');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2004-1', '2003-08-18', '2004-12-05');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2004-2', '2004-02-09', '2004-06-04');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2005-1', '2004-08-16', '2004-12-03');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2005-2', '2005-02-02', '2005-06-03');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2006-1', '2005-08-15', '2005-12-02');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2006-2', '2006-02-13', '2006-06-09');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2007-1', '2006-08-14', '2006-11-30');
+INSERT INTO periods (name, startdate, enddate) VALUES ('2007-2', '2007-02-12', '2007-06-06');
+
+
+
