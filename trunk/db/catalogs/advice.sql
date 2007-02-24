a9a1e39451d8b1bce2aad5284fefebe646c7ef4f
Alejandro Aguilar Sierra <algsierra@gmail.com>
2007-02-24 00:13:24 +0000

    Catalogo aumentado

--- trunk/db/catalogs/advice.sql
+++ trunk/db/catalogs/advice.sql
@@ -1,5 +1,8 @@
 INSERT INTO indivadvicetargets (name) VALUES ('Servicio social');
 INSERT INTO indivadvicetargets (name) VALUES ('Becario');
-INSERT INTO indivadvicetargets (name) VALUES ('Profesor');
 
 
+INSERT INTO instadvicetargets (name) VALUES ('Proyecto de investigación');
+INSERT INTO instadvicetargets (name) VALUES ('Planes o programas de estudio');
+INSERT INTO instadvicetargets (name) VALUES ('Material didáctico');
+INSERT INTO instadvicetargets (name) VALUES ('Evaluaci�n de cualquier cosa wey');
