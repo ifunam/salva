1c4aab08056dc86617e6994ddd0b237e91c73c5f
Alejandro Aguilar Sierra <algsierra@gmail.com>
2007-03-06 03:12:05 +0000

    Correccion a catalogo

--- trunk/db/catalogs/advice.sql
+++ trunk/db/catalogs/advice.sql
@@ -5,8 +5,6 @@ INSERT INTO indivadvicetargets (name) VALUES ('Profesor');
 INSERT INTO instadvicetargets (name) VALUES ('Proyecto de investigaciÃ³n');
 INSERT INTO instadvicetargets (name) VALUES ('Planes o programas de estudio');
 INSERT INTO instadvicetargets (name) VALUES ('Material didÃ¡ctico');
-INSERT INTO instadvicetargets (name) VALUES ('Evaluaciã³n de cualquier cosa wey');
-
 
 INSERT INTO indivadviceprograms (name, institution_id) VALUES ('PAAS', 96);
 INSERT INTO indivadviceprograms (name, institution_id) VALUES ('PIDI', 96);
