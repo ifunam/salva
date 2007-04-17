32919a4601e70671b62c2231d68ba846f68c6812
Developer at IFUNAM <dev@fisica.unam.mx>
2007-04-17 23:54:01 +0000

    Has_many para proyectos y etiquetas para salva.yml

--- trunk/app/controllers/project_controller.rb
+++ trunk/app/controllers/project_controller.rb
@@ -7,8 +7,17 @@ class ProjectController < SalvaController
     @purge_msg = 'La información se ha borrado'
     @per_pages = 10
     @order_by = 'id'
-    @children = { 'projectinstitution' => %w( project_id institution_id ),
-                  'projectfinancingsource' => %w( project_id institution_id amount), 
-                  'projectresearchline' => %w(researchline_id) }
+    @children = { 
+      'projectinstitution' => %w( project_id institution_id ),
+      'projectfinancingsource' => %w( project_id institution_id amount), 
+      'projectresearchline' => %w(project_id researchline_id),
+      'projectresearcharea' => %w(project_id researcharea_id),
+      'projectbook' => %w(project_id book_id),
+      'projectchapterinbook' => %w(project_id chapterinbook_id),
+      'projectconferencetalk' => %w(project_id conferencetalk_id),
+      'projectacadvisit' => %w(project_id acadvisit_id),
+      'projectgenericwork' => %w(project_id genericwork_id),
+      'projectarticle' => %w( project_id article_id ),
+    }
   end
 end
--- trunk/app/models/project.rb
+++ trunk/app/models/project.rb
@@ -1,13 +1,19 @@
 class Project < ActiveRecord::Base
-validates_presence_of :name, :responsible, :descr, :projecttype_id, :projectstatus_id, :startyear
-validates_numericality_of :projecttype_id, :projectstatus_id
-belongs_to :projecttype
-belongs_to :projectstatus
-belongs_to :project
-has_many :projectinstitution
-has_many :projectfinancingsource
-has_many :projectresearchline
-has_many :projectsacadvisit
-has_many :projectsarticle
-has_many :projectsbook
+  validates_presence_of :name, :responsible, :descr, :projecttype_id, :projectstatus_id, :startyear
+  validates_numericality_of :projecttype_id, :projectstatus_id
+
+  belongs_to :projecttype
+  belongs_to :projectstatus
+  belongs_to :project
+  
+  has_many :projectinstitution
+  has_many :projectfinancingsource
+  has_many :projectresearchline
+  has_many :projectresearcharea
+  has_many :projectarticle
+  has_many :projectbook
+  has_many :projectchapterinbook
+  has_many :projectconferencetalk
+  has_many :projectacadvisit
+  has_many :projectgenericwork
 end
