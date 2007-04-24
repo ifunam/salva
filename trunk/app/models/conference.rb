8259555b20f175e94ac3156d7c50cbaf52b78f50
Developer at IFUNAM <dev@fisica.unam.mx>
2007-04-24 23:39:26 +0000

    Unit testing pra Conference*, ver ticket #94, thx gilberto

--- trunk/app/models/conference.rb
+++ trunk/app/models/conference.rb
@@ -1,7 +1,7 @@
 class Conference < ActiveRecord::Base
-  validates_presence_of :name, :year, :conferencetype_id, :country_id
+  validates_presence_of :name, :year, :conferencetype_id, :country_id, :oduser_id
   validates_numericality_of :conferencetype_id, :country_id
-
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
   belongs_to :conferencetype
   belongs_to :country
   belongs_to :conferencescope
--- trunk/app/models/conferencetype.rb
+++ trunk/app/models/conferencetype.rb
@@ -1,3 +1,8 @@
 class Conferencetype < ActiveRecord::Base
+validates_presence_of :id
+validates_numericality_of :id, :allow_nil => true, :only_integer => true
+validates_inclusion_of:id, :in => 1..200
+validates_uniqueness_of :id
 validates_presence_of :name
+validates_uniqueness_of :name
 end
