f681541e532c27968d6dacf6418da6255253f6eb
Developer at IFUNAM <dev@fisica.unam.mx>
2007-04-24 16:26:45 +0000

    Unit testing para Publisher, ver ticket #112, thx miguel

--- trunk/test/unit/publisher_test.rb
+++ trunk/test/unit/publisher_test.rb
@@ -6,7 +6,9 @@ class PublisherTest < Test::Unit::TestCase
   include UnitSimple
 
   def setup
-    @publisher = %w(ak_peters noble)
+    @publisher = %w(ak_peters balkema adasi)
+    @mypublisher = Publisher.new({:name => 'Barks Publications'})
+	
   end
 
   def test_crud 
@@ -20,4 +22,48 @@ class PublisherTest < Test::Unit::TestCase
   def test_collision
     collision_test(@publisher, Publisher)
   end
+ def test_create_with_empty_attributes
+     @publisher = Publisher.new
+     assert !@publisher.save
+   end
+
+   
+   #validates uniqueness
+   def test_uniqueness
+     @publisher = Publisher.new({:name => 'A K Peters Ltd'})
+     @publisher.id = 1
+     assert !@publisher.save
+   end
+
+   
+   # Boundary   #
+   
+   
+   # Checking constraints for name
+   def test_bad_values_for_name
+     @mypublisher.name = nil
+     assert !@mypublisher.valid?
+
+     @mypublisher.name = 'X'
+     assert !@mypublisher.valid?
+
+     @mypublisher.name = 'X' * 201
+     assert !@mypublisher.valid?
+
+     @mypublisher.name = '5' 
+     assert !@mypublisher.valid?
+  end
+   
+   # Checking constraints for ID
+   def test_bad_values_for_id
+     @mypublisher.id = 'xx'
+     assert !@mypublisher.valid?
+
+     @mypublisher.id = nil
+     assert !@mypublisher.valid?
+
+     @mypublisher.id = 3.1416
+     assert !@mypublisher.valid?
+  end
+
 end
