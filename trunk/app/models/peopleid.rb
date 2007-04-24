91efb0a806a9fe40600f48fb2a169418f725f984
Alejandro Aguilar Sierra <algsierra@gmail.com>
2007-04-24 02:31:35 +0000

    un poco de limpieza

--- trunk/app/models/peopleid.rb
+++
@@ -1,9 +0,0 @@
-class Peopleid < ActiveRecord::Base
-  validates_presence_of :descr, :identification_id, :citizen_country_id
-  validates_numericality_of :identification_id, :citizen_country_id
-  belongs_to :identification
-  belongs_to :citizen_country, :class_name => 'Country', :foreign_key => 'citizen_country_id'
-  def name
-  end
-end
-
