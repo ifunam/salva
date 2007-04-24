8f6e77e0973018adf13f7c2de3c2fa62f2271589
Developer at IFUNAM <dev@fisica.unam.mx>
2007-04-24 16:11:36 +0000

    Unit testing para Techproductype, ver ticket #114, thx israel

--- trunk/app/models/techproducttype.rb
+++ trunk/app/models/techproducttype.rb
@@ -1,3 +1,6 @@
 class Techproducttype < ActiveRecord::Base
-validates_presence_of :name
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_presence_of :name, :id
+  validates_uniqueness_of :id, :name
+  validates_length_of :name, :within => 4..50
 end
---
+++ trunk/test/unit/techproducttype_test.rb
@@ -0,0 +1,68 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'techproducttype'
+
+class TechproducttypeTest < Test::Unit::TestCase
+  fixtures :techproducttypes
+  include UnitSimple
+  
+  def setup
+    @techproducttype = %w(videos spots carteles)
+    @mytechproducttype = Techproducttype.new({:name => 'Spots'})
+  end
+
+   #Right - CRUD
+   def test_crud 
+     crud_test(@techproducttype, Techproducttype)
+   end
+
+   def test_validation
+     validate_test(@techproducttype, Techproducttype)
+   end
+
+   def test_collision
+     collision_test(@techproducttype, Techproducttype)
+   end
+
+   def test_create_with_empty_attributes
+     @techproducttype = Techproducttype.new
+     assert !@techproducttype.save
+   end
+
+   def test_check_uniqueness
+     @techproducttype = Techproducttype.new({:name => 'Spots'})
+     assert !@techproducttype.save
+   end
+# boundary
+# Checking constraints for name
+  def test_bad_values_for_name
+     @mytechproducttype.name = nil
+     assert !@mytechproducttype.valid?
+
+     @mytechproducttype.name = 'X'
+     assert !@mytechproducttype.valid?
+
+     @mytechproducttype.name = 'X' * 201
+     assert !@mytechproducttype.valid?
+
+     @mytechproducttype.name = '5' 
+     assert !@mytechproducttype.valid?
+  end
+   
+   # Checking constraints for ID
+   def test_bad_values_for_id
+     
+
+    @mytechproducttype.id = 'xx'
+     assert !@mytechproducttype.valid?
+
+     @mytechproducttype.id = nil
+     assert !@mytechproducttype.valid?
+
+     @mytechproducttype.id = 3.1416
+     assert !@mytechproducttype.valid?
+
+     @mytechproducttype.id = -7
+     assert !@mytechproducttype.valid?
+  end
+
+end
