02ff2c7050c714c10930913ad26d0c11ca7c2b3d
Miguel Vazquez <mvazquez83@gmail.com>
2007-05-08 17:28:18 +0000

    Se realizaron las pruebas de unidad de genericworktype se hizo el cross-checking se creo el fixture asi como se modifico el modelo ver ticket

--- trunk/app/models/genericworktype.rb
+++ trunk/app/models/genericworktype.rb
@@ -1,5 +1,9 @@
 class Genericworktype < ActiveRecord::Base
-validates_presence_of :name, :genericworkgroup_id
-validates_numericality_of :genericworkgroup_id
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :genericworkgroup_id, :only_integer => true
+  validates_length_of :name, :within => 10..500
+  validates_presence_of :name, :genericworkgroup_id
+  validates_uniqueness_of :name, :scope => [:genericworkgroup_id]
+  validates_associated :genericworkgroup, :on => :update
 belongs_to :genericworkgroup
 end
