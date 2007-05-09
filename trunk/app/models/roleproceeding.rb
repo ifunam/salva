553e6b4f55f5db7f81167dc557dedae200cce6ae
Manuel <manuel@geofisica.unam.mx>
2007-05-09 18:42:51 +0000

    Mejoras para las restricciones del id

--- trunk/app/models/roleproceeding.rb
+++ trunk/app/models/roleproceeding.rb
@@ -1,6 +1,5 @@
 class Roleproceeding < ActiveRecord::Base
-  validates_presence_of :id
-  validates_numericality_of :id, :only_integer => true 
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true 
   validates_presence_of :name
   validates_uniqueness_of :name
 end
