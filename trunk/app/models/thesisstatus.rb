3876be687d08de1a447d7798ecba4407ba70bbb6
Miguel Vazquez <mvazquez83@gmail.com>
2007-05-22 18:51:29 +0000

    **Se crearon el thesisstatuses.yml para efectuar pruebas [[br]]**Se modificaron las reglas del modelo thesiststatus [[br]] ** Se creo el archivo thesisstatus_test.rb con las pruebas correspondientes

--- trunk/app/models/thesisstatus.rb
+++ trunk/app/models/thesisstatus.rb
@@ -1,2 +1,7 @@
 class Thesisstatus < ActiveRecord::Base
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_presence_of :name, :id
+  validates_uniqueness_of :id, :name
+  validates_length_of :name, :within => 4..50
+
 end
