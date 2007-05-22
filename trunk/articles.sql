f25a03a5535fec187a8a8a26444242ced3e88108
Alejandro Silva <alexs1010@gmail.com>
2007-05-22 19:13:57 +0000

    Se creo el archivo con datos de prueba yaml, se genero el test de prueba y se modifico el modelo asi como la tabla journals. ver ticket #93

---
+++ trunk/articles.sql
@@ -0,0 +1,288 @@
+----------------------------
+-- Articles and journals --
+----------------------------
+
+CREATE TABLE publicationcategories ( 
+	id SERIAL,
+	name varchar(50) NOT NULL,
+	descr text NULL,
+	moduser_id int4 NULL               	    -- Use it to known who
+            REFERENCES users(id)            -- has inserted, updated or deleted
+            ON UPDATE CASCADE               -- data into or  from this table.
+            DEFERRABLE,
+	created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+	UNIQUE(name)
+);
+COMMENT ON TABLE publicationcategories IS 
+	'Áreas del conocimiento a los que responde una revista:
+	 Genéricos: Ciencias sociales, ciencias naturales, humanidades
+	 Específicos: (desglose :)';
+
+
+CREATE TABLE journals (
+        id SERIAL,
+        name text NOT NULL,
+	issn text NULL,
+        url  text NULL,
+        abbrev text NULL,      
+        other text NULL,           
+	mediatype_id int4 NOT NULL 
+            	REFERENCES mediatypes(id) 
+            	ON UPDATE CASCADE           
+            	DEFERRABLE,
+	publisher_id int4 NULL 
+            	REFERENCES publishers(id) 
+            	ON UPDATE CASCADE           
+            	DEFERRABLE,
+	country_id int4 NOT NULL 
+            	REFERENCES countries(id) 
+            	ON UPDATE CASCADE           
+            	DEFERRABLE,
+	moduser_id int4 NULL               	    -- Use it to known who
+            REFERENCES users(id)            -- has inserted, updated or deleted
+            ON UPDATE CASCADE               -- data into or  from this table.
+            DEFERRABLE,
+	created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY(id),
+        UNIQUE (name,issn,mediatype_id,country_id)
+);
+COMMENT ON TABLE journals IS
+	'Revistas en las cuales pueden publicarse artículos';
+
+
+CREATE TABLE journalstatistics (
+	id SERIAL,
+	journal_id int4 NOT NULL 
+	    REFERENCES journals(id)      
+            ON UPDATE CASCADE
+	    DEFERRABLE,
+	totalcites integer NULL,
+	impactfactor float NULL, 	
+	immediacy   text NULL,
+	dateupdate date NULL, 
+        moduser_id int4 NULL               	    -- Use it to known who
+            REFERENCES users(id)            -- has inserted, updated or deleted
+            ON UPDATE CASCADE               -- data into or  from this table.
+            DEFERRABLE,
+        created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+        updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
+COMMENT ON TABLE journalstatistics IS
+	'Estadísticas asociadas a la revista';
+COMMENT ON COLUMN journalstatistics.dateupdate IS
+	'Fecha en que fue actualizado el factor de impacto/inmediatez';
+
+
+CREATE TABLE journal_publicationcategories ( 
+	id SERIAL,
+	journal_id int4 NOT NULL 
+            REFERENCES journals(id)      
+            ON UPDATE CASCADE
+            DEFERRABLE,
+    	publicationcategory_id int4 NOT NULL 
+            REFERENCES publicationcategories(id)      
+            ON UPDATE CASCADE
+            DEFERRABLE,
+	moduser_id int4 NULL               	    -- Use it to known who
+            REFERENCES users(id)            -- has inserted, updated or deleted
+            ON UPDATE CASCADE               -- data into or  from this table.
+            DEFERRABLE,
+	created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+	UNIQUE(journal_id, publicationcategory_id)
+);
+COMMENT ON TABLE journal_publicationcategories IS
+	'Categorías (áreas del conocimiento) a las que pertenece una revista';
+
+CREATE TABLE roleinjournals (
+	id serial,
+	name text NOT NULL,
+	moduser_id int4 NULL               	    -- Use it to known who
+            REFERENCES users(id)            -- has inserted, updated or deleted
+            ON UPDATE CASCADE               -- data into or  from this table.
+            DEFERRABLE,
+	created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(id),
+	UNIQUE (name)
+);
+COMMENT ON TABLE roleinjournals IS
+	'Roles que un usuario puede tener en una publicación:
+	Editor, Compilador, Revisor, Arbitro, Otro';
+
+
+CREATE TABLE user_journals ( 
+	id SERIAL,
+    	user_id int4 NOT NULL 
+        	REFERENCES users(id)      
+            	ON UPDATE CASCADE
+            	ON DELETE CASCADE   
+            	DEFERRABLE,
+	journal_id int4 NOT NULL 
+        	REFERENCES journals(id)
+            	ON UPDATE CASCADE
+            	DEFERRABLE,
+	roleinjournal_id int4 NOT NULL 
+        	REFERENCES roleinjournals(id)
+            	ON UPDATE CASCADE
+            	DEFERRABLE,
+	startyear int4 NOT NULL,
+	startmonth int4 NULL CHECK (startmonth >= 1 AND startmonth <= 12),
+	endyear   int4 NULL,
+	endmonth int4 NULL CHECK (endmonth >=1 AND endmonth <= 12),
+	other text NULL,
+	moduser_id int4 NULL               	    -- Use it to known who
+            REFERENCES users(id)            -- has inserted, updated or deleted
+            ON UPDATE CASCADE               -- data into or  from this table.
+            DEFERRABLE,
+	created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+	UNIQUE(user_id, journal_id, roleinjournal_id)
+--	CONSTRAINT valid_duration CHECK (endyear IS NULL OR
+--	       (startyear * 12 + coalesce(startmonth,	0)) > (endyear * 12 + coalesce(endmonth,0)))
+);
+COMMENT ON TABLE user_journals IS
+	'Relación entre usuarios del sistema y las publicaciones';
+COMMENT ON COLUMN user_journals.roleinjournal_id IS
+	'Es el rol que tiene el usuario en la publicación';
+
+
+CREATE TABLE articles ( 
+    id SERIAL,
+    title text NOT NULL,
+    journal_id int4 NOT NULL 
+            REFERENCES journals(id)      
+            ON UPDATE CASCADE
+            DEFERRABLE,
+    articlestatus_id int4 NOT NULL  
+            REFERENCES articlestatuses(id)      
+            ON UPDATE CASCADE
+            DEFERRABLE,
+    pages text NULL,   
+    year  int4 NOT NULL,
+    month  int4 NULL CHECK (month >= 1 AND month <= 12),
+    vol text NULL,
+    num text NULL,
+    authors text NOT NULL ,
+    url text NULL,
+    pacsnum text NULL,
+    other text NULL,
+    moduser_id int4 NULL               	    -- Use it to known who
+            REFERENCES users(id)            -- has inserted, updated or deleted
+            ON UPDATE CASCADE               -- data into or  from this table.
+            DEFERRABLE,
+    created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE(title, journal_id, year)
+);
+COMMENT ON TABLE articles IS
+	'Datos de un artículo publicado';
+COMMENT ON COLUMN articles.authors IS
+	'Listado de autores tal cual aparece en el artículo - La relación 
+	entre usuarios y artículos es independiente de esta, ver 
+	authorarticles.';
+
+CREATE TABLE user_articles ( 
+    id SERIAL,
+    user_id int4 NOT NULL 
+            REFERENCES users(id)      
+            ON UPDATE CASCADE
+            ON DELETE CASCADE   
+            DEFERRABLE,
+    article_id int4 NOT NULL 
+            REFERENCES articles(id)
+            ON UPDATE CASCADE
+            DEFERRABLE,
+    ismainauthor BOOLEAN NOT NULL default 't',
+    other text NULL,
+    moduser_id int4 NULL               	    -- Use it to known who
+            REFERENCES users(id)            -- has inserted, updated or deleted
+            ON UPDATE CASCADE               -- data into or  from this table.
+            DEFERRABLE,
+    created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, article_id)
+   );
+COMMENT ON TABLE user_articles IS
+	'Relación entre usuarios del sistema y artículos';
+COMMENT ON COLUMN user_articles.ismainauthor IS
+	'Basta con señalar si el usuario es autor principal o es coautor';
+
+CREATE TABLE articleslog (
+    id SERIAL, 
+    article_id integer NOT NULL 
+            REFERENCES articles(id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+            DEFERRABLE,
+    old_articlestatus_id integer NOT NULL 
+            REFERENCES articlestatuses(id)
+            ON UPDATE CASCADE
+            DEFERRABLE,
+    changedate date NOT NULL default now()::date,
+    moduser_id integer NULL      -- It will be used only to know who has
+            REFERENCES users(id) -- inserted, updated or deleted  
+            ON UPDATE CASCADE    -- data into or from this table.
+            DEFERRABLE,
+    created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+	updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+COMMENT ON TABLE articleslog IS
+	'Estado actual (y bitácora) de un artículo - Cuándo fue enviado, 
+	cuándo fue aceptado, etc.';
+
+CREATE TABLE file_articles (
+   id serial NOT NULL,
+   article_id int4 NOT NULL
+            REFERENCES articles(id)
+            ON UPDATE CASCADE
+            DEFERRABLE,
+   filename text NOT NULL,
+   conten_type text NULL,
+   content bytea NOT NULL,
+   creationtime timestamp NOT NULL DEFAULT now(),
+   lastmodiftime timestamp NOT NULL DEFAULT now(),
+   moduser_id int4 NULL      -- It will be used only to know who has
+            REFERENCES users(id) -- inserted, updated or deleted  
+            ON UPDATE CASCADE    -- data into or from this table.
+            DEFERRABLE,
+    created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_on timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE (article_id, filename)
+);
+COMMENT ON TABLE file_articles IS
+	'Archivos relacionados a los artículos';
+COMMENT ON COLUMN file_articles.article_id IS
+	'ID del artículo referenciado';
+COMMENT ON COLUMN file_articles.content IS
+	'Contenido (binario) del archivo';
+
+
+
+------
+-- Update articleslog if there was a status change
+------
+-- CREATE OR REPLACE FUNCTION articles_update() RETURNS TRIGGER 
+-- SECURITY DEFINER AS '
+-- DECLARE 
+-- BEGIN
+-- 	IF OLD.articlestatus_id = NEW.articlestatus_id THEN
+-- 		RETURN NEW;
+-- 	END IF;
+-- 	INSERT INTO articleslog (articles_id, old_articlestatus_id, moduser_id)
+-- 		VALUES (OLD.id, OLD.articlestatus_id, OLD.moduser_id);
+--         RETURN NEW;
+-- END;
+-- ' LANGUAGE 'plpgsql';
+
+-- CREATE TRIGGER articles_update BEFORE DELETE ON articles
+-- 	FOR EACH ROW EXECUTE PROCEDURE articles_update();
---
+++ trunk/journal_test.rb
@@ -0,0 +1,184 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'country'
+require 'mediatype'
+require 'publisher'
+require 'journal'
+
+class JournalTest < Test::Unit::TestCase
+  fixtures :countries, :mediatypes, :publishers, :journals
+  def setup
+    @journals = %w(quo conozca)
+    @myjournal = Journal.new({:name => 'Todo Ciencia', :country_id => 484, :publisher_id => 2, :mediatype_id => 1})
+  end
+  
+  # Right - CRUD
+  def test_creating_journals_from_yaml
+    @journals.each { | journal|
+      @journal = Journal.find(journals(journal.to_sym).id)
+      assert_kind_of Journal, @journal
+      assert_equal journals(journal.to_sym).id, @journal.id
+      assert_equal journals(journal.to_sym).name, @journal.name
+      assert_equal journals(journal.to_sym).mediatype_id, @journal.mediatype_id
+      assert_equal journals(journal.to_sym).country_id, @journal.country_id
+      assert_equal journals(journal.to_sym).publisher_id, @journal.publisher_id
+    }
+  end
+  
+  def test_updating_journals_name
+      @journals.each { |journal|
+      @journal = Journal.find(journals(journal.to_sym).id)
+      assert_equal journals(journal.to_sym).name, @journal.name
+      @journal.name = @journal.name.chars.reverse 
+      assert @journal.update
+      assert_not_equal journals(journal.to_sym).name, @journal.name
+    }
+  end  
+
+  def test_deleting_journals
+      @journals.each { |journal|
+      @journal = Journal.find(journals(journal.to_sym).id)
+      @journal.destroy
+      assert_raise (ActiveRecord::RecordNotFound) { 
+        Journal.find(journals(journal.to_sym).id) 
+      }
+    }
+  end 
+
+   def test_creating_with_empty_attributes
+     @journal = Journal.new
+     assert !@journal.save
+   end
+
+   def test_creating_duplicated_journal
+     @journal = Journal.new({:name => 'QUO', :mediatype_id => 2, :issn => nil, :country_id => 484})
+     assert !@journal.save
+   end
+
+     # Boundary 
+   def test_bad_values_for_id
+     # Float number for ID 
+     @myjournal.id = 1.6
+     assert !@myjournal.valid?
+   end
+
+   def test_bad_values_for_name
+     @myjournal.name = nil
+     assert !@myjournal.valid?
+
+     @myjournal.name = 'AB' * 800
+     assert !@myjournal.valid?
+   end
+
+   def test_bad_values_for_mediatype_id
+     # Checking constraints for name
+     # Nil name
+     @myjournal.mediatype_id = nil
+     assert !@myjournal.valid?
+
+     @myjournal.mediatype_id = 3.1416
+     assert !@myjournal.valid?
+   end
+
+   #Cross-Checking test
+   def test_cross_checking_for_mediatype_id
+      @journals.each { | journal|
+      @journal = Journal.find(journals(journal.to_sym).id)
+      assert_kind_of Journal, @journal
+      assert_equal @journal.mediatype_id, Mediatype.find(@journal.mediatype_id).id 
+      }
+   end 
+
+   def catch_exception_when_update_invalid_key(record)
+    begin
+       return true if record.update
+     rescue ActiveRecord::StatementInvalid => bang
+       return false
+     end
+   end
+
+   def test_cross_checking_with_bad_values_for_mediatype_id
+    @journals.each { | journal|
+     @journal = Journal.find(journals(journal.to_sym).id)
+     assert_kind_of Journal, @journal
+     @journal.mediatype_id = 10
+     begin 
+            return true if @journal.update
+     rescue StandardError => x
+            return false
+     end
+     }
+    end
+    def test_cross_checking_for_country_id
+      @journals.each { | journal|
+      @journal = Journal.find(journals(journal.to_sym).id)
+      assert_kind_of Journal, @journal
+      assert_equal @journal.country_id, Country.find(@journal.country_id).id 
+     }
+    end 
+
+   def catch_exception_when_update_invalid_key(record)
+    begin
+      return true if record.update
+    rescue ActiveRecord::StatementInvalid => bang
+      return false
+    end
+   end
+
+    def test_cross_checking_with_bad_values_for_country_id
+     @journals.each { | journal|
+     @journal = Journal.find(journals(journal.to_sym).id)
+     assert_kind_of Journal, @journal
+     @journal.country_id = 1000
+     begin 
+            return true if @journal.update
+     rescue StandardError => x
+            return false
+     end
+     }	
+    end
+
+    def test_bad_values_for_country_id
+     # Checking constraints for name
+     # Nil name
+     @myjournal.country_id = nil
+     assert !@myjournal.valid?
+
+     @myjournal.country_id = 3.1416
+     assert !@myjournal.valid?
+    end
+
+    def test_bad_values_for_publisher_id
+     @myjournal.publisher_id = 3.1416
+     assert !@myjournal.valid?
+    end
+
+    #Cross-Checking test
+    def test_cross_checking_for_publisher_id
+     @journals.each { | journal|
+      @journal = Journal.find(journals(journal.to_sym).id)
+      assert_kind_of Journal, @journal
+      assert_equal @journal.publisher_id, Publisher.find(@journal.publisher_id).id 
+      }
+    end 
+
+   def catch_exception_when_update_invalid_key(record)
+     begin
+       return true if record.update
+     rescue ActiveRecord::StatementInvalid => bang
+       return false
+     end
+   end
+
+  def test_cross_checking_with_bad_values_for_publisher_id
+     @journals.each { | journal|
+      @journal = Journal.find(journals(journal.to_sym).id)
+      assert_kind_of Journal, @journal
+      @journal.publisher_id = 10
+      begin 
+            return true if @journal.update
+      rescue StandardError => x
+            return false
+      end
+      }
+  end
+end
