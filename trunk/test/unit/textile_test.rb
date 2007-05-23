5d52c5ba37ecfaba4534e1d4a18fe76b45d4cd6e
Developer at IFUNAM <dev@fisica.unam.mx>
2007-05-23 17:33:04 +0000

    Unit testing para textile y correcciones menores...

--- trunk/test/unit/textile_test.rb
+++ trunk/test/unit/textile_test.rb
@@ -3,15 +3,29 @@ require 'textile'
 
 class TextileTest < Test::Unit::TestCase
   include Textile
-  def  test_should_get_bold_plain_text
+  def  test_should_get_bold_in_plain_text
     assert_equal "hello", bold('hello')
   end
 
-  def  test_should_get_bold_html
+  def  test_should_get_bold_in_html
     assert_equal "<p><strong>hello</strong></p>", bold('hello','html')
-    end
+  end
 
-    def  test_should_get_bold_pdf
-      assert_equal 739.816, bold('hello','pdf')
-    end
+  def  test_should_get_bold_in_pdf
+    assert_equal 739.816, bold('hello','pdf')
+  end
+
+  def  test_should_get_bold_in_pdf
+    assert_equal 'HELLO', header('hello')
+  end
+
+  def  test_should_get_header_in_html
+    (1..3).each {| level|
+      assert_equal "<h#{level}>hello</h#{level}>", header('hello', level, 'html')
+    }
+  end
+
+  def  test_should_get_header_in_pdf
+    assert_equal 739.816, header('hello', 1, 'pdf')
+  end
 end
