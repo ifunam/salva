eebb46c7ba433a043b960e355a534094f5cc5834
Alejandro Silva <alexs1010@gmail.com>
2007-05-24 17:40:43 +0000

    Se crearon las pruebas para el catalogo thesismodalities, asi como sus datos de prueba yaml, y modificaciones al modelo ver ticket #102

--- trunk/app/models/thesismodality.rb
+++ trunk/app/models/thesismodality.rb
@@ -1,2 +1,8 @@
 class Thesismodality < ActiveRecord::Base
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  #validates_inclusion_of :id, :in => 1..999
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates_length_of :name, :within => 3..100
 end
+
---
+++ trunk/test/unit/thesismodality_test.rb
@@ -0,0 +1,74 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'thesismodality'
+
+class ThesismodalitylTest < Test::Unit::TestCase
+  fixtures :thesismodalities
+  def setup
+    @thesismodalities = %w(tesis tesina)
+    @mythesismodality = Thesismodality.new({:name => 'Reporte'})
+  end
+
+  # Right - CRUD
+  def test_creating_from_yaml
+    @thesismodalities.each { | thesismodality|
+      @thesismodality = Thesismodality.find(thesismodalities(thesismodality.to_sym).id)
+      assert_kind_of Thesismodality, @thesismodality
+      assert_equal thesismodalities(thesismodality.to_sym).id, @thesismodality.id
+      assert_equal thesismodalities(thesismodality.to_sym).name, @thesismodality.name
+    }
+  end
+
+  def test_updating_name
+    @thesismodalities.each { |thesismodality|
+      @thesismodality = Thesismodality.find(thesismodalities(thesismodality.to_sym).id)
+      assert_equal thesismodalities(thesismodality.to_sym).name, @thesismodality.name
+      @thesismodality.name = @thesismodality.name.chars.reverse
+      assert @thesismodality.update
+      assert_not_equal thesismodalities(thesismodality.to_sym).name, @thesismodality.name
+    }
+  end 
+
+  def test_deleting
+    @thesismodalities.each { |thesismodality|
+      @thesismodality = Thesismodality.find(thesismodalities(thesismodality.to_sym).id)
+      @thesismodality.destroy
+      assert_raise (ActiveRecord::RecordNotFound) {
+        Thesismodality.find(thesismodalities(thesismodality.to_sym).id)
+      }
+    }
+  end
+
+  def test_uniqueness
+    @thesismodality = Thesismodality.new({:name => 'Tesina'})
+    assert !@thesismodality.save
+  end
+
+  def test_empty_object
+    @thesismodality = Thesismodality.new()
+    assert !@thesismodality.save
+  end
+
+  # Boundaries
+  def test_bad_values_for_id
+    @mythesismodality.id = 'xx'
+    assert !@mythesismodality.valid?
+
+    # Negative number ID
+    #@mythesismodality.id = -1
+    #assert !@mythesismodality.valid?
+
+    # Float number ID
+    @mythesismodality.id = 1.3
+    assert !@mythesismodality.valid?
+  end
+
+  def test_bad_values_for_name
+    # Checking constraints for name
+    # Nil name
+    @mythesismodality = Thesismodality.new
+    @mythesismodality.name = nil
+    assert !@mythesismodality.valid?
+    @mythesismodality.name = 'XX' * 80
+    assert !@mythesismodality.valid?
+  end
+end
