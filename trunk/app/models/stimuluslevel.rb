f804d7389ee41c8c7689998ddd454e3bbf158c22
Miguel Vazquez <mvazquez83@gmail.com>
2007-05-25 17:31:36 +0000

    **Se creo el stimuluslevels.yml para efectuar pruebas [[br]]**Se modificaron las reglas del modelo stimuluslevel [[br]] ** Se creo el archivo stimuluslevel_test.rb con las pruebas correspondientes [[br]] **Se hicieron las pruebas de cross-check con sus respectivas relaciones

--- trunk/app/models/stimuluslevel.rb
+++ trunk/app/models/stimuluslevel.rb
@@ -1,7 +1,10 @@
 class Stimuluslevel < ActiveRecord::Base
 
-  validates_presence_of :name, :stimulustype_id
-  validates_numericality_of :stimulustype_id
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_presence_of :name, :id
+  validates_uniqueness_of :id, :name
+  validates_length_of :name, :within => 1..3
+  validates_associated :stimulustype, :on => :update
 
   belongs_to :stimulustype
 end
