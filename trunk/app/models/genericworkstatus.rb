0e3221541b3ad4b875d4fa1d2a3643a4ce808bd5
Miguel Vazquez <mvazquez83@gmail.com>
2007-05-25 18:02:33 +0000

    **Se creo el genericworkstatuses.yml para efectuar pruebas [[br]]**Se modificaron las reglas del modelo genericworkstatus [[br]] ** Se creo el archivo genericworkstatus_test.rb con las pruebas correspondientes

--- trunk/app/models/genericworkstatus.rb
+++ trunk/app/models/genericworkstatus.rb
@@ -1,3 +1,5 @@
 class Genericworkstatus < ActiveRecord::Base
-validates_presence_of :name
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_presence_of :name, :id
+  validates_uniqueness_of :name
 end
