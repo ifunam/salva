0057fe3fe71481e86dc2072ef915f83b021c97a2
Manuel <manuel@geofisica.unam.mx>
2007-05-28 18:33:23 +0000

    Pruebas a la tabla researchline

--- trunk/app/models/researchline.rb
+++ trunk/app/models/researchline.rb
@@ -1,5 +1,8 @@
 class Researchline < ActiveRecord::Base
 validates_presence_of :name
-validates_numericality_of :researcharea_id
+validates_uniqueness_of :name
+validates_length_of :name, :within => 1..500
+validates_numericality_of :id, :allow_nil => true, :only_integer => true
+validates_numericality_of :researcharea_id, :allow_nil => true, :only_integer => true
 belongs_to :researcharea
 end
