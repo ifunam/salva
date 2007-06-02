0b73f95273014a55be1d1cb7c36c239b31942418
Developer at IFUNAM <dev@fisica.unam.mx>
2007-06-02 19:57:30 +0000

    Correcciones para las pruebas relacionadas a activity* y sus fixtures. Queda pendiente corregir el comportamiento de activitygroup

--- trunk/test/unit/activitygroup_test.rb
+++ trunk/test/unit/activitygroup_test.rb
@@ -4,59 +4,63 @@ require 'activitygroup'
 class ActivitygroupTest < Test::Unit::TestCase
   fixtures :activitygroups
   include UnitSimple
-  
+
   def setup
-   @activitygroup = %w(actividades_de_divulgacion actividades_de_docencia actividades_de_extension)
-   @myactivitygroup = Activitygroup.new({:name => 'Otras actividades'})
+    @groups = %w(actividades_de_divulgacion actividades_de_docencia actividades_de_extension)
+    @myactivitygroup = Activitygroup.new({:name => 'Otras actividades'})
   end
 
   #Right - CRUD
-  def test_crud 
-    crud_test(@activitygroup, Activitygroup)
+  def test_crud
+    crud_test(@groups, Activitygroup)
   end
-  
+
   def test_validation
-    validate_test(@activitygroup, Activitygroup)
+    validate_test(@groups, Activitygroup)
   end
-  
+
   def test_collision
-    collision_test(@activitygroup, Activitygroup)
+    collision_test(@groups, Activitygroup)
   end
-  
+
   def test_create_with_empty_attributes
-     @activitygroup = Activitygroup.new
-    assert !@activitygroup.save
+     @group = Activitygroup.new
+    assert !@group.save
   end
-  
+
   def test_check_uniqueness
-    @activitygroup = Activitygroup.new({:name => 'Actividades de divulgación'})
-    @activitygroup.id= 2
-    assert !@activitygroup.save
-  end
-  # boundary
+    @group = Activitygroup.new({:name => 'Actividades de divulgación'})
+    assert !@group.valid?
+
+    @group = Activitygroup.find(1)
+    @group.id = 2
+    assert !@group.valid?
+ end
+
+  # Boundary tests
   # Checking constraints for name
   def test_bad_values_for_name
     @myactivitygroup.name = nil
     assert !@myactivitygroup.valid?
-    
+
     @myactivitygroup.name = 'X'
     assert !@myactivitygroup.valid?
 
     @myactivitygroup.name = 'X' * 201
     assert !@myactivitygroup.valid?
 
-    @myactivitygroup.name = '5' 
+    @myactivitygroup.name = '5'
     assert !@myactivitygroup.valid?
   end
-  
+
   # Checking constraints for ID
   def test_bad_values_for_id
     @myactivitygroup.id = 'xx'
     assert !@myactivitygroup.valid?
-     
+
      @myactivitygroup.id = nil
-    assert !@myactivitygroup.valid?
-    
+    assert @myactivitygroup.valid?
+
     @myactivitygroup.id = 3.1416
     assert !@myactivitygroup.valid?
     # @myactivitygroup.id = -1
