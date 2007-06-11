59d08b921673d7b914196e3938aee6c67b21f664
Developer at IFUNAM <dev@fisica.unam.mx>
2007-06-11 19:21:36 +0000

    Correcciones en los defectos relacionados a validaciones no necesarias en las llaves primarias

--- trunk/app/models/genericworkgroup.rb
+++ trunk/app/models/genericworkgroup.rb
@@ -1,8 +1,5 @@
 class Genericworkgroup < ActiveRecord::Base
-validates_presence_of :id
-validates_numericality_of :id, :only_integer => true 
-validates_inclusion_of:id, :in => 1..100 
-validates_uniqueness_of :id
-validates_presence_of :name
-validates_uniqueness_of :name
+  validates_numericality_of :id, :only_integer => true , :allow_nil => true
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
