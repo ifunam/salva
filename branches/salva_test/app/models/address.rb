19a90c05dd0fce8835c2dc0b8a124bd1ba75e4f2
Miguel Vazquez <mvazquez83@gmail.com>
2007-06-12 18:25:14 +0000

    Se creo la prueba unitaria de address_type_test y se modifico su modelo ya que al estar haciendo su prueba de controladores se detecto un problema en sus validaciones de hecho no hacia ninguna validacion aqui esta la prueba con sus cambios en modelos y fixtures

--- branches/salva_test/app/models/address.rb
+++ branches/salva_test/app/models/address.rb
@@ -1,7 +1,10 @@
 class Address < ActiveRecord::Base
+
+
   validates_presence_of :country_id, :location
   validates_numericality_of :country_id, :addresstype_id
   validates_inclusion_of :is_postaddress, :in=> [true, false]
+  validates_uniqueness_of :addresstype_id
   belongs_to :country
   belongs_to :addresstype
   belongs_to :city
--- branches/salva_test/app/models/addresstype.rb
+++ branches/salva_test/app/models/addresstype.rb
@@ -1,2 +1,6 @@
 class Addresstype < ActiveRecord::Base
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates_length_of :name, :within => 2..30
 end
