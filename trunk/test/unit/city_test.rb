b38b33a27dfe2cc82992d90e2d69d33c3f2b2993
Ines Fernandez <inesfernan@gmail.com>
2007-06-20 22:16:44 +0000

    Se standarizaron las pruebas y los modelos ver tickets #84, #86, #87, #88, #129, #105

--- trunk/test/unit/city_test.rb
+++ trunk/test/unit/city_test.rb
@@ -1,61 +1,112 @@
 require File.dirname(__FILE__) + '/../test_helper'
-require 'country'
 require 'state'
 require 'city'
 class CityTest < Test::Unit::TestCase
-  fixtures :countries, :states, :cities
+  fixtures  :states, :cities
   include UnitSimple
-  
+
   def setup
-    @city = %w(culiacan monterrey)
+    @cities = %w(culiacan monterrey)
+    @mycity = City.new({:name => 'Ciudad de México', :state_id => 9})
   end
 
-  def test_crud 
-    crud_test(@city, City)
+  def test_creating_records_from_yaml
+    @cities.each { | city|
+      @city = City.find(cities(city.to_sym).id)
+      assert_kind_of City, @city
+      assert_equal cities(city.to_sym).id, @city.id
+      assert_equal cities(city.to_sym).name, @city.name
+      assert_equal cities(city.to_sym).state_id, @city.state_id
+    }
   end
 
-  def test_validation
-    validate_test(@city, City)
+  def test_updating_name
+    @cities.each { |city|
+      @city = City.find(cities(city.to_sym).id)
+      assert_equal cities(city.to_sym).name, @city.name
+      @city.name = @city.name.chars.reverse
+      assert @city.update
+      assert_not_equal cities(city.to_sym).name, @city.name
+    }
   end
 
-  def test_collision
-    collision_test(@city, City)
+  def test_deleting
+    @cities.each { |city|
+      @city = City.find(cities(city.to_sym).id)
+
+      @city.destroy
+      assert_raise (ActiveRecord::RecordNotFound) {
+        City.find(cities(city.to_sym).id)
+      }
+    }
   end
 
-   def test_validate_states_with_bad_values
+  def test_uniqueness
+    @city = City.new({:name => 'Culiacán', :state_id => 25 })
+    assert !@city.save
+  end
 
-     # Checking constraints for ID
-     @city = City.new({:name => 'Los mochis', :state_id => 19})
+  def test_empty_object
+    @city = City.new()
+    assert !@city.save
+  end
 
-     # Non numeric ID 
-     @city.state_id = 'xx'
-     assert !@city.valid?
+  # Boundary
+  def test_bad_values_for_id
+    # Float number for ID
+    @mycity.id = 1.6
+    assert !@mycity.valid?
+    @mycity.id = 'mi_id'
+    assert !@mycity.valid?
+  end
 
-     # Nil ID 
-     @city.state_id = nil
-     assert !@city.valid?
+  def test_bad_values_for_name
+    @mycity.name = nil
+    assert !@mycity.valid?
+  end
 
-     # Negative number ID 
-     @city.state_id = -1
-     assert !@city.valid?
+  def test_bad_values_for_state_id
+    @mycity.state_id = nil
+    assert !@mycity.valid?
+    @mycity.state_id= 1.6
+    assert !@mycity.valid?
+    @mycity.state_id = 'mi_id'
+    assert !@mycity.valid?
 
-     # Too long number ID 
-     @city.state_id = 10001
-     assert !@city.valid?
+    # Negative number ID
+    #@city.state_id = -1
+    #assert !@city.valid?
+  end
 
-     @city.id = 999
-     # Checking constraints for name
-     # Nil name
-     @city.name = nil
-     assert !@city.valid?
-     @city.name = 'Chiapas'
-   end
+  #cross check for state
+  def test_cross_checking_for_state_id
+    @cities.each { | city|
+      @city = City.find(cities(city.to_sym).id)
+      assert_kind_of City, @city
+      assert_equal @city.state_id, State.find(@city.state_id).id
+    }
+  end
 
-   def test_check_uniqueness
-     @city = City.new({:name => 'Los mochis', :state_id => 19})
-     assert @city.save
+  def catch_exception_when_update_invalid_key(record)
+    begin
+      return true if record.update
+    rescue ActiveRecord::StatementInvalid => bang
+      return false
+    end
+  end
 
-     @city2 = City.new({:name => 'Los mochis', :state_id => 19})
-     assert !@city2.save
-   end
+  def test_cross_checking_with_bad_values_for_state_id
+    @cities.each { | city|
+      @city = City.find(cities(city.to_sym).id)
+      assert_kind_of City, @city
+      @city.state_id = 20
+
+      begin
+        return true if @state.update
+      rescue StandardError => x
+        return false
+      end
+    }
+  end
+
 end
