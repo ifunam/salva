486fdb18d001b901487116115279bdb0c519d8c4
Developer at IFUNAM <dev@fisica.unam.mx>
2007-06-20 21:59:03 +0000

    Se mejoran los métodos select_simple y select_condition (queda pendiente refactorar)

--- trunk/app/helpers/select_helper.rb
+++ trunk/app/helpers/select_helper.rb
@@ -2,7 +2,7 @@ require 'finder'
 require 'list_helper'
 require 'application_helper'
 require 'labels'
-module SelectHelper   
+module SelectHelper
   include Labels
 
   def foreignize(model, prefix=nil)
@@ -10,13 +10,14 @@ module SelectHelper
     foreignkey = prefix + '_' + foreignkey unless prefix.nil?
     foreignkey
   end
-  
+
   def simple_select(object, model, tabindex, options={})
     name = (model.column_names.include? 'title') ? 'title': 'name'
     field = foreignize(model,options[:prefix])
     selected = (@edit.nil?) ? options[:selected] : options[:selected] || @edit.send(field)
-    @list = Finder.new(model, [ name ], :all, :order => name+' ASC')
-    select(object, field, @list.as_pair, {:prompt => '-- Seleccionar --', :selected => selected}, {:tabindex => tabindex})
+    @list = Finder.new(model, [ name ], :all, :order => name+' ASC').as_pair
+    @list = @list + (Finder.new(model, [ name ], :first, :conditions => "id = #{selected}").as_pair) if !selected.nil? && @list.rassoc(selected).nil?
+    select(object, field, @list, {:prompt => '-- Seleccionar --', :selected => selected}, {:tabindex => tabindex})
   end
 
   def select_conditions(object, model, tabindex, options={})
@@ -24,10 +25,11 @@ module SelectHelper
     attributes = options[:attributes] || %w(name)
     selected = (@edit.nil?) ? options[:selected] : options[:selected] || @edit.send(field)
     [:attributes, :selected, :field].each { |key| options.delete(key) }
-    @list = Finder.new(model, attributes, :all, options)
-    select(object, field, @list.as_pair, {:prompt => '-- Seleccionar --', :selected => selected}, {:tabindex => tabindex})
+    @list = Finder.new(model, attributes, :all, options).as_pair
+    @list = @list + (Finder.new(model, attributes, :first, :conditions => "id = #{selected}").as_pair) if !selected.nil? && @list.rassoc(selected).nil?
+    select(object, field, @list, {:prompt => '-- Seleccionar --', :selected => selected}, {:tabindex => tabindex})
   end
-  
+
   def observable_select(partial, id, tabindex)
     render :partial => "salva/#{partial}", :locals => { :id => id, :tabindex => tabindex }
   end
@@ -38,10 +40,10 @@ module SelectHelper
     model_id = prefix + '_' + model_id if prefix !=nil
     select(object, model_id, sorted_find(model), {:prompt => '-- Seleccionar --'}, options)
   end
-  
+
   def select_to_update_select(obj, model, model_dest, tabindex, validation_type=nil, attribute='name')
     options = set_options_tags(tabindex, validation_type)
-    options[:onchange] = remote_functag(model, model_dest, tabindex) 
+    options[:onchange] = remote_functag(model, model_dest, tabindex)
     list = []
     if attribute.is_a? Array
       collection = model.find(:all)
@@ -51,31 +53,31 @@ module SelectHelper
     end
     select(obj, set_model_id(model),  list, { :prompt => '-- Seleccionar --' }, options)
   end
-  
-  def select_id_to_update_select(obj, model, model_parent, id, model_dest, tabindex, validation_type=nil, columns=['name']) 
+
+  def select_id_to_update_select(obj, model, model_parent, id, model_dest, tabindex, validation_type=nil, columns=['name'])
     options = set_options_tags(tabindex, validation_type)
     conditions = [ set_model_id(model_parent) + ' = ?', id ]
     collection = model.find(:all, :conditions => conditions)
-    list = list_collection(collection,  columns) 
+    list = list_collection(collection,  columns)
     options[:onchange] = remote_functag(model, model_dest, tabindex) if model_dest != nil
     select(obj, set_model_id(model), list, {:prompt => '-- Seleccionar --'}, options)
   end
-  
+
   def select_by_attribute(object, model, tabindex, validation_type=nil, model_id=nil, attribute='name')
     options = set_options_tags(tabindex, validation_type)
     attribute_id = model_id != nil ? model_id : set_model_id(model)
     select(object, attribute_id, sorted_find(model, attribute), {:prompt => '-- Seleccionar --'}, options)
   end
-  
+
   def select_as_tree(object, model, columns, tabindex, validation_type=nil, conditions=nil)
     options = set_options_tags(tabindex, validation_type)
     collection = model.find(:all, conditions)
     list = list_collection(collection, columns)
-    fieldname = set_model_id(model) 
+    fieldname = set_model_id(model)
     fieldname = 'parent_id' if columns.include? 'parent_id'
     select(object, fieldname, list, {:prompt => '-- Seleccionar --'}, options)
   end
-  
+
   def select_adscription(object, model, tabindex, validation_type=nil)
     options = set_options_tags(tabindex, validation_type)
     institution = get_myinstitution
@@ -101,7 +103,7 @@ module SelectHelper
 
   def selects_to_update_select(obj, models, model_dest, tabindex, validation_type=nil, attribute='name')
     options = set_options_tags(tabindex, validation_type)
-    options[:onchange] = remote_functag(model, model_dest, tabindex) 
+    options[:onchange] = remote_functag(model, model_dest, tabindex)
     list = []
     if attribute.is_a? Array
       collection = model.find(:all)
@@ -115,14 +117,14 @@ module SelectHelper
   private
   def set_options_tags(tabindex, validation_type=nil)
     options = Hash.new
-    options[:tabindex] = tabindex 
+    options[:tabindex] = tabindex
     options
   end
 
 
   def remote_functag(origmodels, destmodel, tabindex, prefix=nil)
     origname = (origmodels.is_a? Array) ? origmodels.join('_').downcase: origmodels.name.downcase
-    partial = "select_#{origname}_#{destmodel.name.downcase}" 
+    partial = "select_#{origname}_#{destmodel.name.downcase}"
     params = "'partial=#{partial}&tabindex=#{tabindex}&id='+value"
     partial_note =  partial + "_note"
     success_msg = "Effect.BlindUp('#{partial_note}', {duration: 0.5});; "
@@ -134,9 +136,9 @@ module SelectHelper
 
    def remote_functag2(origmodels, destmodel, tabindex, prefix=nil)
      origname = origmodels.shift
-     partial = "select_#{origname.name.downcase}_#{destmodel.name.downcase}" 
+     partial = "select_#{origname.name.downcase}_#{destmodel.name.downcase}"
      params = "'partial=#{partial}&tabindex=#{tabindex}&id='+value"
-     origmodels.each { | model | 
+     origmodels.each { | model |
        model_id = set_model_id(model)
        params << "+'&#{model_id}='+$F('edit_#{model_id}')"
      }
@@ -147,12 +149,12 @@ module SelectHelper
      remote_function(:update => partial, :with => params, :url => {:action => :update_select_from_selects},
                      :loading => loading_msg, :success => success_msg)
    end
-  
+
   def select_institutioncareer_by_degree(obj, model, columns, id, tabindex, validation_type=nil)
     options = set_options_tags(tabindex, validation_type)
     collection = model.find_by_sql "SELECT ic.id, ic.career_id, ic.institution_id FROM institutioncareers ic, careers c WHERE c.degree_id = #{id} AND c.id = ic.career_id"
-    list = list_collection(collection,  columns) 
+    list = list_collection(collection,  columns)
     select(obj, set_model_id(model), list, {:prompt => '-- Seleccionar --'})
   end
 
-end  
+end
