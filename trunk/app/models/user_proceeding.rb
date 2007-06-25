3fba3557d35f13f8aae452dab1a612a798dd85e4
Developer at IFUNAM <dev@fisica.unam.mx>
2007-06-25 18:08:08 +0000

    Resolvemos temporalmente los problemas con composed keys y group, en su lugar usamos joins

--- trunk/app/models/user_proceeding.rb
+++ trunk/app/models/user_proceeding.rb
@@ -1,9 +1,11 @@
 class UserProceeding < ModelComposedKeys
   set_table_name "user_proceedings"
+
   set_primary_keys :user_id, :proceeding_id
   validates_presence_of :proceeding_id, :roleproceeding_id
   validates_numericality_of :proceeding_id, :roleproceeding_id
-  validates_uniqueness_of :user_id, :scope => [:proceeding_id, :roleproceeding_id], :message => 'El rol del usuario esta duplicado' 
+  validates_uniqueness_of :user_id, :scope => [:proceeding_id, :roleproceeding_id], :message => 'El rol del usuario esta duplicado'
+
   belongs_to :proceeding
   belongs_to :roleproceeding
 end
