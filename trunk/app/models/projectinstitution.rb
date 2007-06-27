b384191fcd60481423ee84ea2a804114b53580e9
Alejandro Aguilar Sierra <algsierra@gmail.com>
2007-06-27 21:14:49 +0000

    Material para el CCA y correccion menor

--- trunk/app/models/projectinstitution.rb
+++ trunk/app/models/projectinstitution.rb
@@ -1,4 +1,5 @@
 class Projectinstitution < ActiveRecord::Base
+attr_accessor :country_id
 validates_presence_of :project_id, :institution_id
 validates_numericality_of :project_id, :institution_id
 belongs_to :project
