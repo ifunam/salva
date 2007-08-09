a2ea453801915e3a1d3f76b5d0dc5ccde32e9b94
Developer at IFUNAM <dev@fisica.unam.mx>
2007-08-09 15:06:24 +0000

    Cambios menores para country y regresamos algunos cambios previos a la calas model_composedkeys

--- trunk/app/models/model_composed_keys.rb
+++ trunk/app/models/model_composed_keys.rb
@@ -1,19 +1,19 @@
 require 'sql'
 class ModelComposedKeys < ActiveRecord::Base
-  
+
   # Class Methods
   class << self
     include Sql
 
     def set_primary_keys(*keys)
-      cattr_accessor :primary_keys, :reserved_attributes 
+      cattr_accessor :primary_keys, :reserved_attributes
       self.primary_keys = keys.collect {|key| key.to_s}
-      self.reserved_attributes = %w(created_on updated_on)
+      self.reserved_attributes = %w(created_on updated_on moduser_id)
     end
 
     def find(*args)
       options = extract_options_from_args!(args)
-      case args.first      
+      case args.first
       when :first         then find_first_composed_keys(options)
       when :first_nogroup then first_nogroup_composed_keys(options)
       when :all           then find_every_composed_keys(options)
@@ -24,7 +24,7 @@ class ModelComposedKeys < ActiveRecord::Base
 
     private
     def find_composed_keys(ids)
-      set_ids(ids) 
+      set_ids(ids)
       conditions = set_conditions(self.primary_keys,ids.split(':'))
       record = find_initial({:conditions => conditions})
       attributes = (record.attribute_names - self.reserved_attributes) - self.primary_keys
@@ -36,7 +36,7 @@ class ModelComposedKeys < ActiveRecord::Base
 
     def find_every_composed_keys(options)
       grouped_records = find_every({:select => self.primary_keys.join(','), :group => self.primary_keys.join(','),
-                                     :conditions => options[:conditions], :include => options[:include], 
+                                     :conditions => options[:conditions], :include => options[:include],
                                      :joins => options[:joins] })
       grouped_records.collect { |grouped_record| find_composed_keys(get_ids_from_record(grouped_record)) }
     end
@@ -58,7 +58,7 @@ class ModelComposedKeys < ActiveRecord::Base
     def every_nogroup_composed_keys(options)
       find_every({:conditions => options[:conditions], :include => options[:include] })
     end
-    
+
     def set_ids(ids)
       cattr_accessor :ids
       self.ids = ids
@@ -81,16 +81,16 @@ class ModelComposedKeys < ActiveRecord::Base
   include Sql
 
   def destroy
-    conditions = set_conditions(self.primary_keys,self.ids.split(':')) 
+    conditions = set_conditions(self.primary_keys,self.ids.split(':'))
     self.class.find(:all_nogroup, :conditions => conditions).each { |object|
       self.class.delete(object.attributes['id'])
     }
   end
 
-  def save 
+  def save
     (self.public_methods.include? 'ids') ? my_update : my_save
   end
-  
+
   private
 
   def my_save
@@ -100,33 +100,47 @@ class ModelComposedKeys < ActiveRecord::Base
     models.each { |model|  model.create }
     return true
   end
-  
+
   def my_update
     attributes  = get_uncomposed_attributes
-    array_keys  = get_array_keys(attributes) 
+    array_keys  = get_array_keys(attributes)
+
+    # Here is missing a better way to destroy an unselected records,
+    # and Yes, I know, It is a very ugly hack but It works!
+    record = self.class.find(self.class.ids)
+    saved = {}
+    array_keys.each { |k| saved[k] = record.attributes_before_type_cast[k] }
+    unselected = {}
+    saved.keys.each { |k|  unselected[k] = saved[k] - attributes[k]  if saved[k].compact.size > 0 }
+    (0..get_maxsize_from_array_attributes(unselected)).each { |i|
+      conditions = self.class.set_conditions(self.class.primary_keys + unselected.keys, self.class.ids.split(':') + unselected.keys.collect { |key| unselected[key][i] })
+      record = self.class.find(:first, :conditions => conditions)
+      record.destroy unless record.nil?
+    }
+
     (0..get_maxsize_from_array_attributes(attributes)).each { |i|
       conditions = set_conditions_for_find_first_nogroup(attributes,array_keys,i)
       model = self.class.find(:first_nogroup, :conditions => conditions)
       my_create_or_update(attributes,i,model)
     }
-    # Here is missing the destroy method to delete the unselected records.
+
     return true
   end
-  
+
   def my_create_or_update(attributes, i, model=nil)
     if model != nil
       self.attributes = fill_model_to_update(attributes, i)
       model.update
     else
-      new_model = fill_model(attributes, i) 
+      new_model = fill_model(attributes, i)
       new_model.create
     end
   end
 
   def get_uncomposed_attributes
     keys = (self.attribute_names - self.reserved_attributes) - self.primary_keys
-    keys.inject({}) do |params, key|  
-      params[key] = self.attributes_before_type_cast[key] 
+    keys.inject({}) do |params, key|
+      params[key] = self.attributes_before_type_cast[key]
       params
     end
   end
@@ -134,16 +148,16 @@ class ModelComposedKeys < ActiveRecord::Base
   def fill_models(attributes)
     (0..get_maxsize_from_array_attributes(attributes)).collect { |i| fill_model(attributes,i) }
   end
-  
-  def get_maxsize_from_array_attributes(attributes)
-    array = attributes.values.collect! { |value| value.size if value.is_a?(Array) }.compact
+
+  def get_maxsize_from_array_attributes(myattributes)
+    array = myattributes.values.collect! { |value| value.size if value.is_a?(Array) }.compact
     array.empty? ? 0 : array.sort.last - 1
-  end 
-  
+  end
+
   def fill_model(uattributes, i)
     model = self.class.new
     self.primary_keys.each { |key| model.[]=(key, self.attributes[key]) }
-    uattributes.keys.each { |key| 
+    uattributes.keys.each { |key|
       next if key == 'id'
       (uattributes[key].is_a? Array) ? (model.[]=(key, uattributes[key][i])) : (model.[]=(key, uattributes[key]))
     }
@@ -151,22 +165,22 @@ class ModelComposedKeys < ActiveRecord::Base
   end
 
   def fill_model_to_update(attributes, i)
-    attributes.keys.inject({}) do |params, key|  
+    attributes.keys.inject({}) do |params, key|
       (attributes[key].is_a? Array) ? (params[key] = attributes[key][i]) : (params[key] = attributes[key])
       params
     end
   end
-  
+
   def are_valid_models?(models)
-    models.each { |model| 
-      unless model.valid? 
+    models.each { |model|
+      unless model.valid?
         self.errors.add_to_base(model.errors.full_messages)
-        return false 
+        return false
       end
       }
     return true
   end
-  
+
   def get_array_keys(attributes)
     attributes.keys.collect! { |key| key if attributes[key].is_a? Array }.compact!
   end
@@ -174,7 +188,7 @@ class ModelComposedKeys < ActiveRecord::Base
   def set_conditions_for_find_first_nogroup(attributes, array_keys, i)
     keys = self.primary_keys + array_keys
     ids = self.ids.split(':')
-    set_conditions(keys, ids) + array_keys.collect { |key| attributes[key][i] } 
+    set_conditions(keys, ids) + array_keys.collect { |key| attributes[key][i] }
   end
 end
 
