96400c68c8c5bb0c0db751853ee9f5e20450cfe5
Developer at IFUNAM <dev@fisica.unam.mx>
2007-08-10 15:23:39 +0000

    * Limpieza general: Se cambio el método table_select por simple_select y se eliminaron algunos parciales que no usamos.  * Se eliminaron los métodos  select_to_update_select, select_id_to_update_select,  select_by_attribute, remote_functag y remote_functag2 del helper select.  * Se actualizaron las pruebas unitarias del select_helper.  * Cambios menores.

--- trunk/app/helpers/select_helper.rb
+++ trunk/app/helpers/select_helper.rb
@@ -10,8 +10,9 @@ module SelectHelper
     (prefix != nil) ? prefix + '_' +  Inflector.foreign_key(model) : Inflector.foreign_key(model)
   end
 
-  def selectizedid(object, field, selected=nil, filter={})
-    if filter.is_a? Hash and filter.has_key?(field) and !filter[field].nil? # Default value from filter has priority over defined state_id or selected option
+  def selectize_id(object, field, selected=nil, filter={})
+    # Default value from filter has priority over defined state_id or selected option
+    if filter.is_a? Hash and filter.has_key?(field) and !filter[field].nil?
       filter[field]
     elsif !object.nil? and object.respond_to? field and !object.send(field).nil?
       object.send(field)
@@ -20,66 +21,31 @@ module SelectHelper
     end
   end
 
-  def finder_by_id(model, attributes, id)
+  def finder_id(model, attributes, id)
     Finder.new(model, attributes, :first, :conditions => "id = #{id}").as_pair
   end
 
   def simple_select(object, model, tabindex, options={})
-    column = (model.column_names.include? 'title') ? 'title' : 'name'
+    column = options[:attribute] || ((model.column_names.include? 'title') ? 'title' : 'name')
     field = foreignize(model,options[:prefix])
-    selected = selectizedid(@edit, field, options[:selected], @filter)
-    @list = Finder.new(model, [ 'name' ], :all, :order => 'name ASC').as_pair
-    @list = @list + finder_by_id(model, [ column ], selected) if !selected.nil? && @list.rassoc(selected).nil?
+    selected = selectize_id(@edit, field, options[:selected], @filter)
+    @list = Finder.new(model, [ column ], :all, :order => "#{column} ASC").as_pair
+    @list = @list + finder_id(model, [ column ], selected) if !selected.nil? && @list.rassoc(selected).nil?
     select(object, field, @list, {:prompt => '-- Seleccionar --', :selected => selected},  { :tabindex => tabindex})
   end
 
   def select_conditions(object, model, tabindex, options={})
     field = options[:field] || foreignize(model)
     attributes = options[:attributes] || %w(name)
-    selected = selectizedid(@edit, field, options[:selected], @filter)
+    selected = selectize_id(@edit, field, options[:selected], @filter)
      [:attributes, :selected, :field].each { |key| options.delete(key) }
     @list = Finder.new(model, attributes, :all, options).as_pair
-    @list = @list + finder_by_id(model, attributes, selected) if !selected.nil? && @list.rassoc(selected).nil?
+    @list = @list + finder_id(model, attributes, selected) if !selected.nil? && @list.rassoc(selected).nil?
     select(object, field, @list, {:prompt => '-- Seleccionar --', :selected => selected}, {:tabindex => tabindex})
   end
 
   def observable_select(partial, id, tabindex)
     render :partial => "salva/#{partial}", :locals => { :id => id, :tabindex => tabindex }
-  end
-
-  def table_select(object, model, tabindex, validation_type=nil, prefix=nil)
-    model_id = set_model_id(model)
-    options = set_options_tags(tabindex, validation_type)
-    model_id = prefix + '_' + model_id if prefix !=nil
-    select(object, model_id, sorted_find(model), {:prompt => '-- Seleccionar --'}, options)
-  end
-
-  def select_to_update_select(obj, model, model_dest, tabindex, validation_type=nil, attribute='name')
-    options = set_options_tags(tabindex, validation_type)
-    options[:onchange] = remote_functag(model, model_dest, tabindex)
-    list = []
-    if attribute.is_a? Array
-      collection = model.find(:all)
-      list = list_collection(collection, attribute)
-    else
-      list = sorted_find(model, attribute)
-    end
-    select(obj, set_model_id(model),  list, { :prompt => '-- Seleccionar --' }, options)
-  end
-
-  def select_id_to_update_select(obj, model, model_parent, id, model_dest, tabindex, validation_type=nil, columns=['name'])
-    options = set_options_tags(tabindex, validation_type)
-    conditions = [ set_model_id(model_parent) + ' = ?', id ]
-    collection = model.find(:all, :conditions => conditions)
-    list = list_collection(collection,  columns)
-    options[:onchange] = remote_functag(model, model_dest, tabindex) if model_dest != nil
-    select(obj, set_model_id(model), list, {:prompt => '-- Seleccionar --'}, options)
-  end
-
-  def select_by_attribute(object, model, tabindex, validation_type=nil, model_id=nil, attribute='name')
-    options = set_options_tags(tabindex, validation_type)
-    attribute_id = model_id != nil ? model_id : set_model_id(model)
-    select(object, attribute_id, sorted_find(model, attribute), {:prompt => '-- Seleccionar --'}, options)
   end
 
   def select_as_tree(object, model, columns, tabindex, validation_type=nil, conditions=nil)
@@ -114,60 +80,9 @@ module SelectHelper
     select(object, set_model_id(model), list, {:prompt => '-- Seleccionar --'}, options)
   end
 
-  def selects_to_update_select(obj, models, model_dest, tabindex, validation_type=nil, attribute='name')
-    options = set_options_tags(tabindex, validation_type)
-    options[:onchange] = remote_functag(model, model_dest, tabindex)
-    list = []
-    if attribute.is_a? Array
-      collection = model.find(:all)
-      list = list_collection(collection, attribute)
-    else
-      list = sorted_find(model, 'name')
-    end
-    select(obj, set_model_id(model),  list, { :prompt => '-- Seleccionar --' }, options)
-  end
-
-  private
   def set_options_tags(tabindex, validation_type=nil)
     options = Hash.new
     options[:tabindex] = tabindex
     options
   end
-
-
-  def remote_functag(origmodels, destmodel, tabindex, prefix=nil)
-    origname = (origmodels.is_a? Array) ? origmodels.join('_').downcase: origmodels.name.downcase
-    partial = "select_#{origname}_#{destmodel.name.downcase}"
-    params = "'partial=#{partial}&tabindex=#{tabindex}&id='+value"
-    partial_note =  partial + "_note"
-    success_msg = "Effect.BlindUp('#{partial_note}', {duration: 0.5});; "
-    success_msg += "return false;"
-    loading_msg = "Toggle.display('#{partial_note}');"
-    remote_function(:update => partial, :with => params, :url => {:action => :update_select},
-                    :loading => loading_msg, :success => success_msg)
-  end
-
-   def remote_functag2(origmodels, destmodel, tabindex, prefix=nil)
-     origname = origmodels.shift
-     partial = "select_#{origname.name.downcase}_#{destmodel.name.downcase}"
-     params = "'partial=#{partial}&tabindex=#{tabindex}&id='+value"
-     origmodels.each { | model |
-       model_id = set_model_id(model)
-       params << "+'&#{model_id}='+$F('edit_#{model_id}')"
-     }
-     partial_note =  partial + "_note"
-     success_msg = "Effect.BlindUp('#{partial_note}', {duration: 0.5});; "
-     success_msg += "return false;"
-     loading_msg = "Toggle.display('#{partial_note}');"
-     remote_function(:update => partial, :with => params, :url => {:action => :update_select_from_selects},
-                     :loading => loading_msg, :success => success_msg)
-   end
-
-  def select_institutioncareer_by_degree(obj, model, columns, id, tabindex, validation_type=nil)
-    options = set_options_tags(tabindex, validation_type)
-    collection = model.find_by_sql "SELECT ic.id, ic.career_id, ic.institution_id FROM institutioncareers ic, careers c WHERE c.degree_id = #{id} AND c.id = ic.career_id"
-    list = list_collection(collection,  columns)
-    select(obj, set_model_id(model), list, {:prompt => '-- Seleccionar --'})
-  end
-
 end
--- trunk/test/unit/select_helper_test.rb
+++ trunk/test/unit/select_helper_test.rb
@@ -9,30 +9,35 @@ class ConferenceTest < Test::Unit::TestCase
     assert_equal 'orig_language_id', foreignize(Language, 'orig')
   end
 
-  def test_should_selectizedid_from_object
+  def test_should_selectize_id_from_object
     record = Person.find_by_firstname('Juana')
-    selected_id= selectizedid(record, 'country_id')
+    selected_id= selectize_id(record, 'country_id')
     assert_equal 484, selected_id
   end
 
   def test_should_set_default_value_for_fieldid_in_a_new_object
     record = Person.new
-    record.country_id = selectizedid(record, 'country_id', 500)
+    record.country_id = selectize_id(record, 'country_id', 500)
     assert_equal 500, record.country_id
   end
 
   def test_not_should_set_default_value_for_unexistent_fieldid_from_object
     record = Person.new
-    record.country_id = selectizedid(record, 'country_id2')
+    record.country_id = selectize_id(record, 'country_id2')
     assert_equal nil, record.country_id
   end
 
-  def test_should_selectizedid_from_filter
+  def test_should_selectize_id_from_filter
     record = Person.find_by_firstname('Juana')
     assert_equal 25, record.state_id
     filter = { 'state_id' => 19}
-    record.state_id = selectizedid(record, 'state_id', 9, filter)
+    record.state_id = selectize_id(record, 'state_id', 9, filter)
     assert_equal 19, record.state_id
+  end
+
+  def test_finder_id
+    assert_equal [['México', 484]], finder_id(Country, ['name'], 484)
+    assert_equal [['Sinaloa, México', 25]], finder_id(State,  %w(name country) , 25)
   end
 
   def test_simple_select
