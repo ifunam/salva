e68b981098f9d2e08fb63f2566bb34315c4630f1
Alejandro Silva <alexs1010@gmail.com>
2007-08-10 20:26:10 +0000

    Se generaron las pruebas unitarias para los modelos role y controller, se generaron sus archivos yaml y se modificaron sus modelos.

--- branches/salva_test/app/models/controller.rb
+++ branches/salva_test/app/models/controller.rb
@@ -1,4 +1,6 @@
 class Controller < ActiveRecord::Base
+   validates_numericality_of :id, :only_integer => true , :allow_nil => true
+
   validates_presence_of :name
   validates_uniqueness_of :name
 end
---
+++ branches/salva_test/test/unit/controller_test.rb
@@ -0,0 +1,62 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'controller'
+
+class RcontrollerTest < Test::Unit::TestCase
+  fixtures :controllers
+  include UnitSimple
+
+  def setup
+    @controllers = %w(citizen address person)
+  end
+
+  # Right - CRUD
+  def test_crud
+    crud_test(@controllers, Controller)
+  end
+
+  def test_validation
+    validate_test(@controllers, Controller)
+  end
+
+  def test_collision
+    collision_test(@controllers, Controller)
+  end
+
+  def test_create_with_empty_attributes
+    @mycontroller = Controller.new
+    assert !@mycontroller.save
+  end
+
+  def test_check_uniqueness
+    @mycontroller2 = Controller.new({:name => 'address'})
+    assert !@mycontroller2.save
+  end
+
+  # Boundaries
+  def test_bad_values_for_id
+    @mycontroller = Controller.new
+    @mycontroller.id = 'xx'
+    assert !@mycontroller.valid?
+
+    # Negative number ID
+    #@mycontroller.id = -1
+    #assert !@mycontroller.valid?
+
+    # Float number ID
+    @mycontroller.id = 1.3
+    assert !@mycontroller.valid?
+
+    # Nil number ID
+    @mycontroller.id = nil
+    assert !@mycontroller.valid?
+  end
+
+  def test_bad_values_for_name
+    # Checking constraints for name
+    # Nil name
+    @mycontroller = Controller.new
+    @mycontroller.name = nil
+    assert !@mycontroller.valid?
+  end
+
+end
