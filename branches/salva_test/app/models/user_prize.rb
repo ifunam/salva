193568698389bcafb8f00147dcdb0835716c9641
Alejandro Silva <alexs1010@gmail.com>
2007-08-21 18:08:48 +0000

    Se modifico el  archivo YAML de user_prize asi como modificaciones a su modelo, se agrego un registro mas al YAML de prizes

--- branches/salva_test/app/models/user_prize.rb
+++ branches/salva_test/app/models/user_prize.rb
@@ -1,5 +1,11 @@
 class UserPrize < ActiveRecord::Base
-validates_presence_of :prize_id, :year
-validates_numericality_of :prize_id
-belongs_to :prize
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :user_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :prize_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :year, :allow_nil => true, :only_integer => true
+  validates_presence_of :prize_id, :year, :user_id
+  validates_uniqueness_of :user_id, :scope => [:prize_id, :year]
+
+  belongs_to :prize
+  belongs_to :user
 end
