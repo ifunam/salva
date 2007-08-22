696e64b5ab07a3c891b794db94b9799b6f68dddc
Developer at IFUNAM <dev@fisica.unam.mx>
2007-08-22 16:02:59 +0000

    Correcciones menores a conferencetalk

--- trunk/app/models/conferencetalk.rb
+++ trunk/app/models/conferencetalk.rb
@@ -1,6 +1,7 @@
 class Conferencetalk < ActiveRecord::Base
 validates_presence_of :title, :authors, :conference_id, :talktype_id, :talkacceptance_id, :modality_id
 validates_numericality_of :conference_id, :talktype_id, :talkacceptance_id, :modality_id
+  validates_uniqueness_of :title, :scope => [:conference_id, :title, :authors, :talktype_id]
 belongs_to :conference
 belongs_to :talktype
 belongs_to :talkacceptance
