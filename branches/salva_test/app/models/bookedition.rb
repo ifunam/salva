32408367bd2536d8f411b6eba74fed4c91d52b54
Alejandro Silva <alexs1010@gmail.com>
2007-08-22 19:16:33 +0000

    Se genero la prueba unitaria para bookedition, se modifico su archivo YAML, se le agregaron reglas al modelo y datos a los YAML's: editions, books y mediatypes, conservando los existentes.

--- branches/salva_test/app/models/bookedition.rb
+++ branches/salva_test/app/models/bookedition.rb
@@ -1,15 +1,24 @@
 class Bookedition < ActiveRecord::Base
-  validates_presence_of :edition_id, :message => "Proporcione el edition_id"
-  validates_presence_of :publisher_id, :message => "Proporcione el publisher_id"
-  validates_presence_of :mediatype_id, :message => "Proporcione el mediatype_id"
+ validates_numericality_of :id, :allow_nil => true, :only_integer => true
+ validates_numericality_of :edition_id, :allow_nil => true, :only_integer => true
+ validates_numericality_of :book_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :publisher_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :mediatype_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :editionstatus_id, :allow_nil => true, :only_integer => true
+
+
+  validates_presence_of :edition_id
+  validates_presence_of :publisher_id
+  validates_presence_of :mediatype_id
   validates_uniqueness_of :edition_id, :scope => [ :book_id, :mediatype_id ]
 
   belongs_to :book
   belongs_to :edition
-  belongs_to :publisher 
+  belongs_to :publisher
   belongs_to :mediatype
   belongs_to :editionstatus
 
-  has_many :bookedition_comment
-  has_many :chapterinbook
+  has_many :bookedition_comments
+  has_many :chapterinbooks
+  has_many :bookedition_publishers
 end
