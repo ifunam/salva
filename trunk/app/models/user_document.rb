d8859a9479572a45e8e1e8f93c10ef5f3d62115b
Developer at IFUNAM <dev@fisica.unam.mx>
2007-08-24 01:21:45 +0000

    Se incluye soporte para  manejar mensajes entre el responsable y el autor del documento.

--- trunk/app/models/user_document.rb
+++ trunk/app/models/user_document.rb
@@ -1,7 +1,6 @@
 class UserDocument < ActiveRecord::Base
-  validates_presence_of :document_id, :status,  :ip_address
+  validates_presence_of :document_id, :ip_address
   validates_numericality_of :document_id
-  validates_inclusion_of :status, :in=> [true, false]
   validates_uniqueness_of :user_id, :scope => [:user_id, :document_id]
   belongs_to :document
   belongs_to :user
