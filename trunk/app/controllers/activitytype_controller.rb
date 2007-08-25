8c0f8c31ab45a62717e5bd900a63e60ed9479f6d
Developer at IFUNAM <dev@fisica.unam.mx>
2007-08-25 00:19:26 +0000

    Se corrigen errores en el controlador activitytype...

--- trunk/app/controllers/activitytype_controller.rb
+++ trunk/app/controllers/activitytype_controller.rb
@@ -7,6 +7,6 @@ class ActivitytypeController < SalvaController
     @purge_msg = 'La información se ha borrado'
     @per_pages = 10
     @order_by = 'id'
-    @list = { :include => [:activitytype], :conditions => "activitytypes.name = 'Otras actividades' AND activities.activitytype_id = activitytypes.id" }
+    @list = { :include => [:activities], :conditions => "activitytypes.name = 'Otras actividades' AND activities.activitytype_id = activitytypes.id" }
   end
 end
