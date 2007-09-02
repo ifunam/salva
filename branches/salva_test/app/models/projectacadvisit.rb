7718bb81594647f72f9eae882018cf8e251ca5b1
Ines Fernandez <inesfernan@gmail.com>
2007-09-02 04:19:39 +0000

    Se genero Test Unit para la tabla projectacadvisits, se modifica YAML y modelo

--- branches/salva_test/app/models/projectacadvisit.rb
+++ branches/salva_test/app/models/projectacadvisit.rb
@@ -1,6 +1,12 @@
 class Projectacadvisit < ActiveRecord::Base
-validates_presence_of :project_id, :acadvisit_id
-validates_numericality_of :project_id, :acadvisit_id
-belongs_to :project
-belongs_to :acadvisit
+  validates_presence_of :project_id, :acadvisit_id
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :project_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :acadvisit_id, :allow_nil => true, :only_integer => true
+
+  validates_uniqueness_of :acadvisit_id, :scope => [:project_id]
+
+
+  belongs_to :project
+  belongs_to :acadvisit
 end
