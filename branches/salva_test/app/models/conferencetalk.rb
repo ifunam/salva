13db30d70529ca96c9db84f39a631ec516e2d822
Ines Fernandez <inesfernan@gmail.com>
2007-09-03 01:54:02 +0000

    Se genero Test Unit para la tabla projectconferencetalks, se modifico su modelo y conferencetalk.rb

--- branches/salva_test/app/models/conferencetalk.rb
+++ branches/salva_test/app/models/conferencetalk.rb
@@ -1,8 +1,16 @@
 class Conferencetalk < ActiveRecord::Base
-validates_presence_of :title, :authors, :conference_id, :talktype_id, :talkacceptance_id, :modality_id
-validates_numericality_of :conference_id, :talktype_id, :talkacceptance_id, :modality_id
-belongs_to :conference
-belongs_to :talktype
-belongs_to :talkacceptance
-belongs_to :modality
+  validates_presence_of :title, :authors, :conference_id, :talktype_id, :talkacceptance_id, :modality_id
+
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :conference_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :talktype_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :talkacceptance_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :modality_id, :allow_nil => true, :only_integer => true
+
+  belongs_to :conference
+  belongs_to :talktype
+  belongs_to :talkacceptance
+  belongs_to :modality
+
+  has_many :projectconferencetalks
 end
--- branches/salva_test/app/models/projectconferencetalk.rb
+++ branches/salva_test/app/models/projectconferencetalk.rb
@@ -1,6 +1,12 @@
 class Projectconferencetalk < ActiveRecord::Base
-validates_presence_of :project_id, :conferencetalk_id
-validates_numericality_of :project_id, :conferencetalk_id
-belongs_to :project
-belongs_to :conferencetalk
+  validates_presence_of :project_id, :conferencetalk_id
+
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :project_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :conferencetalk_id, :allow_nil => true, :only_integer => true
+
+  validates_uniqueness_of :project_id, :scope => [:conferencetalk_id]
+
+  belongs_to :project
+  belongs_to :conferencetalk
 end
