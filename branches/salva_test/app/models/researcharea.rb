edb2dae95042198764364cf925e423cad050ef92
Ines Fernandez <inesfernan@gmail.com>
2007-09-04 19:09:28 +0000

    Se genero la Unit Test para la tabla: projectresearchareas, se modifica su modelo y el de researchareas

--- branches/salva_test/app/models/researcharea.rb
+++ branches/salva_test/app/models/researcharea.rb
@@ -1,6 +1,9 @@
 class Researcharea < ActiveRecord::Base
-  has_many :researchlines
-  validates_numericality_of :id, :allow_nil => true, :only_integer => true
   validates_presence_of :name
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+
   validates_uniqueness_of :name
+
+  has_many :researchlines
+  has_many :projectresearchareas
 end
---
+++ branches/salva_test/test/unit/projectresearcharea_test.rb
@@ -0,0 +1,136 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'project'
+require 'researcharea'
+require 'projectresearcharea'
+
+class ProjectresearchareaTest < Test::Unit::TestCase
+  fixtures :projecttypes,:projectstatuses, :projects, :researchareas, :projectresearchareas
+
+  def setup
+    @projectresearchareas = %w(proyecto_area_salva proyecto_area_issste proyecto_area_issste_2)
+    @myprojectresearcharea = Projectresearcharea.new({:project_id => 2, :researcharea_id => 1})
+  end
+
+  # Right - CRUD
+  def test_creating_projectresearchareas_from_yaml
+    @projectresearchareas.each { | projectresearcharea|
+      @projectresearcharea = Projectresearcharea.find(projectresearchareas(projectresearcharea.to_sym).id)
+      assert_kind_of Projectresearcharea, @projectresearcharea
+      assert_equal projectresearchareas(projectresearcharea.to_sym).id, @projectresearcharea.id
+      assert_equal projectresearchareas(projectresearcharea.to_sym).researcharea_id, @projectresearcharea.researcharea_id
+      assert_equal projectresearchareas(projectresearcharea.to_sym).project_id, @projectresearcharea.project_id
+    }
+  end
+
+  def test_deleting_projectresearchareas
+    @projectresearchareas.each { |projectresearcharea|
+      @projectresearcharea = Projectresearcharea.find(projectresearchareas(projectresearcharea.to_sym).id)
+      @projectresearcharea.destroy
+      assert_raise (ActiveRecord::RecordNotFound) {
+        Projectresearcharea.find(projectresearchareas(projectresearcharea.to_sym).id)
+      }
+    }
+  end
+
+  def test_creating_with_empty_attributes
+    @projectresearcharea = Projectresearcharea.new
+    assert !@projectresearcharea.save
+  end
+
+  def test_creating_duplicated_projectresearcharea
+    @projectresearcharea = Projectresearcharea.new({:project_id => 2, :researcharea_id => 3})
+    assert !@projectresearcharea.save
+  end
+
+  # Boundary
+  def test_bad_values_for_id
+    # Float number for ID
+    @myprojectresearcharea.id = 1.6
+    assert !@myprojectresearcharea.valid?
+    @myprojectresearcharea.id = 'mi_id'
+    assert !@myprojectresearcharea.valid?
+  end
+
+  def test_bad_values_for_researcharea_id
+    @myprojectresearcharea.researcharea_id = nil
+    assert !@myprojectresearcharea.valid?
+
+    @myprojectresearcharea.researcharea_id= 1.6
+    assert !@myprojectresearcharea.valid?
+
+    @myprojectresearcharea.researcharea_id = 'mi_id'
+    assert !@myprojectresearcharea.valid?
+  end
+
+  def test_bad_values_for_project_id
+    @myprojectresearcharea.project_id = nil
+    assert !@myprojectresearcharea.valid?
+
+    @myprojectresearcharea.project_id = 3.1416
+    assert !@myprojectresearcharea.valid?
+    @myprojectresearcharea.project_id = 'mi_id'
+    assert !@myprojectresearcharea.valid?
+  end
+
+  #Cross-Checking test
+  def test_cross_checking_for_project_id
+    @projectresearchareas.each { | projectresearcharea|
+      @projectresearcharea = Projectresearcharea.find(projectresearchareas(projectresearcharea.to_sym).id)
+      assert_kind_of Projectresearcharea, @projectresearcharea
+      assert_equal @projectresearcharea.project_id, Project.find(@projectresearcharea.project_id).id
+    }
+  end
+
+
+  def catch_exception_when_update_invalid_key(record)
+    begin
+      return true if record.update
+    rescue ActiveRecord::StatementInvalid => bang
+      return false
+    end
+  end
+
+  def test_cross_checking_with_bad_values_for_project_id
+    @projectresearchareas.each { | projectresearcharea|
+      @projectresearcharea = Projectresearcharea.find(projectresearchareas(projectresearcharea.to_sym).id)
+      assert_kind_of Projectresearcharea, @projectresearcharea
+      @projectresearcharea.project_id = 1000000
+      begin
+        return true if @projectresearcharea.update
+      rescue StandardError => x
+        return false
+      end
+    }
+  end
+
+  def test_cross_checking_for_researcharea_id
+    @projectresearchareas.each { | projectresearcharea|
+      @projectresearcharea = Projectresearcharea.find(projectresearchareas(projectresearcharea.to_sym).id)
+
+      assert_kind_of Projectresearcharea, @projectresearcharea
+      assert_equal @projectresearcharea.researcharea_id, Researcharea.find(@projectresearcharea.researcharea_id).id
+    }
+  end
+
+  def catch_exception_when_update_invalid_key(record)
+    begin
+      return true if record.update
+    rescue ActiveRecord::StatementInvalid => bang
+      return false
+    end
+  end
+
+  def test_cross_checking_with_bad_values_for_researcharea_id
+    @projectresearchareas.each { | projectresearcharea|
+      @projectresearcharea = Projectresearcharea.find(projectresearchareas(projectresearcharea.to_sym).id)
+      assert_kind_of Projectresearcharea, @projectresearcharea
+      @projectresearcharea.researcharea_id = 100000
+      begin
+        return true if @projectresearcharea.update
+      rescue StandardError => x
+        return false
+      end
+    }
+  end
+
+end
