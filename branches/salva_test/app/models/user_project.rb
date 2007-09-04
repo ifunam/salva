68a3d20e24d423e2aa7e22b75b529fcf0d442275
Alejandro Silva <alexs1010@gmail.com>
2007-09-04 18:41:04 +0000

    Se genero la prueba unitaria para user_project, se agrego datos al archivo yaml user_project y se modificaron las reglas del modelo.

--- branches/salva_test/app/models/user_project.rb
+++ branches/salva_test/app/models/user_project.rb
@@ -1,6 +1,13 @@
 class UserProject < ActiveRecord::Base
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :user_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :project_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :roleinproject_id, :allow_nil => true, :only_integer => true
+
   validates_presence_of :project_id, :roleinproject_id
-  validates_numericality_of :project_id, :roleinproject_id
+
+  validates_uniqueness_of :project_id, :scope => [:roleinproject_id, :user_id]
+
   belongs_to :user
   belongs_to :project
   belongs_to :roleinproject
