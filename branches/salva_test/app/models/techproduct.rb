793bbc4322b4aedc9348de0047edd7dc1f84b2b1
Alejandro Silva <alexs1010@gmail.com>
2007-09-07 18:47:14 +0000

    Prueba unitaria para techproduct, modificaciones a su archivo YAML asi como a su modelo

--- branches/salva_test/app/models/techproduct.rb
+++ branches/salva_test/app/models/techproduct.rb
@@ -1,6 +1,10 @@
 class Techproduct < ActiveRecord::Base
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :institution_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :techproductstatus_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :techproducttype_id, :allow_nil => true, :only_integer => true
+
   validates_presence_of :title, :authors
-  validates_numericality_of :techproducttype_id, :techproductstatus_id
   belongs_to :techproducttype
   belongs_to :techproductstatus
   belongs_to :institution
