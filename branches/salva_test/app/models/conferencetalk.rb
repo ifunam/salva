9f06c4b5ae1bb04edf89f622179252cc2649ccd7
Miguel Vazquez <mvazquez83@gmail.com>
2007-09-11 19:02:53 +0000

    Se modifica modelo  asi como prueba de unidad conferencetalks_test jalando perfectamente, tambien se modifica la prueba conference_test.rb seguiendo standares de la elaboracion de pruebas de unidad

--- branches/salva_test/app/models/conferencetalk.rb
+++ branches/salva_test/app/models/conferencetalk.rb
@@ -2,11 +2,11 @@ class Conferencetalk < ActiveRecord::Base
   validates_presence_of :title, :authors, :conference_id, :talktype_id, :talkacceptance_id, :modality_id
 
   validates_numericality_of :id, :allow_nil => true, :only_integer => true
-  validates_numericality_of :conference_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :conference_id, :only_integer => true
   validates_numericality_of :talktype_id, :allow_nil => true, :only_integer => true
   validates_numericality_of :talkacceptance_id, :allow_nil => true, :only_integer => true
   validates_numericality_of :modality_id, :allow_nil => true, :only_integer => true
-
+  validates_uniqueness_of :conference_id, :title, :authors
   belongs_to :conference
   belongs_to :talktype
   belongs_to :talkacceptance
