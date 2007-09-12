9bd90f28a00f43a1e8fdf0f814a7f41332794d12
Alejandro Silva <alexs1010@gmail.com>
2007-09-12 18:53:13 +0000

    Se crea prueba unitaria para user_skill, se modifico su modelo. [[BR]] Se agrego un registro al archivo YAML skilltypes.

--- branches/salva_test/app/models/user_skill.rb
+++ branches/salva_test/app/models/user_skill.rb
@@ -1,6 +1,8 @@
 class UserSkill < ActiveRecord::Base
-  validates_presence_of :skilltype_id
-  validates_numericality_of :skilltype_id
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :user_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :skilltype_id, :allow_nil => true, :only_integer => true
+  validates_presence_of :skilltype_id, :user_id
 
   belongs_to :skilltype
 end
