931dde7ed351156260c0ef1232b44dd7177d8e5d
Alejandro Silva <alexs1010@gmail.com>
2007-09-12 17:26:12 +0000

    Prueba unitaria para roleingroup, modificaciones al modelo. [[BR]] Se genero el archivo YAML para roleingroups y se agrego una registro al archivo YAML de roles.

--- branches/salva_test/app/models/roleingroup.rb
+++ branches/salva_test/app/models/roleingroup.rb
@@ -1,4 +1,9 @@
 class Roleingroup < ActiveRecord::Base
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :group_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :role_id, :allow_nil => true, :only_integer => true
+  validates_presence_of :role_id, :group_id
+  validates_uniqueness_of :group_id, :scope => [:role_id]
   belongs_to  :group
   belongs_to :role
 end
