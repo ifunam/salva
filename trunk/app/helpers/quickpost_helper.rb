b081d46c46ee9c0311275d55b9232d9bd8806519
Alejandro Aguilar Sierra <algsierra@gmail.com>
2007-09-13 07:03:51 +0000

    Estilo para botones quickpost sin usar input type=image y funcionar bien en IE

--- trunk/app/helpers/quickpost_helper.rb
+++ trunk/app/helpers/quickpost_helper.rb
@@ -3,13 +3,13 @@ module QuickpostHelper
   include Labels
 
   def quickpost(controller)
-    tag :input, { "type" => "image", "src" => image_path('add.gif'),
-                   "class" => "add", "name" => 'stack', "value" => controller }
+    tag :input, { "type" => "submit", "class" => "quickpost", 
+    	          "name" => 'stack', "value" => controller }
   end
 
   def submit_for_stack_list(controller)
-    tag :input, { "type" => "image", "src" => image_path('select.gif'),
-                   "class" => "add", "name" => 'stacklist', "value" => controller }
+    tag :input, { "type" => "submit", "class" => "quickpost-select", 
+                  "name" => 'stacklist', "value" => controller }
   end
 
 end
