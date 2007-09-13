582c40bb5d7c2e376ffb5494faa2f760658a5d4b
Alejandro Silva <alexs1010@gmail.com>
2007-09-13 21:17:49 +0000

    Prueba unitaria para user_regularcourse, modificaciones a su modelo, a su archivo YAML, asi como modificaciones a los archivos YAML: [[BR]] periods y regularcourses

--- branches/salva_test/app/models/user_regularcourse.rb
+++ branches/salva_test/app/models/user_regularcourse.rb
@@ -1,8 +1,14 @@
 class UserRegularcourse < ActiveRecord::Base
   validates_presence_of :regularcourse_id, :roleinregularcourse_id, :period_id
-  validates_numericality_of :regularcourse_id, :period_id, :roleinregularcourse_id, :only_integer => true
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :user_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :regularcourse_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :period_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :roleinregularcourse_id, :allow_nil => true, :only_integer => true
   validates_numericality_of :hoursxweek, :allow_nil => true, :only_integer => true
-  validates_inclusion_of :hoursxweek, :in => 1..40, :allow_nil => true 
+  validates_inclusion_of :hoursxweek, :in => 1..40, :allow_nil => true
+  validates_uniqueness_of :user_id, :scope => [:regularcourse_id, :period_id, :roleinregularcourse_id]
+
   belongs_to :regularcourse
   belongs_to :period
   belongs_to :roleinregularcourse
