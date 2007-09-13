a81189d2a88d7dafd2bdfba868e421b30fb1055e
Ines Fernandez <inesfernan@gmail.com>
2007-09-13 20:49:22 +0000

    Se actualizan modelo con respecto al trunk

--- branches/salva_test/app/models/user_journal.rb
+++ branches/salva_test/app/models/user_journal.rb
@@ -1,6 +1,4 @@
 class UserJournal < ModelComposedKeys
-  set_table_name "user_journals"
-  set_primary_keys :user_id, :journal_id
   validates_presence_of :journal_id, :roleinjournal_id, :startyear
   validates_numericality_of :journal_id, :roleinjournal_id
   validates_uniqueness_of :user_id, :scope => [:journal_id, :roleinjournal_id]
