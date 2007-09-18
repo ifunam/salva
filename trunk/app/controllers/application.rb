1c5a85281947a7337d048b52515db8eaad95c736
Alejandro Aguilar Sierra <algsierra@gmail.com>
2007-09-18 20:10:30 +0000

    Se resuelve el problema de las fechas

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
   include Sessions
   include Rbac
   include NavigatorTree
-  before_filter :configure_datestyle
   before_filter :login_required
   before_filter :change_userid
   before_filter :set_user_id
@@ -41,12 +40,6 @@ class ApplicationController < ActionController::Base
 
   def update_remote_partial
     render :partial => 'salva/'+params[:partial]
-  end
-
-  def configure_datestyle
-    suppress(ActiveRecord::StatementInvalid) do
-      ActiveRecord::Base.connection.execute  "SET datestyle TO 'SQL, DMY';"
-    end
   end
 
   def change_userid
