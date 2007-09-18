ddafa326601cf181b15aafcac5ad34ab861e686b
Developer at IFUNAM <dev@fisica.unam.mx>
2007-09-18 17:17:52 +0000

    Se restaura el registro para el domicilio temporal

--- trunk/db/catalogs/addresstypes.sql
+++ trunk/db/catalogs/addresstypes.sql
@@ -1,3 +1,3 @@
 INSERT INTO addresstypes (name) VALUES ('Domicilio profesional');
 INSERT INTO addresstypes (name) VALUES ('Domicilio particular');
---INSERT INTO addresstypes (name) VALUES ('Domicilio temporal');
+INSERT INTO addresstypes (name) VALUES ('Domicilio temporal');
