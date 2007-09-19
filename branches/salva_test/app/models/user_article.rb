8a4758b6a610698838f2368127b6604a8114315a
Gilberto Casillas <gacp@tonatiuh.igeofcu.unam.mx>
2007-09-19 17:36:17 +0000

    Se modifico el modelo user_article y su fixture correspondiente

--- branches/salva_test/app/models/user_article.rb
+++ branches/salva_test/app/models/user_article.rb
@@ -1,6 +1,11 @@
 class UserArticle < ActiveRecord::Base
-  validates_presence_of :article_id
-  validates_numericality_of :article_id
+  validates_presence_of :article_id, :user_id
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :article_id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :user_id, :allow_nil => true, :only_integer => true
+
+  validates_uniqueness_of :user_id, :article_id
+ 
   belongs_to :article
   belongs_to :user
 end
