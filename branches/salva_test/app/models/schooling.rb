6c9f11bf27dee9b626fc4dd816e8ea5f89db1ae7
Miguel Vazquez <mvazquez83@gmail.com>
2007-09-20 18:42:48 +0000

    Se añaden datos de prueba a lgunos fixtures y se realiza la prueba schooling_test.rb jalando perfectamente

--- branches/salva_test/app/models/schooling.rb
+++ branches/salva_test/app/models/schooling.rb
@@ -1,7 +1,10 @@
 class Schooling < ActiveRecord::Base
   validates_presence_of :institutioncareer_id, :startyear
-  validates_numericality_of :institutioncareer_id, :startyear
+  validates_numericality_of :id, :allow_nil => true, :only_integer => true
+  validates_numericality_of :institutioncareer_id, :startyear, :only_integer => true
   validates_numericality_of :endyear, :credits, :allow_nil => true
+  validates_uniqueness_of :user_id, :institutioncareer_id
+
   belongs_to :institutioncareer
   has_many :professionaltitles
 end
