c3cdaf63bc25c5516c1167cc4616eb3e1d3aa724
Developer at IFUNAM <dev@fisica.unam.mx>
2007-09-21 18:27:31 +0000

    Agregamos validacion para la unicidad de la participacion de jurados en tesis (de acuerdo al usuario y rol), y quitamos ModelComposedKeys en user_roleingroup

--- trunk/app/models/user_roleingroup.rb
+++ trunk/app/models/user_roleingroup.rb
@@ -1,6 +1,8 @@
-class UserRoleingroup < ModelComposedKeys
-  set_table_name "user_roleingroups"
-  set_primary_keys :user_id
+class UserRoleingroup < ActiveRecord::Base
+  validates_presence_of :user_id, :roleingroup_id
+  validates_numericality_of :user_id, :roleingroup_id
+  validates_uniqueness_of :user_id, :scope => [:roleingroup_id]
   belongs_to :user
-  belongs_to :roleingroup 
+  belongs_to :roleingroup
+
 end
--- trunk/app/models/user_techproduct.rb
+++ trunk/app/models/user_techproduct.rb
@@ -1,6 +1,9 @@
 class UserTechproduct < ActiveRecord::Base
-validates_presence_of :techproduct_id, :year
-validates_numericality_of :techproduct_id, :userrole_id
-belongs_to :techproduct
-belongs_to :userrole
+  validates_presence_of :techproduct_id, :year
+  validates_numericality_of :techproduct_id, :userrole_id, :user_id
+  validates_uniqueness_of :techproduct_id, :scope => [:userrole_id, :user_id]
+
+  belongs_to :user
+  belongs_to :techproduct
+  belongs_to :userrole
 end
