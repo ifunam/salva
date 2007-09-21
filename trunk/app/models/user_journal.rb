390a18f651cc21459f65ade3fda4c6f1f3cb6fb8
Developer at IFUNAM <dev@fisica.unam.mx>
2007-09-21 23:16:53 +0000

    Quitamos model_composed_keys y otros cambios menores

--- trunk/app/models/user_journal.rb
+++ trunk/app/models/user_journal.rb
@@ -1,6 +1,8 @@
 class UserJournal < ActiveRecord::Base
-validates_presence_of :journal_id, :roleinjournal_id, :startyear
-validates_numericality_of :journal_id, :roleinjournal_id
-belongs_to :journal
-belongs_to :roleinjournal
+  validates_presence_of :journal_id, :roleinjournal_id, :startyear
+  validates_numericality_of :journal_id, :roleinjournal_id, :user_id
+  validates_uniqueness_of :user_id, :scope =>[:journal_id, :roleinjournal_id]
+  belongs_to :user
+  belongs_to :journal
+  belongs_to :roleinjournal
 end
