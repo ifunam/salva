16f3e05e480b9a216feebe1a91a6d519a2a86d18
Developer at IFUNAM <dev@fisica.unam.mx>
2007-09-21 23:49:25 +0000

    Quitamos model_composed_keys

--- trunk/app/models/user_genericwork.rb
+++ trunk/app/models/user_genericwork.rb
@@ -1,6 +1,4 @@
-class UserGenericwork  < ModelComposedKeys
-  set_table_name "user_genericworks"
-  set_primary_keys :user_id, :genericwork_id
+class UserGenericwork  < ActiveRecord::Base
   validates_presence_of :genericwork_id, :userrole_id
   validates_numericality_of :genericwork_id, :userrole_id
   validates_uniqueness_of :user_id, :scope => [:genericwork_id, :userrole_id]
