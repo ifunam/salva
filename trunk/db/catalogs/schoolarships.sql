0c966aa2547451821ef6b6c8d9bfe6a69584727e
Alejandro Aguilar Sierra <algsierra@gmail.com>
2007-10-01 06:58:47 +0000

    Cambios para cerrar ticket 230

--- trunk/db/catalogs/schoolarships.sql
+++ trunk/db/catalogs/schoolarships.sql
@@ -1,4 +1,4 @@
-INSERT INTO schoolarships (name, institution_id) VALUES ('Dirección General de Asuntos del Personal Académic', 1);
+INSERT INTO schoolarships (name, institution_id) VALUES ('Dirección General de Asuntos del Personal Académico', 1);
 INSERT INTO schoolarships (name, institution_id) VALUES ('Dirección General de Estudios de Posgrado', 1);
 INSERT INTO schoolarships (name, institution_id) VALUES ('Beca SEP', 5588);
 INSERT INTO schoolarships (name, institution_id) VALUES ('Beca Conacyt', 5588);
