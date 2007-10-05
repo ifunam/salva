e7c271fc277d22371ff06981d4e7b43540e2d59a
Ines Fernandez <inesfernan@gmail.com>
2007-10-05 22:33:02 +0000

    Se actualiza branch con respecto al trunk hasta el Changeset 1710

--- branches/salva_test/app/controllers/user_document_controller.rb
+++ branches/salva_test/app/controllers/user_document_controller.rb
@@ -7,8 +7,8 @@ class UserDocumentController < ApplicationController
   end
 
   def list
-    @pages, @collection = paginate :user_documents, :per_page => 10,
-    :conditions => "user_id = #{session[:user]} AND document_id = #{@document}"
+    @collection = UserDocument.paginate :page => 1, :per_page => 10,
+    :conditions => "user_id = #{session[:user]} AND document_id = #{@document_id}"
     render :action => 'list'
   end
 
@@ -16,26 +16,31 @@ class UserDocumentController < ApplicationController
     @user = User.find(session[:user])
     record = UserDocument.new
     record.ip_address = request.env['REMOTE_ADDR']
-    record.document_id = @document
+    record.document_id = @document_id
     record.file = StringIO.new(@file).read
     record.filename  = @filename
     record.content_type = @content_type
     record.moduser_id = @user.id
     record.user_id = @user.id
-    record.status = 't'  unless @user.has_user_incharge?
+    record.user_incharge_id = @user.user_incharge_id if @user.has_user_incharge?
     if record.save
       mail =  {
         :body => { :institution => get_myinstitution.name },
-        :attachment => { :file => record.file,  :content_type => record.content_type }
+        :attachment => { :file => record.file,  :content_type => record.content_type, :filename => @filename }
       }
       if @user.has_user_incharge?
-        mail[:recipients] = [@user.email,  @user.user_incharge.email]
+        mail[:recipients] = [@user.email]
         mail[:subject] = @request_for_approval_subject
-        @notifier.deliver_request_for_approval(mail)
+        UserDocumentNotifier.deliver_request_for_approval(mail)
+
+        mail[:recipients] = [@user.user_incharge.email]
+        mail[:subject] = @request_for_approval_subject
+        mail.delete(:attachment)
+        UserDocumentNotifier.deliver_request_for_approval_notification(mail)
       else
         mail[:recipients] = @user.email
         mail[:subject] = @notification_subject
-        @notifier.deliver_notification_of_delivery(mail)
+        UserDocumentNotifier.deliver_notification_of_delivery(mail)
       end
       flash[:notice] = @sent_msg
       redirect_to :action => 'list'
@@ -46,12 +51,12 @@ class UserDocumentController < ApplicationController
   end
 
   def show
-    @record = UserDocument.find(params[:id])
-    @headers['Cache-Control'] = 'no-cache, must-revalidate'
-    if  @record.file and  !@record.filename.nil? and !@record.content_type.nil? then
-      send_data(@record.file, :filename => @record.filename, :type => "application/"+@record.content_type.to_s, :disposition => "inline")
+    record = UserDocument.find(params[:id])
+    response.headers['Cache-Control'] = 'no-cache, must-revalidate'
+    if  record.file and  !record.filename.nil? and !record.content_type.nil? then
+      send_data(record.file, :filename => record.filename, :type => record.content_type, :disposition => "inline")
     else
-      redirect_to "/images/comodin.png"
+      send_file RAILS_ROOT + "/public/images/comodin.png", :type => 'image/png', :disposition => 'inline'
     end
   end
 
--- branches/salva_test/app/models/user_document_notifier.rb
+++ branches/salva_test/app/models/user_document_notifier.rb
@@ -1,24 +1,27 @@
+require 'yaml'
 class UserDocumentNotifier < ActionMailer::Base
-  helper ActionView::Helpers::UrlHelper
-
   def setup(options)
-    @recipients = options[:recipients] || "salva@salva.unam.mx"
-    @from = options[:from] || "salva@salva.unam.mx"
-    @cc = options[:cc] || ""
-    @bcc = options[:bcc] || ""
+    smtp = YAML.load(File.read("#{RAILS_ROOT}/config/mail.yml"))
+    domain = smtp['settings'][:domain].to_s
+    @recipients = options[:recipients] || "noreply@#{domain}"
+    @from = options[:from] || "noreply@#{domain}"
     @subject = "[SALVA] "
     @subject << options[:subject] unless options[:subject].nil?
     @body = options[:body] || {}
     @headers = options[:headers] || {}
-    @charset = options[:charset] || "utf-8"
-    @sent_on     = Time.now
+    @sent_on  = Time.now
   end
 
   def add_attachment(att)
-    attachment :content_type => att[:content_type], :body => StringIO.new(att[:file]).read
+    attachment :content_type => att[:content_type], :body => StringIO.new(att[:file]).read, :filename => att[:filename]
   end
 
   def request_for_approval(options)
+    self.setup(options)
+    add_attachment(options[:attachment]) if options[:attachment].is_a? Hash
+  end
+
+  def request_for_approval_notification(options)
     self.setup(options)
     add_attachment(options[:attachment]) if options[:attachment].is_a? Hash
   end
@@ -29,6 +32,11 @@ class UserDocumentNotifier < ActionMailer::Base
   end
 
   def notification_of_delivery(options)
+    self.setup(options)
+    add_attachment(options[:attachment]) if  options[:attachment].is_a? Hash
+  end
+
+  def deny_notification(options)
     self.setup(options)
     add_attachment(options[:attachment]) if  options[:attachment].is_a? Hash
   end
