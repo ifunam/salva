bf17fe8ecfa0f670e071f13ada0f3232b1363fbc
Developer at IFUNAM <dev@fisica.unam.mx>
2007-10-05 06:12:10 +0000

    Avances en el disen~o del nuevo finder

--- branches/salva_test/test/unit/finder_test.rb
+++ branches/salva_test/test/unit/finder_test.rb
@@ -13,29 +13,29 @@ class FinderTest < Test::Unit::TestCase
     assert_equal 'journals',  @f.tableize('journal')
   end
 
-    def test_columns_for_select
+    def test_build_select
       args = [UserArticle, ['article', 'title', ['journal', 'name', ['country', 'name']]], 'ismainauthor', ['city', 'name']]
-      assert_equal 'articles.title AS articles_title, journals.name AS journals_name, countries.name AS countries_name, user_articles.ismainauthor AS user_articles_ismainauthor, cities.name AS cities_name', @f.columns_for_select(*args)
+      assert_equal 'articles.title AS articles_title, journals.name AS journals_name, countries.name AS countries_name, user_articles.ismainauthor AS user_articles_ismainauthor, cities.name AS cities_name', @f.build_select(*args)
       args = [UserArticle, ['article', 'title', ['journal', 'name', ['country', 'name']]], 'ismainauthor']
-      assert_equal 'articles.title AS articles_title, journals.name AS journals_name, countries.name AS countries_name, user_articles.ismainauthor AS user_articles_ismainauthor', @f.columns_for_select(*args)
+      assert_equal 'articles.title AS articles_title, journals.name AS journals_name, countries.name AS countries_name, user_articles.ismainauthor AS user_articles_ismainauthor', @f.build_select(*args)
     end
 
-    def test_get_tables
+    def test_set_tables
       args = [UserArticle, ['article', 'title', ['journal', 'name', ['country', 'name']]], 'ismainauthor']
-      assert_equal 'user_articles, articles, journals, countries', @f.get_tables(*args).join(', ')
+      assert_equal 'user_articles, articles, journals, countries', @f.set_tables(*args).join(', ')
       args = [UserArticle, ['article', 'title', ['journal', 'name']], 'ismainauthor']
-      assert_equal 'user_articles, articles, journals', @f.get_tables(*args).join(', ')
+      assert_equal 'user_articles, articles, journals', @f.set_tables(*args).join(', ')
       args = [UserArticle, 'ismainauthor']
-      assert_equal 'user_articles', @f.get_tables(*args).join(', ')
+      assert_equal 'user_articles', @f.set_tables(*args).join(', ')
    end
 
-   def  test_set_conditions
+   def  test_build_conditions
      args =[UserArticle, ['article', 'title', ['journal', 'name', ['country', 'name']]], 'ismainauthor']
-     assert_equal 'user_articles.article_id = articles.id  AND articles.journal_id = journals.id  AND journals.country_id = countries.id ', @f.set_conditions(*args).join(' AND ')
+     assert_equal 'user_articles.article_id = articles.id  AND articles.journal_id = journals.id  AND journals.country_id = countries.id ', @f.build_conditions(*args).join(' AND ')
    end
 
   def test_sql
-    assert_equal 'SELECT user_articles.id AS id, articles.title AS articles_title, journals.name AS journals_name FROM user_articles, articles, journals  WHERE user_articles.article_id = articles.id  AND articles.journal_id = journals.id ', @f.sql
+    assert_equal "    SELECT user_articles.id AS id, articles.title AS articles_title, journals.name AS journals_name\n    FROM user_articles, articles, journals\n    WHERE user_articles.article_id = articles.id  AND articles.journal_id = journals.id \n", @f.sql
   end
 
   def test_as_text
