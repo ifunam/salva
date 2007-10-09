7e5e9692d6559a7f84c75d271c29609873df1b45
Alejandro Silva <alexs1010@gmail.com>
2007-10-09 18:15:07 +0000

    Correcciones a modelos

--- branches/salva_test/app/models/user_article.rb
+++ branches/salva_test/app/models/user_article.rb
@@ -1,6 +1,6 @@
 class UserArticle < ActiveRecord::Base
-  validates_presence_of :article_id, :user_id
-  validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
+  validates_presence_of :article_id
+  validates_numericality_of :id,  :allow_nil => true, :greater_than => 0
   validates_numericality_of :user_id, :article_id, :greater_than => 0, :only_integer => true
   validates_uniqueness_of :user_id, :scope => [:article_id]
 
--- branches/salva_test/app/models/user_techproduct.rb
+++ branches/salva_test/app/models/user_techproduct.rb
@@ -1,7 +1,8 @@
 class UserTechproduct < ActiveRecord::Base
   validates_presence_of :techproduct_id, :user_id, :year
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_numericality_of :techproduct_id, :user_id, :userrole_id, :year, :greater_than => 0, :only_integer => true
+  validates_numericality_of :userrole_id, :allow_nil => true, :greater_than => 0, :only_integer => true
+  validates_numericality_of :techproduct_id, :user_id, :year, :greater_than => 0, :only_integer => true
   validates_uniqueness_of :techproduct_id, :scope => [:userrole_id, :user_id]
 
   belongs_to :user
