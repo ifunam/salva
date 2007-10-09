d9df3ed058711facd8fa2b25b774b944c0223b4a
Alejandro Silva <alexs1010@gmail.com>
2007-10-09 18:24:59 +0000

    Correciones a modelos subidas erroneamente en la revision [1719]

--- branches/salva_test/app/models/user_article.rb
+++ branches/salva_test/app/models/user_article.rb
@@ -1,7 +1,7 @@
 class UserArticle < ActiveRecord::Base
-  validates_presence_of :article_id
-  validates_numericality_of :id,  :allow_nil => true, :greater_than => 0
-  validates_numericality_of :user_id, :article_id, :greater_than => 0, :only_integer => true
+  validates_presence_of :article_id, :ismainauthor
+  validates_numericality_of :id,  :allow_nil => true, :greater_than => 0, :only_integer => true
+  validates_numericality_of :article_id, :greater_than => 0, :only_integer => true
   validates_uniqueness_of :user_id, :scope => [:article_id]
 
   belongs_to :article
--- branches/salva_test/app/models/user_techproduct.rb
+++ branches/salva_test/app/models/user_techproduct.rb
@@ -1,8 +1,8 @@
 class UserTechproduct < ActiveRecord::Base
-  validates_presence_of :techproduct_id, :user_id, :year
+  validates_presence_of :techproduct_id, :year
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_numericality_of :userrole_id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_numericality_of :techproduct_id, :user_id, :year, :greater_than => 0, :only_integer => true
+  validates_numericality_of :techproduct_id, :year, :greater_than => 0, :only_integer => true
   validates_uniqueness_of :techproduct_id, :scope => [:userrole_id, :user_id]
 
   belongs_to :user
