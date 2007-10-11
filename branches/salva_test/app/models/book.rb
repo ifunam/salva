6709c70c675de4113dbbea23609c3441e6109171
Alejandro Silva <alexs1010@gmail.com>
2007-10-11 21:30:44 +0000

    Correcciones a validaciones en modelos.

--- branches/salva_test/app/models/book.rb
+++ branches/salva_test/app/models/book.rb
@@ -1,18 +1,19 @@
 class Book < ActiveRecord::Base
-  validates_presence_of :title
-  validates_presence_of :author
-  validates_presence_of :country_id
-  validates_presence_of :booktype_id
+  validates_presence_of :title, :author, :country_id, :booktype_id
+  validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
+  validates_numericality_of :country_id, :booktype_id, :greater_than => 0, :only_integer => true
+  validates_numericality_of :volume_id, :orig_language_id,  :trans_language_id, :allow_nil => true, :greater_than => 0, :only_integer => true
 
   belongs_to :country
   belongs_to :booktype
   belongs_to :volume
-  belongs_to :orig_language,
-  :class_name => 'Language',
-  :foreign_key => 'orig_language_id'
-  belongs_to :trans_language,
-  :class_name => 'Language',
-  :foreign_key => 'trans_language_id'
+  belongs_to :orig_language, :class_name => 'Language', :foreign_key => 'orig_language_id'
+  belongs_to :trans_language, :class_name => 'Language', :foreign_key => 'trans_language_id'
+
+  validates_associated :country
+  validates_associated :booktype
+  validates_associated :volume
+  validates_associated :language
 
   has_many :bookeditions
 end
--- branches/salva_test/app/models/tutorial_committee.rb
+++ branches/salva_test/app/models/tutorial_committee.rb
@@ -1,6 +1,13 @@
 class TutorialCommittee < ActiveRecord::Base
-validates_presence_of :studentname, :degree_id, :institutioncareer_id, :year
-validates_numericality_of :degree_id, :institutioncareer_id
-belongs_to :degree
-belongs_to :institutioncareer
+  validates_presence_of :studentname, :degree_id, :institutioncareer_id, :year
+  validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
+  validates_numericality_of :user_id, :degree_id, :institutioncareer_id, :year, :greater_than => 0, :only_integer => true
+
+  belongs_to :degree
+  belongs_to :institutioncareer
+  belongs_to :user
+
+  validates_associated :degree
+  validates_associated :institutioncareer
+  validates_associated :user
 end
