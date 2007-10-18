899660a6b30d90ea0d3fd379311e1149e67e8fd9
Developer at IFUNAM <dev@fisica.unam.mx>
2007-10-18 16:37:14 +0000

    Actualizamos la raíz del website, en lugar de usa public/index.html, se usará :controlle => 'user, :action => 'index'

--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -26,6 +26,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.connect '', :controller => 'user', :action => 'index'
 
   # See how all your routes lay out with "rake routes"
 
