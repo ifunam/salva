793aefc49ad842ef4e1c2c478e43868b0e66386f
Developer at IFUNAM <dev@fisica.unam.mx>
2007-10-23 04:19:48 +0000

    Actualizamos disen~o de  tablas y mvc referente a la citas bibliograficas por usuario

--- trunk/app/models/user_cite.rb
+++ trunk/app/models/user_cite.rb
@@ -1,3 +1,8 @@
 class UserCite < ActiveRecord::Base
-validates_presence_of :total
+  validates_presence_of :author_name, :total
+  validates_numericality_of :total, :greater_than => 0, :only_integer => true
+  validates_uniqueness_of :user_id
+
+  belongs_to :user
+  validates_associated :user
 end
