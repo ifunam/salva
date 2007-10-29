3960e36ede15714f59a75ee7535bc5c8cf9d6dca
Developer at IFUNAM <dev@fisica.unam.mx>
2007-10-29 23:17:43 +0000

    Se resuelven problemas con user_outreachwork y se agregan las pruebas del controlador.

--- trunk/test/functional/user_outreachwork_controller_test.rb
+++ trunk/test/functional/user_outreachwork_controller_test.rb
@@ -4,13 +4,13 @@ require 'user_outreachwork_controller'
 class UserOutreachworkController; def rescue_action(e) raise e end; end
 
 class  UserOutreachworkControllerTest < SalvaControllerTest
-  
+  fixtures :userroles, :genericworkgroups, :genericworktypes, :genericworkstatuses, :genericworks, :user_genericworks  
 
   def initialize(*args)
    super
    @mycontroller =  UserOutreachworkController.new
-   @myfixtures = {}
-   @mybadfixtures = {   }
+   @myfixtures = { :genericwork_id => 1,  :user_id => 3,  :userrole_id => 1 }
+   @mybadfixtures = { :genericwork_id => nil,  :user_id => 3,  :userrole_id => nil }
    @model = UserGenericwork
   end
 end
