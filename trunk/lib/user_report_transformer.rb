b98707494a0d04fb8a06230895b837ed2734be62
Developer at IFUNAM <dev@fisica.unam.mx>
2007-10-31 22:38:34 +0000

    Se renombran metodos y se realizan cambios menores

--- trunk/lib/user_report_transformer.rb
+++ trunk/lib/user_report_transformer.rb
@@ -8,42 +8,38 @@ class UserReportTransformer
   include Labels
 
   def as_html(data)
-    output = ''
+    body = ''
     index = 0
     toc = [ ]
     section = [ ]
     data.each do |hash|
       section << { :title => hash[:title], :level => hash[:level]}
-      output << "<div class=\"section\">\n" if hash[:level] == 1
-      output << internal_link(hash[:title]) + header(get_label(hash[:title]), hash[:level], 'html') + "\n"
-      if hash.has_key?(:data)
-        output << paragraph(hash[:data].collect { |text|
-                              (text.is_a?Array ) ? bold(get_label(text[0]) + ":", 'html') + text[1].to_s + '<br/>': ('# ' + text)
-        }.join("\n"))
-      end
-      index +=1
+      body << "<div class=\"section\">\n" if hash[:level] == 1
+      body << internal_link(hash[:title]) + header(get_label(hash[:title]), hash[:level], 'html') + "\n"
+      body << html_paragraph(hash[:data])  if hash.has_key?(:data)
+      index += 1
       if data[index].nil? or data[index][:level] == 1
         toc.push(section)
         section = [ ]
-        output << '<span class="back">' + link_to_internal('toc', get_label('back'), "back_top.gif") + "</span>\n"
-        output << "\n</div>\n"
+        body << '<span class="back">' + link_to_internal('toc', get_label('top'), "back_top.gif") + "</span>\n"
+        body << "\n</div>\n"
       end
     end
-    html_toc(toc) + output
+    html_table_of_content(toc) + body
+  end
+  
+  def html_paragraph(data)
+    paragraph(data.collect { |text|   (text.is_a?Array ) ? bold(get_label(text[0]) + ":", 'html') + text[1].to_s + '<br/>': ('# ' + text) }.join("\n"))
   end
 
-  def html_toc(toc) 
-   n = 0
-   internal_link('toc') + "\n" + table("table(toc).\n" + '|' + toc.collect { |section| html_toc_section(section, n+=1) }.join("|") + '|') + "\n"
+  def html_table_of_content(toc) 
+    n = 0
+    internal_link('toc') + "\n" + table("table(toc).\n" + '|' + toc.collect { |section| html_table_of_content_section(section, (n <= 4 ?  n += 1 : n = 1)) }.join("|") + '|') + "\n"
   end
 
-  def html_toc_section(section, n)
+  def html_table_of_content_section(section, n)
     section.collect {|item|
-      if item[:level] == 1
-       link_to_internal(item[:title], get_label(item[:title]), "section#{n}.gif")
-      else
-        '*' * item[:level] + ' ' + link_to_internal(item[:title], get_label(item[:title]))
-      end
+      item[:level] == 1 ?  link_to_internal(item[:title], get_label(item[:title]), "section#{n}.gif") : '*' * item[:level] + ' ' + link_to_internal(item[:title], get_label(item[:title]))
     }.join("\n")
   end
 end
