b0587cdb9d00f7c80947b42349b64327264aa12e
Developer at IFUNAM <dev@fisica.unam.mx>
2007-10-31 22:57:33 +0000

    Nombres de metodos simplificados

--- trunk/lib/user_report_html_transformer.rb
+++ trunk/lib/user_report_html_transformer.rb
@@ -14,7 +14,7 @@ class UserReportHtmlTransformer
       section << { :title => hash[:title], :level => hash[:level]}
       body << "<div class=\"section\">\n" if hash[:level] == 1
       body << internal_link(hash[:title]) + header(get_label(hash[:title]), hash[:level], 'html') + "\n"
-      body << html_paragraph(hash[:data])  if hash.has_key?(:data)
+      body << paragraph_data(hash[:data])  if hash.has_key?(:data)
       index += 1
       if data[index].nil? or data[index][:level] == 1
         toc.push(section)
@@ -23,10 +23,10 @@ class UserReportHtmlTransformer
         body << "\n</div>\n"
       end
     end
-    html_table_of_content(toc) + body
+    table_of_content(toc) + body
   end
   
-  def html_paragraph(data)
+  def paragraph_data(data)
     paragraph(data.collect { |text| 
       if text.is_a?Array  
         bold(get_label(text[0]) + ":", 'html') + text[1].to_s + '<br/>' if !text[1].nil? and !text[1].blank?
@@ -36,12 +36,12 @@ class UserReportHtmlTransformer
     }.compact.join("\n"))
   end
 
-  def html_table_of_content(toc) 
+  def table_of_content(toc) 
     n = 0
-    internal_link('toc') + "\n" + table("table(toc).\n" + '|' + toc.collect { |section| html_table_of_content_section(section, (n <= 4 ?  n += 1 : n = 1)) }.join("|") + '|') + "\n"
+    internal_link('toc') + "\n" + table("table(toc).\n" + '|' + toc.collect { |section| table_of_content_section(section, (n <= 4 ?  n += 1 : n = 1)) }.join("|") + '|') + "\n"
   end
 
-  def html_table_of_content_section(section, n)
+  def table_of_content_section(section, n)
     section.collect {|item|
       item[:level] == 1 ?  link_to_internal(item[:title], get_label(item[:title]), "section#{n}.gif") : '*' * item[:level] + ' ' + link_to_internal(item[:title], get_label(item[:title]))
     }.join("\n")
