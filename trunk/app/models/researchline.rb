062d1dda17560f16322803fd84fb3ad4ce65a5db
Developer at IFUNAM <dev@fisica.unam.mx>
2007-11-13 02:57:41 +0000

    Correccion menor (validacion de unicidad)

--- trunk/app/models/researchline.rb
+++ trunk/app/models/researchline.rb
@@ -2,7 +2,7 @@ class Researchline < ActiveRecord::Base
   validates_presence_of :name
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_numericality_of :researcharea_id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :scope => [:researcharea_id]
 
   belongs_to :researcharea
   has_many :projectresearchlines
