ce3169d951b61592233511bf71a5340bf6cf1d09
Developer at IFUNAM <dev@fisica.unam.mx>
2007-11-15 18:22:03 +0000

    Agregamos nuevos archivos requeridos por rails 2.0 usando lighttpd o apache2

--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -26,11 +26,11 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.connect '', :controller => 'user', :action => 'index'
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect '', :controller => 'user', :action => 'index'
 end
