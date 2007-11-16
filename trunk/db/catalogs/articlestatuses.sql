6ecf0d0b796570d764b678be0ff4f149d5c7bc45
Developer at IFUNAM <dev@fisica.unam.mx>
2007-11-16 00:23:32 +0000

    Se actualiza la lista de estados para articulos

--- trunk/db/catalogs/articlestatuses.sql
+++ trunk/db/catalogs/articlestatuses.sql
@@ -1,7 +1,7 @@
 INSERT INTO articlestatuses (name) VALUES ('Aceptado');
 INSERT INTO articlestatuses (name) VALUES ('Enviado');
-INSERT INTO articlestatuses (name) VALUES ('En prensa');
-INSERT INTO articlestatuses (name) VALUES ('En proceso');
 INSERT INTO articlestatuses (name) VALUES ('Publicado');
+INSERT INTO articlestatuses (name) VALUES ('En preparación');
+
 
 
