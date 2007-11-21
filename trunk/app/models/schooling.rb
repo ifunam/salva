e73ae95bab43882c4ce62d4636689decbca1783d
Alejandro Aguilar Sierra <algsierra@gmail.com>
2007-11-21 03:53:08 +0000

    Multiples correcciones a los bugs reportados por FCPyS el 15 de noviembre

--- trunk/app/models/schooling.rb
+++ trunk/app/models/schooling.rb
@@ -3,8 +3,8 @@ class Schooling < ActiveRecord::Base
   validates_numericality_of :id, :allow_nil => true,  :greater_than => 0, :only_integer => true
   validates_numericality_of :institutioncareer_id, :startyear, :greater_than => 0, :only_integer => true
   validates_numericality_of :endyear, :credits, :allow_nil => true
-  validates_inclusion_of :is_titleholder, :in => [true], :if => lambda { |record| record.is_studying_this == false}
-  validates_inclusion_of :is_studying_this, :in => [true], :if => lambda { |record| record.is_titleholder == false}
+  validates_inclusion_of :is_titleholder, :in => [true, false], :if => lambda { |record| record.is_studying_this == false}
+  validates_inclusion_of :is_studying_this, :in => [true, false], :if => lambda { |record| record.is_titleholder == false}
   validates_uniqueness_of :user_id, :scope => [:institutioncareer_id] 
   belongs_to :institutioncareer
 end
