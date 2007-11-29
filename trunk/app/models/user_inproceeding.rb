d0b4777316bdb47999f75f98a2d514a1c82fd5f9
Developer at IFUNAM <dev@fisica.unam.mx>
2007-11-29 20:19:25 +0000

    correccion menor

--- trunk/app/models/user_inproceeding.rb
+++ trunk/app/models/user_inproceeding.rb
@@ -3,7 +3,7 @@ class UserInproceeding < ActiveRecord::Base
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_numericality_of :inproceeding_id, :user_id,  :greater_than => 0, :only_integer => true
   validates_inclusion_of :ismainauthor, :in => [true, false]
-  validates_uniqueness_of :institutioncareer_id, :scope => [:user_id]
+  validates_uniqueness_of :inproceeding_id, :scope => [:user_id]
 
   belongs_to :inproceeding
   belongs_to :user
