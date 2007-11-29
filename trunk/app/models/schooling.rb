47d5d411d658d8f7d967124b78d9505d6db5c7df
Developer at IFUNAM <dev@fisica.unam.mx>
2007-11-29 20:09:16 +0000

    Se corrigen errores relacionados al uso de validates_inclusion_of y validates_presence_of, el uso del primer metodo en algun atributo debe excluir al segundo metodo (validates_presence_of)

--- trunk/app/models/schooling.rb
+++ trunk/app/models/schooling.rb
@@ -5,6 +5,6 @@ class Schooling < ActiveRecord::Base
   validates_numericality_of :endyear, :credits, :allow_nil => true
   validates_inclusion_of :is_titleholder, :in => [true, false], :if => lambda { |record| record.is_studying_this == false}
   validates_inclusion_of :is_studying_this, :in => [true, false], :if => lambda { |record| record.is_titleholder == false}
-  validates_uniqueness_of :user_id, :scope => [:institutioncareer_id] 
+  validates_uniqueness_of :institutioncareer_id, :scope => [:user_id] 
   belongs_to :institutioncareer
 end
--- trunk/app/models/user_inproceeding.rb
+++ trunk/app/models/user_inproceeding.rb
@@ -1,9 +1,9 @@
 class UserInproceeding < ActiveRecord::Base
-  validates_presence_of :inproceeding_id, :ismainauthor
+  validates_presence_of :inproceeding_id
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_numericality_of :inproceeding_id, :user_id,  :greater_than => 0, :only_integer => true
   validates_inclusion_of :ismainauthor, :in => [true, false]
-  validates_uniqueness_of :user_id, :scope => [:inproceeding_id]
+  validates_uniqueness_of :institutioncareer_id, :scope => [:user_id]
 
   belongs_to :inproceeding
   belongs_to :user
