0a1f80ef63306ddf1e30e3d9341be55a8500e107
Developer at IFUNAM <dev@fisica.unam.mx>
2007-11-29 17:27:29 +0000

    Se corrigen problemas con user_newspaperarticle

--- trunk/app/models/user_newspaperarticle.rb
+++ trunk/app/models/user_newspaperarticle.rb
@@ -1,17 +1,10 @@
 class UserNewspaperarticle < ActiveRecord::Base
-  validates_presence_of :newspaperarticle_id, :ismainauthor
-
+  validates_presence_of :newspaperarticle_id
   validates_numericality_of :id, :allow_nil => true, :greater_than =>0, :only_integer => true
   validates_numericality_of :newspaperarticle_id, :user_id,  :greater_than =>0, :only_integer => true
-
   validates_inclusion_of :ismainauthor, :in=> [true, false]
-
-  validates_uniqueness_of :user_id, :scope => [:newspaperarticle_id]
+  validates_uniqueness_of :newspaperarticle_id, :scope => [:user_id]
 
   belongs_to :newspaperarticle
-
   validates_associated :newspaperarticle
-
-
-
 end
