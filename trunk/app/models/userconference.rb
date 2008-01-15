033c5364b53a93c1f6a09558ea70e62269eb3ce7
Alejandro Silva <alexs1010@gmail.com>
2008-01-15 01:37:23 +0000

    Se hizo cambios para manejo de multiples formularios en Useconference_committe

--- trunk/app/models/userconference.rb
+++ trunk/app/models/userconference.rb
@@ -1,13 +1,13 @@
 class Userconference < ActiveRecord::Base
 
-  validates_presence_of :conference_id, :roleinconference_id
+  validates_presence_of  :roleinconference_id
 
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_numericality_of :conference_id, :roleinconference_id, :user_id,  :greater_than =>0,  :only_integer => true
+  validates_numericality_of  :roleinconference_id, :user_id,  :greater_than =>0,  :only_integer => true
 
 
 
-  validates_uniqueness_of :user_id, :scope => [:conference_id, :roleinconference_id], :message => 'El rol del usuario esta duplicado'
+  #validates_uniqueness_of :user_id, :scope => [:conference_id, :roleinconference_id], :message => 'El rol del usuario esta duplicado'
 
   belongs_to :conference
   belongs_to :roleinconference
