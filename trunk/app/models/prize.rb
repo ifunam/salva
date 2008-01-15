b6a78cf3ac7517988476085eb8706f290abc5027
Developer at IFUNAM <dev@fisica.unam.mx>
2008-01-15 13:28:54 +0000

    Correcciones menores  a premios y distinciones

--- trunk/app/models/prize.rb
+++ trunk/app/models/prize.rb
@@ -1,9 +1,8 @@
  class Prize < ActiveRecord::Base
   validates_numericality_of :id, :allow_nil => true,  :greater_than =>0, :only_integer => true
-
   validates_presence_of :name
-
   belongs_to :prizetype
-
+  belongs_to :institution
+  
   validates_associated :prizetype
 end
--- trunk/app/models/user_prize.rb
+++ trunk/app/models/user_prize.rb
@@ -1,9 +1,8 @@
 class UserPrize < ActiveRecord::Base
-  attr_accessor :prizetype
-  validates_presence_of :prize_id, :year
+  validates_presence_of  :year
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_numericality_of :user_id, :prize_id, :year,  :greater_than => 0, :only_integer => true
-  validates_uniqueness_of :user_id, :scope => [:prize_id, :year]
+  validates_numericality_of :user_id, :year,:greater_than => 0, :only_integer => true
+  #validates_uniqueness_of :user_id, :scope => [:prize_id, :year]
 
   belongs_to :prize
   belongs_to :user
