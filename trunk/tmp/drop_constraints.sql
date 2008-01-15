9967c3ac85d292382a090f63ed94726de8430f50
Developer at IFUNAM <dev@fisica.unam.mx>
2008-01-15 05:47:59 +0000

    Script temporal para eliminar constraints unique en algunas tablas

---
+++ trunk/tmp/drop_constraints.sql
@@ -0,0 +1,7 @@
+ALTER TABLE articles DROP CONSTRAINT articles_title_key;
+ALTER TABLE projects DROP CONSTRAINT projects_name_key;
+ALTER TABLE institutions DROP CONSTRAINT institutions_name_key;
+ALTER TABLE seminaries DROP CONSTRAINT seminaries_title_key;
+ALTER TABLE conferences DROP CONSTRAINT conferences_name_key;
+ALTER TABLE stimulustypes DROP CONSTRAINT stimulustypes_name_key;
+ALTER TABLE stimuluslevels DROP CONSTRAINT stimuluslevels_name_key;
