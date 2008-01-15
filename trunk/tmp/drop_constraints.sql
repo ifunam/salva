732138097fb4f3d8f54b08c73ab8d204d64daba7
Developer at IFUNAM <dev@fisica.unam.mx>
2008-01-15 15:10:55 +0000

    Se actualiza la lista constraints tipo UNIQUE que debemos borrar de la BD

--- trunk/tmp/drop_constraints.sql
+++ trunk/tmp/drop_constraints.sql
@@ -5,3 +5,11 @@ ALTER TABLE seminaries DROP CONSTRAINT seminaries_title_key;
 ALTER TABLE conferences DROP CONSTRAINT conferences_name_key;
 ALTER TABLE stimulustypes DROP CONSTRAINT stimulustypes_name_key;
 ALTER TABLE stimuluslevels DROP CONSTRAINT stimuluslevels_name_key;
+ALTER TABLE user_prizes DROP CONSTRAINT user_prizes_prize_id_key;
+ALTER TABLE prizes DROP CONSTRAINT prizes_name_key;
+ALTER TABLE bookedition_roleinbooks DROP CONSTRAINT bookedition_roleinbooks_user_id_key;
+ALTER TABLE bookeditions DROP CONSTRAINT bookeditions_book_id_key;
+ALTER TABLE books DROP CONSTRAINT books_title_key;
+ALTER TABLE chapterinbooks DROP CONSTRAINT chapterinbooks_bookedition_id_key;
+ALTER TABLE chapterinbook_roleinchapters DROP CONSTRAINT chapterinbook_roleinchapters_user_id_key;
+
