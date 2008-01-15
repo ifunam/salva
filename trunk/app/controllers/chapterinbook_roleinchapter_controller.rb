3223037210abfd41e8679e854d2c65e3428043fc
Developer at IFUNAM <dev@fisica.unam.mx>
2008-01-15 15:09:53 +0000

    Convertimos chapterinbook_roleinchapter a multi_salva_controller

--- trunk/app/controllers/chapterinbook_roleinchapter_controller.rb
+++ trunk/app/controllers/chapterinbook_roleinchapter_controller.rb
@@ -1,7 +1,9 @@
-class ChapterinbookRoleinchapterController < SalvaController
+class ChapterinbookRoleinchapterController < MultiSalvaController
   def initialize
     super
     @model = ChapterinbookRoleinchapter
+    @views = [ :chapterinbook, :book, :bookedition, :chapterinbook_roleinchapter ]
+    @models = [ ChapterinbookRoleinchapter, [Chapterinbook, [Bookedition, Book] ] ]
     @create_msg = 'La información se ha guardado'
     @update_msg = 'La información ha sido actualizada'
     @purge_msg = 'La información se ha borrado'
--- trunk/app/models/chapterinbook.rb
+++ trunk/app/models/chapterinbook.rb
@@ -1,9 +1,9 @@
 class Chapterinbook < ActiveRecord::Base
-  validates_presence_of :bookedition_id, :bookchaptertype_id, :title
+  validates_presence_of :bookchaptertype_id, :title
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_numericality_of :bookedition_id,  :bookchaptertype_id, :greater_than => 0, :only_integer => true
+  validates_numericality_of :bookchaptertype_id, :greater_than => 0, :only_integer => true
 
-  validates_uniqueness_of :title, :scope => [:bookedition_id, :bookchaptertype_id]
+  #validates_uniqueness_of :title, :scope => [:bookedition_id, :bookchaptertype_id]
 
   belongs_to :bookedition
   belongs_to :bookchaptertype
@@ -11,6 +11,6 @@ class Chapterinbook < ActiveRecord::Base
   has_many :projectchapterinbooks
   has_many :chapterinbook_comments
 
-  validates_associated :bookedition
-  validates_associated :bookchaptertype
+  #validates_associated :bookedition
+  #validates_associated :bookchaptertype
 end
