facb982794d3e024cbea175d0bb7eab01cf8a7ec
Developer at IFUNAM <dev@fisica.unam.mx>
2008-01-16 02:40:33 +0000

    student_advice -> multi_salva_controller

--- trunk/app/controllers/student_advice_controller.rb
+++ trunk/app/controllers/student_advice_controller.rb
@@ -1,7 +1,9 @@
-class StudentAdviceController < SalvaController
+class StudentAdviceController < MultiSalvaController
   def initialize
     super
     @model = Indivadvice
+    @views = [:student_advice, :institution]
+    @models = [ Indivadvice, Institution ]
     @create_msg = 'La información se ha guardado'
     @update_msg = 'La información ha sido actualizada'
     @purge_msg = 'La información se ha borrado'
--- trunk/app/models/indivadvice.rb
+++ trunk/app/models/indivadvice.rb
@@ -1,6 +1,6 @@
 class Indivadvice < ActiveRecord::Base
   validates_presence_of :indivadvicetarget_id, :indivname, :startyear, :hours
-  validates_numericality_of :institution_id, :indivadvicetarget_id
+  validates_numericality_of :indivadvicetarget_id
   belongs_to :user
   belongs_to :indivuser, :class_name => "User", :foreign_key => "indivuser_id"
   belongs_to :institution
