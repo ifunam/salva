df3a0ef3d78aafd9a93de6140130b6cc8915f3c1
Alejandro Silva <alexs1010@gmail.com>
2008-01-23 00:17:53 +0000

    Se implemento MVC para schooling usando MultiSalvaController y cambios menores

--- trunk/app/models/schooling.rb
+++ trunk/app/models/schooling.rb
@@ -1,10 +1,10 @@
 class Schooling < ActiveRecord::Base
-  validates_presence_of :institutioncareer_id, :startyear
+  validates_presence_of  :startyear
   validates_numericality_of :id, :allow_nil => true,  :greater_than => 0, :only_integer => true
-  validates_numericality_of :institutioncareer_id, :startyear, :greater_than => 0, :only_integer => true
+
   validates_numericality_of :endyear, :credits, :allow_nil => true
   validates_inclusion_of :is_titleholder, :in => [true, false], :if => lambda { |record| record.is_studying_this == false}
   validates_inclusion_of :is_studying_this, :in => [true, false], :if => lambda { |record| record.is_titleholder == false}
-  validates_uniqueness_of :institutioncareer_id, :scope => [:user_id] 
+ # validates_uniqueness_of :institutioncareer_id, :scope => [:user_id]
   belongs_to :institutioncareer
 end
