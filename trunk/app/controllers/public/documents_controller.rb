93d5aaad04873eb2bb6b27d4551c859130c3c048
Developer at IFUNAM <dev@fisica.unam.mx>
2008-01-25 22:43:53 +0000

    Correcciones varias a public/documents

--- trunk/app/controllers/public/documents_controller.rb
+++ trunk/app/controllers/public/documents_controller.rb
@@ -18,12 +18,12 @@ class Public::DocumentsController < ActionController::Base
     end
   end
 
-  def user_list
+  def list
     @collection = []
     @total_docs = 0
     document = Document.find(params[:id])
-    Adscription.find(:all, :order => 'name ASC').each do |adscription|
-      @user_documents = UserDocument.find_by_sql("SELECT user_documents.id, user_documents.document_id, user_documents.user_id FROM user_documents, people, user_adscriptions WHERE user_documents.document_id = #{document.id} AND user_documents.status = 't' AND user_documents.user_id = people.user_id AND user_documents.user_id = user_adscriptions.user_id AND people.user_id = user_adscriptions.user_id AND user_adscriptions.adscription_id = #{adscription.id}")
+    Adscription.find(:all, :order => 'name DESC').each do |adscription|
+      @user_documents = UserDocument.find_by_sql("SELECT user_documents.id, user_documents.document_id, user_documents.user_id FROM user_documents, people, user_adscriptions WHERE user_documents.user_id = people.user_id AND user_documents.user_id = user_adscriptions.user_id AND people.user_id = user_adscriptions.user_id AND user_documents.document_id = #{document.id} AND user_documents.status = 't'  AND user_adscriptions.adscription_id = #{adscription.id} GROUP BY user_documents.id, user_documents.document_id, user_documents.user_id,  people.lastname1, people.lastname2, people.firstname  ORDER BY people.lastname1 ASC, people.lastname2, people.firstname")
       unless @user_documents.nil?
         @total_docs += @user_documents.size
         @collection << { :adscription => adscription.name, :docs => @user_documents } if @user_documents.size > 0
@@ -31,7 +31,7 @@ class Public::DocumentsController < ActionController::Base
     end
     @document_title = document.documenttype.name + ' ' + document.title.strip 
     respond_to do |format|
-      format.html { render :action => "user_list"}
+      format.html { render :action => "list"}
     end
   end
 
