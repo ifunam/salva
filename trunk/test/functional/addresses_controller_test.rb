cb108835b517fab87c2bbe25d20f0187e80a1409
Alejandro Silva <alexs1010@gmail.com>
2008-03-13 16:55:17 +0000

    Completamos la prueba para el controlador addresses

--- trunk/test/functional/addresses_controller_test.rb
+++ trunk/test/functional/addresses_controller_test.rb
@@ -1,8 +1,9 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require 'addresses_controller'
 
-class AddressesControllerTest < ActionController::TestCase
+class AddressesControllerTest < Test::Unit::TestCase
    include Session
-   fixtures :userstatuses, :users
+   fixtures :userstatuses, :users, :countries, :states, :cities, :addresstypes,  :addresses
 
   def setup
     @request     = ActionController::TestRequest.new
@@ -14,41 +15,42 @@ class AddressesControllerTest < ActionController::TestCase
   def test_should_get_index
     get :index
     assert_response :success
-# assert_not_nil assigns(:addresses)
+    assert_not_nil assigns(:collection)
   end
-#   def test_should_get_new
-#     get :new
-#     assert_response :success
-#   end
 
-#   def test_should_create_address
-#     assert_difference('Address.count') do
-#       post :create, :address => { }
-#     end
+  def test_should_get_new
+    get :new
+    assert_response :success
+  end
 
-#     assert_redirected_to address_path(assigns(:address))
-#   end
+  def test_should_create_address
+    assert_difference('Address.count') do
+      post :create, :address => {:location => "Tajín No. 6344, Int 1, Col. Letrán Valle, Delegación Benito Juárez", :zipcode => 03650,  :country_id => 484,  :state_id => 9,  :city_id => 64,  :addresstype_id =>  2, :is_postaddress=> true}
 
-#   def test_should_show_address
-#     get :show, :id => addresses(:one).id
-#     assert_response :success
-#   end
+    end
+    assert_redirected_to address_path(assigns(:record))
+   end
 
-#   def test_should_get_edit
-#     get :edit, :id => addresses(:one).id
-#     assert_response :success
-#   end
+  def test_should_show_address
+    get :show, :id => Address.find(:first).id
+    assert_response :success
+  end
 
-#   def test_should_update_address
-#     put :update, :id => addresses(:one).id, :address => { }
-#     assert_redirected_to address_path(assigns(:address))
-#   end
+  def test_should_get_edit
+    get :edit, :id => Address.find(:first).id
+    assert_response :success
+  end
 
-#   def test_should_destroy_address
-#     assert_difference('Address.count', -1) do
-#       delete :destroy, :id => addresses(:one).id
-#     end
+  def test_should_update_address
+    put :update, :id => Address.find(:first).id, :address => {:location => "Clavel 50",:zipcode => 0364,  :country_id => 484,  :state_id => 9,  :city_id => 64,  :addresstype_id =>  3, :is_postaddress=> false}
+    assert_equal 3, assigns(:record).addresstype_id
+    assert_redirected_to address_path(assigns(:record))
+  end
 
-#     assert_redirected_to addresses_path
-#   end
+  def test_should_destroy_address
+    assert_difference('Address.count', -1) do
+      delete :destroy, :id => Address.find(:first).id
+    end
+    assert_redirected_to addresses_path
+  end
 end
