352a1dfdffbc88c580a53732423c4472c2959f40
Developer at IFUNAM <dev@fisica.unam.mx>
2008-09-24 11:35:10 -0500

    Disabling migration transaction and making more portable code for sequence alteration for postgresql

--- db/migrate/20080604170822_standarize_people.rb
+++ db/migrate/20080604170822_standarize_people.rb
@@ -1,6 +1,5 @@
 class StandarizePeople < ActiveRecord::Migration
   def self.up
-    transaction do
       add_column :people, :id, :serial;
       add_index :people, [:id], :name => :people_id_key, :unique => true
       i = 0
@@ -8,7 +7,8 @@ class StandarizePeople < ActiveRecord::Migration
         record.id = i++
         record.save
       end
-      execute "ALTER SEQUENCE people_id_seq RESTART WITH #{i};" if i > 0
-    end
+      if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+        execute "ALTER SEQUENCE people_id_seq RESTART WITH #{i};" if i > 0
+      end
   end
 end
