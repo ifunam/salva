485727e4d44d988b949e3ec97b4a35182a62560f
Developer at IFUNAM <dev@fisica.unam.mx>
2008-10-22 13:52:27 -0500

    Regexp to clean some characters in the title of articles and named_scope for published_articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,13 +10,18 @@ class Article < ActiveRecord::Base
 
   has_many :user_articles
   has_many :users, :through => :user_articles
-
+  named_scope :published_by_year, lambda { |y|  { :conditions => ['year = ?', y], :order => 'articles.year DESC, articles.month DESC, articles.authors ASC, articles.title ASC' } }
+  
   def as_text
-    [authors, title, journal.name, year, journal_issue, pages ].compact.join(', ').gsub(/\,+/, ',')
+    [authors, title, journal.name, year, journal_issue, pages ].compact.collect { |v|
+      s = (v.is_a? String) ? v.strip.gsub(/\n/,'').gsub(/\r/,'').strip.gsub(/\s{1}+/,' ').gsub(/^\"|\"$|^\“|\”$|^\'|\'$/,'').gsub(/ \./,'').sub(/( \,$)+/,'') : v.to_s
+      s unless s.strip.empty?
+    }.compact.join(', ').gsub(/( \,)/,',').gsub(/\,+/,',')
   end
-
+  
+  private
   def journal_issue
-    if !vol.nil? and !num.nil?
+    if !vol.to_s.empty? and !num.to_s.empty?
       "#{vol.to_s.strip}(#{num.to_s.strip})"
     elsif !vol.nil?
       vol.to_s.strip
