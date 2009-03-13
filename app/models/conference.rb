83a6789c230391f14630e2d46d3e2d7cb07464ca
Developer at IFUNAM <dev@fisica.unam.mx>
2009-03-12 18:10:54 -0600

    named_scope for userconference and conference models

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -7,14 +7,14 @@ class Conference < ActiveRecord::Base
 
   validates_inclusion_of :isspecialized, :in=> [true, false]
 
-  #validates_uniqueness_of  :name,  :scope => [:year, :country_id]
-
-
+  named_scope :local_scope, :conditions => 'conferencescope_id = 1'
+  named_scope :national_scope, :conditions => 'conferencescope_id = 2'
+  named_scope :international_scope, :conditions => 'conferencescope_id = 3'
+  
   belongs_to :conferencetype
   belongs_to :country
   belongs_to :conferencescope
 
   has_many :conference_institutions
   has_many :institutions, :through => :conference_institutions
-
 end
--- app/models/userconference.rb
+++ app/models/userconference.rb
@@ -5,10 +5,19 @@ class Userconference < ActiveRecord::Base
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_numericality_of  :roleinconference_id, :user_id,  :greater_than =>0,  :only_integer => true
 
-  #validates_uniqueness_of :user_id, :scope => [:conference_id, :roleinconference_id], :message => 'El rol del usuario esta duplicado'
 
   belongs_to :conference
   belongs_to :roleinconference
   belongs_to :user
 
+  default_scope :order => 'conferences.month ASC, conferences.name ASC', :include => [ :conference ]
+  named_scope :as_attenders, :conditions => "roleinconference_id = 1" 
+  named_scope :as_coordinator, :conditions => "roleinconference_id != 1"
+  named_scope :find_by_conference_year, lambda { |y| { :conditions => [ 'conferences.year = ?', y ],  :include => [ :conference ] } }
+  
+  def as_text
+      as_text_line([conference.name, conference.location, conference.country.name, month_to_s(conference.month),
+                    conference.year, label_for(user.person.fullname, roleinconference.name)
+                    ])
+  end
 end
