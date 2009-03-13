602f9598f88f60cf5a000064aced978b3cb3d619
Developer at IFUNAM <dev@fisica.unam.mx>
2009-03-13 11:31:00 -0600

    as_text method for user_proceding and user_journal

--- app/models/user_journal.rb
+++ app/models/user_journal.rb
@@ -11,4 +11,12 @@ class UserJournal < ActiveRecord::Base
 
   validates_associated :journal
   validates_associated :roleinjournal
+  default_scope :order => 'journals.name ASC, people.lastname1, people.lastname2, people.firstname', :include => [ :journal, { :user => :person } ]
+  named_scope :find_by_year, lambda { |y| { :conditions => ['user_journals.startyear = ? OR user_journals.endyear = ?', y, y] } }
+  
+  def as_text
+    as_text_line([journal.name, label_for(user.person.fullname, roleinjournal.name), label_for(startyear, 'Año de inicio'),
+                  label_for(endyear, 'Año de término')])
+  end
+  
 end
--- app/models/user_proceeding.rb
+++ app/models/user_proceeding.rb
@@ -8,4 +8,18 @@ class UserProceeding < ActiveRecord::Base
   belongs_to :proceeding
   belongs_to :roleproceeding
 
+   named_scope :find_by_year, :conditions => 'proceedings.year = 2008 OR conferences.year = 2008', 
+              :order => 'proceedings.title ASC, conferences.name ASC, people.lastname1 ASC, people.lastname2, people.firstname',
+              :include => [ { :proceeding => :conference }, { :user => :person }]
+
+  def as_text
+    publisher_name = proceeding.publisher.nil? ? nil : proceeding.publisher.name
+    as_text_line([proceeding.title, 
+                  label_for(proceeding.conference.name, proceeding.conference.conferencetype.name) + " (#{proceeding.conference.year})",
+                  publisher_name, 
+                  proceeding.conference.location, proceeding.conference.country.name, proceeding.volume,  
+                  proceeding.year, label_for(user.person.fullname, roleproceeding.name)])
+  end
+
+
 end
