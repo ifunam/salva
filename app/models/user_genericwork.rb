ca043f461a692db82f76213327ebb6c54bb4437d
Developer at IFUNAM <dev@fisica.unam.mx>
2009-03-13 12:24:21 -0600

    as_text for user_genericwork (including popularscienceworks and techreports)

--- app/models/user_genericwork.rb
+++ app/models/user_genericwork.rb
@@ -7,27 +7,44 @@ class UserGenericwork  < ActiveRecord::Base
   belongs_to :userrole
   belongs_to :user
 
-  named_scope :popularscienceworks, :joins => %Q( INNER JOIN genericworkgroups ON genericworkgroups.name = 'Productos de divulgación'  
-                                                  INNER JOIN genericworktypes ON genericworktypes.genericworkgroup_id = genericworkgroups.id 
-                                                  INNER JOIN genericworks ON genericworks.id = user_genericworks.genericwork_id 
-                                                  AND genericworks.genericworktype_id = genericworktypes.id
-                                                )
-                                                
-  named_scope :find_by_popularsciencework_year, lambda { |y| 
-                                      {
-                                        :joins => %Q( INNER JOIN genericworkgroups ON genericworkgroups.name = 'Productos de divulgación'  
-                                                  INNER JOIN genericworktypes ON genericworktypes.genericworkgroup_id = genericworkgroups.id 
-                                                  INNER JOIN genericworks ON genericworks.id = user_genericworks.genericwork_id 
-                                                  AND genericworks.genericworktype_id = genericworktypes.id AND genericworks.year = #{y}
-                                                )
-                                        }
-                                      }
-  
-  
+  named_scope :popularscienceworks, :joins => %Q( 
+  INNER JOIN genericworkgroups ON genericworkgroups.name = 'Productos de divulgación'  
+  INNER JOIN genericworktypes ON genericworktypes.genericworkgroup_id = genericworkgroups.id 
+  INNER JOIN genericworks ON genericworks.id = user_genericworks.genericwork_id 
+  AND genericworks.genericworktype_id = genericworktypes.id
+  )
+
+  named_scope :find_popularscienceworks_by_year, lambda { |y| 
+    {
+      :joins => %Q( 
+      INNER JOIN genericworkgroups ON genericworkgroups.name = 'Productos de divulgación'  
+      INNER JOIN genericworktypes ON genericworktypes.genericworkgroup_id = genericworkgroups.id 
+      INNER JOIN genericworks ON genericworks.id = user_genericworks.genericwork_id 
+      AND genericworks.genericworktype_id = genericworktypes.id AND genericworks.year = #{y}
+      )
+    }
+  }
+
+  named_scope :techreports, :joins => %Q(
+  INNER JOIN genericworks ON genericworks.id = user_genericworks.genericwork_id
+  AND genericworks.genericworktype_id = 7
+  )
+
+  named_scope :find_techreports_by_year,  lambda { |y|
+    {
+      :joins => %Q(
+      INNER JOIN genericworks ON genericworks.id = user_genericworks.genericwork_id
+      AND genericworks.genericworktype_id = 7 AND genericworks.year = #{y}
+      )
+    }
+  }
+
+
+
   def as_text
     institution_name = genericwork.institution.nil? ? nil : genericwork.institution.name
     publisher_name =  genericwork.publisher.nil? ? nil :  genericwork.publisher.name
     as_text_line([ genericwork.authors, genericwork.title, institution_name, genericwork.genericworktype.name, publisher_name,
-                   genericwork.reference, genericwork.year, genericwork.vol, genericwork.pages, label_for(genericwork.genericworkstatus.name, 'Estado')])
+      genericwork.reference, genericwork.year, genericwork.vol, genericwork.pages, label_for(genericwork.genericworkstatus.name, 'Estado')])
+    end
   end
-end
