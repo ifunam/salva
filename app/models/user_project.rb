e381c3810b3271b0f9051625f751ea52a0965403
Developer at IFUNAM <dev@fisica.unam.mx>
2009-03-13 12:05:26 -0600

    as_text method for user_project (thx mike)

--- app/models/user_project.rb
+++ app/models/user_project.rb
@@ -7,4 +7,14 @@ class UserProject < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   belongs_to :roleinproject
+  
+  default_scope  :include => [:project]
+  named_scope :find_by_project_year, lambda { |y| { :conditions => ['projects.startyear = ? OR projects.endyear = ?', y, y] } }
+  
+  def as_text
+     as_text_line([project.name, label_for(user.person.fullname, roleinproject.name), label_for(project.other, 'Fuente de financiamiento'),  
+                  label_for(project.projecttype.name, 'Tipo de proyecto'), label_for(project.startyear, 'Año de inicio'), 
+                  label_for(project.endyear, 'Año de término'),  label_for(project.projectstatus.name, 'Estado')
+                  ])
+  end
 end
