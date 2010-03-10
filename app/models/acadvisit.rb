d7b50c59cb647e8617bab4bdf9750c13178032ba
Developer at IFUNAM <dev@fisica.unam.mx>
2010-03-10 12:45:12 -0600

    Removing externalusers table and its foreing key references

--- app/models/acadvisit.rb
+++ app/models/acadvisit.rb
@@ -1,8 +1,5 @@
 class Acadvisit < ActiveRecord::Base
   validates_presence_of :institution_id, :country_id, :acadvisittype_id, :descr, :startyear
-
-   validates_numericality_of :id, :externaluser_id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_numericality_of :id, :externaluser_id, :allow_nil => true, :only_integer => true
   validates_numericality_of :user_id, :institution_id, :country_id, :acadvisittype_id, :startyear, :greater_than => 0, :only_integer => true
   validates_numericality_of :endyear, :startmonth, :endmonth, :allow_nil => true, :greater_than => 0, :only_integer => true
 
@@ -13,17 +10,10 @@ class Acadvisit < ActiveRecord::Base
   belongs_to :institution
   belongs_to :country
   belongs_to :acadvisittype
-  belongs_to :externaluser
 
   validates_associated :institution
   validates_associated :country
   validates_associated :acadvisittype
-  validates_associated :externaluser
 
   has_many :projectacadvisits
-
-
-# def validate
-#      errors.add(:startyear, "La fecha de inicio no debe ser posterior")  || endyear < staryear
-#  end
 end
---
+++ db/migrate/20100310181938_remove_externalusers.rb
@@ -0,0 +1,47 @@
+class RemoveExternalusers < ActiveRecord::Migration
+  def self.up
+    if Rails.env == 'production'
+      remove_index  :student_activities, :student_activities_user_id_key
+      remove_index  :student_activities, :student_activities_user_id_key1
+      remove_index :userrefereedpubs, :userrefereedpubs_refereedpubs_id_key
+      remove_index :userresearchgroups, [:externaluser_id, :researchgroup_id], :name => :userresearchgroups_researchgroup_id_key1, :unique => true
+      remove_index :externaluserlevels, :externaluserlevels_name_key
+    end
+    
+    remove_column :student_activities, :tutor_externaluser_id
+    remove_column :usercredits, :externalusergive_id
+    remove_column :userrefereedpubs, :externaluser_id
+    remove_column :userresearchgroups, :externaluser_id
+    remove_column :acadvisits, :externaluser_id
+    
+    drop_table :externaluserlevels
+    drop_table :externalusers
+  end
+
+  def self.down
+    create_table :externaluserlevels, :force => true do |t|
+      t.text :name, :null => false
+      t.references :moduser, :class_name => 'User', :foreign_key => 'moduser_id'
+      t.timestamps
+    end
+    add_index :externaluserlevels, [:name], :name => :externaluserlevels_name_key, :unique => true
+
+    create_table :externalusers, :force => true do |t|
+      t.references :institution, :externaluserlevel, :degree
+      t.text :firstname, :lastname1, :null => false
+      t.text :lastname2
+      t.references :moduser, :class_name => 'User', :foreign_key => 'moduser_id'
+      t.timestamps
+    end
+  
+     add_column :student_activities, :tutor_externaluser_id, :integer
+     add_index :student_activities, [:tutor_user_id, :user_id], :name => :student_activities_user_id_key, :unique => true
+     add_index :student_activities, [:tutor_externaluser_id, :user_id], :name => :student_activities_user_id_key1, :unique => true
+
+     add_column :usercredits, :externalusergive_id, :integer     
+     add_index :userrefereedpubs, [:externaluser_id, :internaluser_id, :refereedpubs_id], :name => :userrefereedpubs_refereedpubs_id_key, :unique => true
+
+     add_column :usercredits, :externaluser_id, :integer
+     add_index :userresearchgroups, [:externaluser_id, :researchgroup_id], :name => :userresearchgroups_researchgroup_id_key1, :unique => true
+   end
+end
