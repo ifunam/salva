04669f2d5fd85b33d412b07285ddb1ca129a2d8b
Developer at IFUNAM <dev@fisica.unam.mx>
2010-04-20 18:11:03 -0500

    Removing authlogic and acts_as_tree dependencies in user model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,4 @@
 class Group < ActiveRecord::Base
-  acts_as_tree 
   validates_presence_of :name
   validates_uniqueness_of :parent_id, :scope => [:name, :parent_id]
   validates_numericality_of :parent_id, :allow_nil => true, :only_integer => true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
-   validates_presence_of :login, :userstatus_id
-#  acts_as_authentic do |c|
-#    c.logged_in_timeout = 120.minutes
-#  end
+  validates_presence_of :login, :userstatus_id
   has_one :person
   has_one :address
   accepts_nested_attributes_for :person, :address
