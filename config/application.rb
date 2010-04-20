c8629fdedfe472e26edbaea03ba49320b5966f8b
Developer at IFUNAM <dev@fisica.unam.mx>
2010-04-20 18:13:10 -0500

    Configuration for rails generators in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -30,11 +30,12 @@ module Salva
     # config.i18n.default_locale = :de
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
-    # config.generators do |g|
-    #   g.orm             :active_record
-    #   g.template_engine :erb
-    #   g.test_framework  :test_unit, :fixture => true
-    # end
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :haml
+      g.test_framework  :rspec, :fixture => false, :views => false
+      g.fixture_replacement :machinist, :dir => "spec/blueprints"
+    end
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
