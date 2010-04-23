42b615d14c0044d093e5121265465f27685976eb
Ramón Martínez Olvera <martinezo@martinezo-desktop.(none)>
2010-04-23 11:23:30 -0500

    Minor changes

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Auto-require default libraries and those for the current Rails environment.
-#Bundler.require :default, Rails.env
 
 module Salva
   class Application < Rails::Application
