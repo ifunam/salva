718c467933ef0d3f413c2ab696e84faa78604fd4
Ramon Martinez <martinezo@inb.unam.mx>
2010-05-04 11:58:00 -0500

    Adding jquery and jquery-ui

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+   $('#user_person_attributes_dateofbirth').datepicker({ changeYear: true, changeMonth: true, yearRange: '1910:1995', dateFormat: 'dd-mm-yy' });
+});
