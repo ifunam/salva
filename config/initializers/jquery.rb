9db2fe0d2d9052dc92640778f83dd06b7652932a
Developer at IFUNAM <dev@fisica.unam.mx>
2010-09-07 15:16:48 -0500

    config/initializers/jquery.rb

--- config/initializers/jquery.rb
+++ config/initializers/jquery.rb
@@ -1,3 +1,3 @@
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery-1.4.2', 'rails', 'jquery.ui/jquery-ui-1.8.1.custom']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery', 'rails', 'jquery.ui/jquery-ui-1.8.1.custom']
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jrails => ['jrails.min']
 
