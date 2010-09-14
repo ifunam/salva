0d7ac2cc25e1e86dcf55f3695d2b6d976ee027df
Developer at IFUNAM <dev@fisica.unam.mx>
2010-09-14 17:31:55 -0500

    Document uploader integration with Document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,15 +1,22 @@
 class Document < ActiveRecord::Base
+  # FIXIT: Remove not null constraints for :documenttype_id, :title, :startdate, :enddate
   validates_presence_of :documenttype_id, :title, :startdate, :enddate
+
 
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_numericality_of :documenttype_id, :greater_than => 0, :only_integer => true
 
   validates_uniqueness_of  :title, :scope => [:documenttype_id]
 
+  belongs_to :document_type
+  belongs_to :user
+  mount_uploader :file, DocumentUploader
+
+  # FIXIT: Remove documenttype reference
   belongs_to :documenttype
   validates_associated :documenttype
 
+  # FIXIT: Remove user_documents and users references
   has_many :user_documents
   has_many :users, :through => :user_documents
- 
 end
--- db/migrate/20100824190644_add_schoolarship_id_to_jobpositions.rb
+++
@@ -1,9 +0,0 @@
-class AddSchoolarshipIdToJobpositions < ActiveRecord::Migration
-  def self.up
-    add_column :jobpositions, :schoolard_id, :integer
-  end
-
-  def self.down
-    remove_column :jobpositions, :schoolard_id
-  end
-end
