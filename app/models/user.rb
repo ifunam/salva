1af6377c6a5cdeb140cd21420aac9c938e210be1
Developer at IFUNAM <dev@fisica.unam.mx>
2010-09-14 17:38:37 -0500

    New methods: initialize, login_likes, friendly_email, new associations and new nested_attributes for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,31 +21,42 @@ class User < ActiveRecord::Base
   # :userstatus_id_equals => find_all_by_userstatus_id  
   scope :fullname_like, lambda { |fullname| where(" users.id IN (#{Person.find_by_fullname(fullname).select('user_id').to_sql}) ") }
   scope :adscription_id_equals, lambda { |adscription_id| joins(:user_adscriptions).where(["user_adscriptions.adscription_id = ?", adscription_id] ) }
+  scope :schoolarship_id_equals, lambda { |schoolarship_id| joins(:user_schoolarships).where(["user_schoolarships.schoolarship_id = ?", schoolarship_id] ) }
   scope :annual_report_year_equals, lambda { |year| includes(:documents).where(["documents.documenttype_id = 1 AND documents.title = ?", year]) }
-  scope :annual_plan_year_equals, lambda { |year| includes(:documents).where(["documents.documenttype_id = 2 AND documents.title = ?", year]) }
   scope :jobposition_start_date_year_equals, lambda { |year| where(" users.id IN (#{UserAdscription.by_year(year, :field => :start_date).select('user_id').to_sql}) ") }
   scope :jobposition_end_date_year_equals, lambda { |year| where(" users.id IN (#{UserAdscription.by_year(year, :field => :end_date).select('user_id').to_sql}) ") }
 
-  search_methods :fullname_like, :adscription_id_equals, :annual_report_year_equals, :annual_plan_year_equals, :jobposition_start_date_year_equals, :jobposition_end_date_year_equals
+  search_methods :fullname_like, :adscription_id_equals, :schoolarship_id_equals, :annual_report_year_equals, :jobposition_start_date_year_equals, :jobposition_end_date_year_equals
   
   belongs_to :userstatus
   belongs_to :user_incharge, :class_name => 'User', :foreign_key => 'user_incharge_id'
+
   has_one :person
-  
-  # These relationships will be changed to has_many
+  has_one :user_group
   has_one :address
   has_one :jobposition
-  has_one :jobposition_log
-  has_one :user_group
-  has_many :user_adscriptions
-  has_one  :user_adscription_as_posdoc, :class_name => 'UserAdscription', :foreign_key => 'user_id', :conditions => 'jobpositions.jobpositioncategory_id = 38 and user_adscriptions.jobposition_id = jobpositions.id', :include => [:jobposition]
-  has_many :user_documents
-  has_many :documents, :through=> :user_documents
+  has_one  :user_identification
 
-  accepts_nested_attributes_for :person, :address, :jobposition, :jobposition_log, :user_group
+  has_many :user_adscriptions
+  has_one  :user_as_posdoc_adscription, :class_name => 'UserAdscription', :conditions => 'jobpositions.jobpositioncategory_id = 38 and user_adscriptions.jobposition_id = jobpositions.id', :include => :jobposition, :order => 'user_adscriptions.start_date DESC, user_adscriptions.end_date DESC'
+  has_many :user_schoolarships, :order => 'user_schoolarships.start_date DESC, user_schoolarships.end_date DESC'
+  has_many :documents
+  accepts_nested_attributes_for :person, :address, :jobposition, :user_group, :user_identification, :user_schoolarships, :documents
   
   def self.posdoc_search(search_options={}, page=1, per_page=10)
     posdoc.fullname_asc.search(search_options).all.paginate(:page => page, :per_page => per_page)
+  end
+
+  def self.login_likes(login)
+    where(:login.matches => "%#{login.downcase}%")
+  end
+
+  def initialize(*args)
+    super
+    [:person, :user_group, :address, :jobposition, :user_identification].each do |association_name|
+      send "#{association_name}=", ActiveSupport::Inflector.camelize(association_name).constantize.new
+    end
+    jobposition.user_adscription = UserAdscription.new
   end
 
   def authorname
@@ -62,6 +73,10 @@ class User < ActiveRecord::Base
 
   def fullname_or_email
      person.nil? ? email : person.fullname
+  end
+
+  def friendly_email
+    "#{fullname_or_email} <#{email}>"
   end
 
   def user_incharge_fullname
