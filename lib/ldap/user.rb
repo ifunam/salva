3c45f7ff5cab5af45576933d05a256e1773ed376
Developer at IFUNAM <dev@fisica.unam.mx>
2010-09-28 17:25:36 -0500

    Refactoring LDAP::User implementation

--- lib/ldap/user.rb
+++ lib/ldap/user.rb
@@ -1,122 +1,115 @@
-require 'net/ldap'
+require 'digest/sha1'
 module LDAP
-  module Connection
-    def ldap_config
-      @config ||= YAML.load(ERB.new(File.read("#{Rails.root}/config/ldap.yml")).result)[Rails.env]
-    end
-
-    def ldap_connection_config
-      { 
-        :host => ldap_config['host'], :port => ldap_config['port'], :encryption => (ldap_config['tls'] ? :simple_tls : nil),
-        :auth => { :method => :simple, :username => ldap_config['username'], :password => ldap_config['password'] }
-      }
-    end
-    def ldap_connection
-      Net::LDAP.new ldap_connection_config
-    end
-  end
-  
   class User
     include ActiveModel::Validations
     include ActiveModel::Serialization
     include ActiveModel::MassAssignmentSecurity
     include ActiveModel::Dirty
-    include LDAP::Connection
-
     extend  ActiveModel::Translation
-    extend LDAP::Connection
 
-    
-    attr_accessor :login, :group, :fullname, :email, :password
-    attr_accessible :login, :group, :fullname, :email, :password
-    define_attribute_methods [:login, :group, :fullname, :email, :password]
-
-    validates_presence_of :login, :group, :fullname, :email, :password
+    attr_accessor :login, :fullname, :email, :password, :group, :new_record
+    attr_accessible :login, :fullname, :email, :password, :group
+    validates_presence_of :login, :fullname, :email, :password, :group
     validates_confirmation_of :password
 
     def self.all_by_login_likes(login)
-      filter = Net::LDAP::Filter.eq( "uid", "*#{login.downcase}*")
-      domain = ldap_config['base'].gsub(/dc\=/,'').gsub(/\,/,'.')
-      ldap_connection.search(:base => ldap_config['base'], :attributes => { :uid => login.downcase }, :filter => filter, :return_result => true ).collect do |entry|
-        user = new
-        user.login = entry.dn.split(',').first.split('=').last # FIX IT: Replace splits with a good RegExp to extract the login
-        user.email = user.login + '@' + domain 
-        user
+      filter = Net::LDAP::Filter.eq("uid", "*#{login.downcase}*")
+      self.ldap.search(:base => ldap_config['base'], :filter => filter, :return_result => true).collect do |entry|
+        entry_to_record(entry)
       end
     end
 
+    def self.find_by_login(login)
+      entry = self.ldap.search(:base => ldap_config['base'], :filter => Net::LDAP::Filter.eq("uid", login), :return_result => true ).first
+      entry_to_record(entry) unless entry.nil?
+    end
+
+    def self.uid_exist?(uid)
+      !self.find_by_login(uid).nil?
+    end
+
+    def self.ldap
+      Net::LDAP.new(:host => ldap_config['host'], :port => ldap_config['port'], :encryption => (ldap_config['ssl'] ? :simple_tls : nil),
+                    :auth => { :method => :simple, :username => ldap_config['admin_user'], :password => ldap_config['admin_password'] })
+    end
+
+    def self.ldap_config
+      YAML.load(ERB.new(File.read("#{Rails.root}/config/ldap.yml")).result)[Rails.env]
+    end
+
+    def self.entry_to_record(entry)
+      new(:login => entry.uid.first, :email => entry.mail.first, :fullname => entry.cn.first, :password => entry.userpassword.first, :group => entry.ou.first, :new_record => false)
+    end
+
     def initialize(attributes={})
-      @new_record = true
-      attributes=(attributes)
+      if attributes.has_key? :new_record
+        self.new_record = attributes[:new_record] 
+        attributes.delete(:new_record)
+      else
+        self.new_record = true
+      end
+      self.attributes=(attributes)
       self
     end
 
-    def attributes=(values)
-      sanitize_for_mass_assignment(values).each do |k, v|
-        send("#{k}=", v)
+    def attributes=(hash)
+      sanitize_for_mass_assignment(hash).each do |attribute, value|
+        send("#{attribute}=", value)
       end
     end
 
     def save
-      if valid? 
-        if new_record?
-          create
-        else
-          update
-        end
+      if valid?
+        new_record? ? create : update
       else
         false
       end
     end
-
-    def destroy
-      ldap_connection.delete(:dn => dn) unless new_record?
-    end
-
-    def new_record?
-      @new_record
-    end
-
-    private
 
     def create
-      unless uid_exist?(login)
-        ldap_connection.add(:dn => dn, :attributes => attributes)
-        @new_record = false
-        true
-      else
-        false
+      unless LDAP::User.uid_exist?(login)
+        LDAP::User.ldap.add(:dn => ldap_dn, :attributes => ldap_attributes)
+        self.new_record = false
+        return true
       end
+      return false
     end
 
     def update
-      if uid_exist?(login_was)
-        ldap_connection.modify(:dn => "uid=#{login_was},ou=#{group},#{ldap_config['base']}", :attributes => attributes)
-        true
-      else
-        false
+      if LDAP::User.uid_exist?(login)
+        LDAP::User.ldap.modify(:dn => ldap_dn, :attributes => ldap_attributes)
+        return true
       end
+      return false
     end
 
-    def uid_exist?(uid)
-      filter = Net::LDAP::Filter.eq("uid", uid)
-      ldap_connection.search(:base => ldap_config['base'], :attributes => { :uid => uid }, :filter => filter, :return_result => true ).size > 0
+    def destroy
+      LDAP::User.ldap.delete(:dn => ldap_dn) unless new_record?
     end
 
-    def dn
-      "uid=#{login},ou=#{group},#{ldap_config['base']}"
+    private
+    def new_record?
+      self.new_record
     end
 
-    def attributes
-      { 
-        :objectClass => ["inetOrgPerson", "organizationalPerson", "person", "top"],
+    def ldap_attributes
+      {
+        :objectClass => ldap_object_class,
         :cn => fullname,
         :sn => 'sn',
         :mail => email,
         :ou => group,
         :uid => login,
-        :userPassword => password
-      } 
+        :userPassword => Net::LDAP::Password.generate(:sha, password)
+      }
+    end
+
+    def ldap_dn
+      "uid=#{login},#{LDAP::User.ldap_config['base']}"
+    end
+
+    def ldap_object_class
+      LDAP::User.ldap_config['require_attribute']['objectClass'].split(',').collect {|object_class_name| object_class_name.strip }
     end
   end
 end
