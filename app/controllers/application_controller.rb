4c74b675aba23181abc27af54ed767e79cdcb93c
Developer at IFUNAM <dev@fisica.unam.mx>
2010-09-28 18:35:34 -0500

    Adding LdapException and authenticate_user filter in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
   respond_to :html
 
   protect_from_forgery
+  before_filter :authenticate_user!
 end
