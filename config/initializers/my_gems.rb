d5ff4b087aa28ee15e3de9ee47554e58b134d718
Developer at IFUNAM <dev@fisica.unam.mx>
2010-10-01 12:39:07 -0500

    Removing not used initializer file

--- config/initializers/my_gems.rb
+++
@@ -1,6 +0,0 @@
-# require 'acts-as-taggable-on'
-# # Required by tsearch plugin
-# require 'texticle'
-# require 'texticle/full_text_index'
-# require 'texticle/parser'
-# ActiveRecord::Base.extend(Texticle)
