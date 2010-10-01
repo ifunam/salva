390e611a601fbcd5e09d0b7be7ead1770bc84ec6
Developer at IFUNAM <dev@fisica.unam.mx>
2010-10-01 14:25:16 -0500

    Refactoring notifier: Replacing the use of instanced objects  as parameter for all methods, We will use integers or hashes to create background jobs, placing those jobs on multiple queues, and processing them later with workers based on Resque

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,35 +3,37 @@ class Notifier < ActionMailer::Base
 
   default :from => "salva@fisica.unam.mx"
 
-  def new_user_to_admin(user)
-    # You should resist the temptation to pass database-backed objects as parameters 
-    # in your mailer and instead pass record identifiers, because database objects 
-    # couldn't be handled by Resque and Redis
-    @user =  hash_to_record(user)
+  def new_user_to_admin(user_id)
+    @user =  User.find(user_id)
     mail(:to => 'alex@fisica.unam.mx', :cc => 'vic@fisica.unam.mx, cuentas@fisica.unam.mx',
          :subject => 'SALVA - Nuevo usuario') do |format|
       format.text
     end
   end
 
-  def ldap_errors_to_admin(user)
-    @user = hash_to_record(user)
+  def ldap_errors_to_admin(user_id)
+    @user = User.find(user_id)
     mail(:to => 'vic@fisica.unam.mx', :cc => 'alex@fisica.unam.mx',
          :subject => 'SALVA - Errores al crear usuario en LDAP') do |format|
       format.text
     end
   end
 
-  def deleted_user_to_admin(record)
-    @user = (record.is_a? Hash) ? User.new(:login => record['user']['login']) : record
+  def deleted_user_to_admin(login)
+    @login = login
     mail(:to => 'alex@fisica.unam.mx', :cc => 'vic@fisica.unam.mx, cuentas@fisica.unam.mx',
          :subject => 'SALVA - Usuario borrado') do |format|
       format.text
     end
   end
 
-  def hash_to_record(record)
-    (record.is_a? Hash) ? User.find(record['user']['id']) : record
+  def updated_userstatus_to_admin(user_id)
+    @user =  User.find(user_id)
+    mail(:to => 'alex@fisica.unam.mx', :cc => 'vic@fisica.unam.mx, cuentas@fisica.unam.mx',
+         :subject => 'SALVA - Estado de usuario actualizado') do |format|
+      format.text
+    end
   end
+
   # TODO: Implement the welcome message for new users
 end
--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,19 +1,24 @@
 class UserObserver < ActiveRecord::Observer
-  def after_save(user)
+  def after_create(user)
     @new_user = User.find(user.id)
     @ldap_user = LDAP::User.new(:login => @new_user.login, :group => @new_user.adscription_abbrev, :fullname => @new_user.fullname_or_email, 
                                 :email => @new_user.email, :password => user.password, :password_confirmation => user.password)
     if @ldap_user.valid?
       @ldap_user.save
-      Notifier.new_user_to_admin(@new_user).deliver
+      Notifier.new_user_to_admin(@new_user.id).deliver
     else
-      Notifier.ldap_errors_to_admin(@new_user).deliver
+      Notifier.ldap_errors_to_admin(@new_user.id).deliver
     end
+  end
+  
+  def before_update(user)
+    Notifier.updated_userstatus_to_admin(user.id).deliver if user.userstatus_id_changed?
+    # TODO: Add LDAP code to update the user status 
   end
   
   def after_destroy(user)
     @ldap_user = LDAP::User.find_by_login(user.login)
     @ldap_user.destroy unless @ldap_user.nil?
-    Notifier.deleted_user_to_admin(user).deliver
+    Notifier.deleted_user_to_admin(user.login).deliver
   end
 end
