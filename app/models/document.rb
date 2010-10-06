68e9c785b972c81b186e5801292af3698dd63425
Developer at IFUNAM <dev@fisica.unam.mx>
2010-10-05 19:42:14 -0500

    Disabling old validations in document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,12 +1,9 @@
 class Document < ActiveRecord::Base
   # FIXIT: Remove not null constraints for :documenttype_id, :title, :startdate, :enddate
-  validates_presence_of :documenttype_id, :title, :startdate, :enddate
-
+  validates_presence_of :document_type_id
 
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_numericality_of :documenttype_id, :greater_than => 0, :only_integer => true
-
-  validates_uniqueness_of  :title, :scope => [:documenttype_id]
+  validates_numericality_of :document_type_id, :greater_than => 0, :only_integer => true
 
   belongs_to :document_type
   belongs_to :user
