693622f69e191cf3f73b1e40440d2e18a5a2f566
Developer at IFUNAM <dev@fisica.unam.mx>
2010-10-05 19:45:31 -0500

    Adding carrierwave initializer and gridfs support for document_uploader

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -3,6 +3,7 @@
 class DocumentUploader < CarrierWave::Uploader::Base
 
   storage :file
+  # storage :grid_fs
 
   # Override the directory where uploaded files will be stored
   # This is a sensible default for uploaders that are meant to be mounted:
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,4 @@
+CarrierWave.configure do |config|
+  # config.grid_fs_database = 'salva'
+  # config.grid_fs_host = 'db.fisica.unam.mx'
+end
