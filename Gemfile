877cc469c5be70531ad7d0f10a5b15f4afff6df6
Developer at IFUNAM <dev@fisica.unam.mx>
2010-10-14 18:39:39 -0500

    Updating to rails 3.0.1

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # Edit this Gemfile to bundle your application's dependencies.
 source :gemcutter
  
-gem "rails", "3.0.0"
+gem "rails", "3.0.1"
 # Views, css and javascript
 gem "compass", "0.10.5"
 gem "show_for", ">= 0.2.3"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,9 @@ Salva::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
-  
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
+
