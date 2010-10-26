522b38d69a78ee4e9e057f9be0d4aa4f8999f308
Developer at IFUNAM <dev@fisica.unam.mx>
2010-10-26 18:30:13 -0500

    Adding MVC for admin/schoolarships based on admin_super_catalog_controller class

--- app/models/schoolarship.rb
+++ app/models/schoolarship.rb
@@ -8,8 +8,18 @@ class Schoolarship < ActiveRecord::Base
   belongs_to :institution
   validates_associated :institution
 
+  search_methods :name_likes
+
+  default_scope :order => 'name ASC'
+
+  def self.name_likes(name)
+    where('LOWER(schoolarships.name) LIKE ?', "%#{name.downcase}%")
+  end
+
   def name_and_institution_abbrev
      institution.nil? ? name : [name, institution.abbrev].join(' - ')
   end
+  
+  has_many :user_schoolarships
 end
 
