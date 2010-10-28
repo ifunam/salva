fe472c10c5176750635fbd83217aa250836cd24b
Ramón Martínez Olvera <martinezo@developer-vi.(none)>
2010-10-28 10:10:36 -0500

    Adding MVC admin/seminarytypes based on SuperCatalogController class

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Salva::Application.routes.draw do
   namespace :admin do
     resources :users
     resources :catalogs
-    super_catalog_for :cities, :states, :journals, :publishers, :adscriptions, :researchareas, :schoolarships, :institutions, :periods
+    super_catalog_for :cities, :states, :journals, :publishers, :adscriptions, :researchareas, :schoolarships, :institutions, :periods, :seminarytypes
   end
 
   namespace :academic_secretary do
