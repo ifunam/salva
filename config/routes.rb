dba943b85988b6f19085382f13bc29c5e92b8eff
Ramón Martínez Olvera <martinezo@developer-vi.(none)>
2010-11-09 17:25:19 -0600

    Adding MVC admin/activitytypes based on SuperController class

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Salva::Application.routes.draw do
       get :user_incharge, :on => :member
     end
     resources :catalogs
-    super_catalog_for :cities, :states, :journals, :publishers, :adscriptions, :researchareas, :schoolarships, :institutions, :periods, :seminarytypes, :prizetypes, :careers, :institutiontypes, :institutiontitles
+    super_catalog_for :cities, :states, :journals, :publishers, :adscriptions, :researchareas, :schoolarships, :institutions, :periods, :seminarytypes, :prizetypes, :careers, :institutiontypes, :institutiontitles, :activitytypes
   end
 
   namespace :academic_secretary do
