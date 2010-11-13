7bd7e64fdcb5bda8921f2cd7f20ed54a70606eca
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-13 14:47:29 -0600

    Using methods to generate routes for controllers using catalog and publication controller classes

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,8 @@ Salva::Application.routes.draw do
   resources :documents
   resources :user_schoolarships
 
-  publication_resources_for :articles, :unpublished_articles
-
-  resources :journals do
-    get :search_by_name, :on => :collection
-    get :autocomplete_form, :on => :collection
-  end
+  publication_resources_for :articles, :unpublished_articles, :popular_science_works
+  catalog_resources_for :journals, :publishers, :institutions
 
   namespace :admin do
     resources :users  do
