294a0c9b84a47d1c931e248a9af0a6f4278a2a08
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-13 14:44:49 -0600

    JournalsController based on CatalogController

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,16 +1,9 @@
-class JournalsController < InheritedResources::Base
-  respond_to :json, :only => [:search_by_name]
-  respond_to :js, :only => [:autocomplete_form, :create, :new, :show]
+class JournalsController < CatalogController 
 
   def search_by_name
     @records = Journal.search(:name_starts_with => params[:term]).all
     @results = @records.collect { |record| {:id => record.id, :value => record.name, :label => record.name} }
     render :json => @results
   end
-  
-  def create
-     create! do |format|
-       format.js { render :action => 'show.js' }
-     end
-  end
+
 end
