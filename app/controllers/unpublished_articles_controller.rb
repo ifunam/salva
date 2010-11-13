b95083fd1094a4ea18e7288bdadcf4afba9f6d85
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-13 14:52:11 -0600

    Removing not necesary code for index and not_mine actions in unpublished_articles_controller

--- app/controllers/unpublished_articles_controller.rb
+++ app/controllers/unpublished_articles_controller.rb
@@ -1,15 +1,4 @@
 class UnpublishedArticlesController < PublicationController
-  defaults :user_role_class => :user_articles, :resource_class => Article, :collection_name => 'articles', :instance_name => 'article'
-
-  def index
-    params[:search] ||= {}
-    params[:search].merge!(:user_id_eq => current_user.id)
-    respond_with (@articles = Article.unpublished.paginated_search(params))
-  end
-
-  def not_mine
-    params[:search] ||= {}
-    params[:search].merge!(:user_id_not_eq => current_user.id)
-    respond_with (@articles = Article.unpublished.paginated_search(params))
-  end
+  defaults :user_role_class => :user_articles, :resource_class => Article, :collection_name => 'articles', 
+           :instance_name => 'article', :resource_class_scope => :unpublished
 end
