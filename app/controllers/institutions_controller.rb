a4a0c37204c2da7d39d26eca16cd8084b708f4ca
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-13 14:51:09 -0600

    Adding MVC for institutions based on catalog_controller

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,20 +1,9 @@
-class InstitutionsController < InheritedResources::Base
-  respond_to :json, :only => [:autocomplete_name]
-  respond_to :js, :only => [:show, :new]
+class InstitutionsController < CatalogController
 
-  def autocomplete_name
+  def search_by_name
     @records = Institution.search_for_autocomplete params[:term]
     @results = @records.collect { |record| {:id => record.id, :value => record.name, :label => record.name} }
     render :json => @results
   end
-  
-  def autocomplete_form
-     render :action => 'autocomplete_form.js'
-  end
-  
-  def create
-    create! do |format|
-      format.js { render :action => 'create.js' }
-    end
-  end
+
 end
