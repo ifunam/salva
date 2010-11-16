449409e7a476387f862a4c9528f76d69c927f377
Ramón Martínez Olvera <martinezo@developer-vi.(none)>
2010-11-16 10:06:52 -0600

    Adding MVC admin/degrees based on SuperCatalogController class

---
+++ app/controllers/admin/degrees_controller.rb
@@ -0,0 +1,2 @@
+class Admin::DegreesController < Admin::SuperCatalogController
+end
--- app/models/degree.rb
+++ app/models/degree.rb
@@ -2,5 +2,9 @@ class Degree < ActiveRecord::Base
   validates_presence_of :name
   validates_numericality_of :id, :allow_nil => true, :greater_than =>0, :only_integer => true
   validates_uniqueness_of :name
-  has_many :careers, :through => :careers
+  #has_many :careers, :through => :careers
+  has_many :careers
+  has_many :indivadvices
+  #has_many :externalusers
+  has_many :tutorial_committees
 end
