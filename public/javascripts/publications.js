c76e11df7019138732790f8583ce033cf9f969d5
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-15 21:32:10 -0600

    New publication.js to support generic autocomplete functionality in select elements and to include generic dialogs for remote forms

--- public/javascripts/publications.js
+++ public/javascripts/publications.js
@@ -1,3 +1,54 @@
+(function( $ ) {
+        $.widget( "ui.combobox", {
+            _create: function() {
+                var self = this,
+                    select = this.element.hide(),
+                    selected = select.children( ":selected" ),
+                    value = selected.val() ? selected.text() : "";
+                var controller_name = self.element.context.parentNode.getAttribute('data-controller-name');
+                var input = $( "<input size=40>" )
+                    .insertAfter( select )
+                    .val( value )
+                    .autocomplete({
+                        delay: 3,
+                        minLength: 1,
+                        source: '/'+controller_name+'/autocompleted_search',
+                        select: function( event, ui ) {
+                            select.children().remove().end().append('<option selected value="'+ui.item.id+'">'+ui.item.label+'</option>');
+                        },
+                        change: function( event, ui ) {
+                            if ( !ui.item ) {
+                                var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i" ),
+                                    valid = false;
+                                select.children( "option" ).each(function() {
+                                    if ( this.value.match( matcher ) ) {
+                                        this.selected = valid = true;
+                                        return false;
+                                    }
+                                });
+                                if ( !valid ) {
+                                    // remove invalid value, as it didn't match anything
+                                    $( this ).val( "" );
+                                    select.val( "" );
+                                    return false;
+                                }
+                            }
+                        }
+                    })
+                    .addClass( "ui-widget ui-widget-content ui-corner-left" );
+
+                input.data( "autocomplete" )._renderItem = function( ul, item ) {
+                    return $( "<li></li>" )
+                        .data( "item.autocomplete", item )
+                        .append( "<a>" + item.label + "</a>" )
+                        .appendTo( ul );
+                };
+
+
+            }
+        });
+})( jQuery );
+
 $(document).ready(function() {
     $(".associated_authors a").live("click", function() {
         publication_id = this.id;
@@ -54,69 +105,30 @@ $(document).ready(function() {
         return false;
     });
 
-    $('#autocomplete_journal_name').live('focus', function(){
-        search_by_journal_name();
-    });
-
-    $('#autocomplete_filter_journal_name').live('focus', function(){
-        filter_by_journal_name();
-    });
-
-    $('.change_record').live('click', function() {
-        change_record(this.getAttribute('data-class-name'));
-    });
-
     $("#new_record").live('click', function() {
-        dialog_for_new_record(this.getAttribute('data-controller-name'));
+        dialog_for_new_record(this.parentNode.getAttribute('data-controller-name'));
     });
 
     $('#new_record_form').live('submit', function() {
         class_name = this.getAttribute('data-class-name');
         $("#new_record_form").ajaxComplete(function(event, request, settings){
             object = jQuery.parseJSON(request.responseText);
-            $("#autocomplete_"+class_name+"_name").val(eval('object.'+class_name+'.name'));
-            $("#"+class_name).find("input[type=hidden]").val(eval('object.'+class_name+'.id'));
-            $("#autocomplete_"+class_name+"_name").attr("disabled", "disabled");
-            $('#change_'+class_name).show();
+            label = eval('object.'+class_name+'.name');
+            value = eval('object.'+class_name+'.id');
+            html_options = '<option selected value="'+value+'" >'+label+'</option>';
+            $("#"+class_name).find('select').children().remove().end().append(html_options);
+            $("#"+class_name).find('input').val(label);
             $('#dialog').dialog('close');
         });
     });
+    $( ".autocompleted_select" ).combobox();
+
+    $('#autocomplete_filter_journal_name').live('focus', function(){
+        filter_by_journal_name();
+    });
+
     set_button_behaviour();
 });
-
-function search_by_journal_name() {
-    $('#autocomplete_journal_name').autocomplete({
-        source: '/journals/search_by_name',
-        minLength: 1,
-        delay: 0,
-        select: function(event, ui) {
-            $('#article_journal_id').val(ui.item.id);
-            $('#autocomplete_journal_name').attr("disabled", "disabled");
-            $('#change_journal').show();
-        }
-    });
-}
-
-
-function filter_by_journal_name() {
-    $('#autocomplete_filter_journal_name').autocomplete({
-        source: '/journals/search_by_name',
-        minLength: 1,
-        delay: 0,
-        select: function(event, ui) { 
-            $('#search_journal_id_eq').val(ui.item.id);
-        }
-    });
-}
-
-function show_record(id) {
-    $.ajax({
-        url: "/journals/" + id + '.js',
-        success: function(request) {
-            $("#journal").html(request); 
-        },
-    });
-}
 
 function dialog_for_new_record(controller) {
     $('#dialog').dialog({title:'Nuevo registro', width: 400, height: 320}).dialog('open');
@@ -132,4 +144,15 @@ function change_record(class_name) {
     $('#autocomplete_'+class_name+'_name').removeAttr("disabled");
     $('#autocomplete_'+class_name+'_name').val('');
     $('#change_'+class_name).hide();
+}
+
+function filter_by_journal_name() {
+    $('#autocomplete_filter_journal_name').autocomplete({
+        source: '/journals/search_by_name',
+        minLength: 1,
+        delay: 0,
+        select: function(event, ui) { 
+            $('#search_journal_id_eq').val(ui.item.id);
+        }
+    });
 }
