8044809435e90e6f8e2327c544b4c15303ddd3d3
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-16 01:25:14 -0600

    Add role_list method and renaming author_list, add_author and del_author methods to user_list, add_user and del_user methods in PublicationController class

--- app/controllers/publication_controller.rb
+++ app/controllers/publication_controller.rb
@@ -2,18 +2,19 @@ class PublicationController < InheritedResources::Base
   layout 'publications'
 
   respond_to :html
-  respond_to :js, :only => [:index, :not_mine, :author_list, :add_author, :del_author]
+  respond_to :js, :only => [:index, :not_mine, :user_list, :add_user, :del_user, :role_list]
 
   # Overwritting defaults method 
-  class_inheritable_accessor :user_role_class, :resource_class_scope
+  class_inheritable_accessor :user_role_class, :resource_class_scope, :role_class
 
   def self.defaults(options)
-    [:user_role_class, :resource_class_scope].each do |k|
+    [:user_role_class, :resource_class_scope, :role_class].each do |k|
       self.set_default(options, k)
     end
+    self.role_list_method unless self.role_class.nil?
     super
-  end  
-  
+  end
+
   def self.set_default(options, key)
     if options.has_key? key
       self.send("#{key}=", options[key]) 
@@ -21,11 +22,17 @@ class PublicationController < InheritedResources::Base
     end
   end
 
+  def self.role_list_method
+    define_method :role_list do
+      resource
+      respond_with(instance_variable_set("@#{self.role_class.to_s.pluralize}", self.role_class.to_s.classify.constantize.all))
+    end
+  end
+
   def index
     params[:search] ||= {}
     params[:search].merge!(:user_id_eq => current_user.id)
     respond_with set_collection_ivar(scoped_resource_class.paginated_search(params))
-    
   end
 
   def not_mine
@@ -43,23 +50,23 @@ class PublicationController < InheritedResources::Base
     resource.modified_by_id = current_user.id
     super
   end
-  
-  def author_list
+
+  def user_list
     respond_with(resource)
   end
 
-  def add_author  
-    resource_user_role.create(:user_id => current_user.id)
+  def add_user
+    resource_with_user_role.create(user_role_attributes)
     respond_with(resource)
   end
 
-  def del_author
-    resource_user_role.where(:user_id => current_user.id).first.destroy
+  def del_user
+    resource_with_user_role.where(:user_id => current_user.id).first.destroy
     respond_with(resource)
   end
 
   protected
-  def resource_user_role
+  def resource_with_user_role
     resource.send(self.user_role_class) 
   end
 
@@ -68,6 +75,14 @@ class PublicationController < InheritedResources::Base
   end
 
   def scoped_resource_class
-     has_resource_class_scope? ? self.resource_class.send(self.resource_class_scope) : self.resource_class
+    has_resource_class_scope? ? self.resource_class.send(self.resource_class_scope) : self.resource_class
+  end
+
+  def user_role_attributes
+    { :user_id => current_user.id }.merge(role_attributes)
+  end
+
+  def role_attributes
+    self.role_class.nil? ? { } : { self.role_class.to_s.classify.foreign_key => params[self.role_class.to_s.classify.foreign_key.to_sym] }
   end
 end
