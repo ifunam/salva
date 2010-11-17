6bd9284d6e19f50c0678d2880e5a541412c9bd98
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-17 11:51:16 -0600

    Adding support for scopes of models in user_resources_controller

--- app/controllers/user_resources_controller.rb
+++ app/controllers/user_resources_controller.rb
@@ -3,11 +3,20 @@ class UserResourcesController < InheritedResources::Base
 
   respond_to :html
   respond_to :js, :only => [:index]
+  class_inheritable_accessor :resource_class_scope
+
+  def self.defaults(options)
+    if options.has_key? :resource_class_scope
+      self.resource_class_scope =  options[:resource_class_scope]
+      options.delete :resource_class_scope
+    end
+    super
+  end
 
   def index
     params[:search] ||= {}
     params[:search].merge!(:user_id_eq => current_user.id)
-    respond_with set_collection_ivar(self.resource_class.search(params[:search]).paginate(:page => params[:page] || 1, :per_page => params[:per_page] || 10))
+    respond_with set_collection_ivar(scoped_resource_class.search(params[:search]).paginate(:page => params[:page] || 1, :per_page => params[:per_page] || 10))
   end
 
   def create
@@ -25,5 +34,14 @@ class UserResourcesController < InheritedResources::Base
     if resource.registered_by_id == current_user.id
       super
     end
+  end
+
+  protected
+  def has_resource_class_scope?
+    !self.resource_class_scope.nil?
+  end
+
+  def scoped_resource_class
+    has_resource_class_scope? ? self.resource_class.send(self.resource_class_scope) : self.resource_class
   end
 end
