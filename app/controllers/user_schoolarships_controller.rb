86ee67f83f177ceda5a165feeaf034bd012016bb
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-17 10:29:47 -0600

    Adding user_schoolarships MVC based in user_resources_controller class

--- app/controllers/user_schoolarships_controller.rb
+++ app/controllers/user_schoolarships_controller.rb
@@ -1,8 +1,2 @@
-class UserSchoolarshipsController < ApplicationController
-  respond_to :xml
-  def destroy
-    @user_schoolarship = UserSchoolarship.find(params[:id])
-    @user_schoolarship.destroy
-    respond_with(@user_schoolarship)
-  end
+class UserSchoolarshipsController < UserResourcesController
 end
