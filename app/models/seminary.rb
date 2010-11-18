0181460f84d66b2a50318ea4c304b9a218d6bd01
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-18 13:59:55 -0600

    Adding registered and modified associations, and instructors attribute to seminaries table. Adding as_text method for seminary model

--- app/models/seminary.rb
+++ app/models/seminary.rb
@@ -1,12 +1,28 @@
 class Seminary < ActiveRecord::Base
- validates_presence_of :title, :year, :seminarytype_id
- validates_numericality_of :seminarytype_id, :year,  :greater_than =>0, :only_integer => true
- validates_numericality_of :id, :month, :allow_nil => true, :greater_than =>0, :only_integer => true
+  validates_presence_of  :title, :year, :seminarytype_id
+  validates_numericality_of :seminarytype_id, :year,  :greater_than =>0, :only_integer => true
+  validates_numericality_of :id, :month, :allow_nil => true, :greater_than =>0, :only_integer => true
 
- #validates_uniqueness_of :title, :scope => [:year, :institution_id]
+  belongs_to :institution
+  belongs_to :seminarytype
+  belongs_to :registered_by, :class_name => 'User'
+  belongs_to :modified_by, :class_name => 'User'
+  has_many   :user_seminaries
 
- belongs_to :institution
- belongs_to :seminarytype
- validates_associated :institution
+  def as_text
+    [instructors, title, 'Tipo: ' + seminarytype.name, organizers, date].compact.join(', ')
+  end
 
+  def organizers
+    organizers = user_seminaries.where(:roleinseminary_id => 3)
+    'Organizador(es): ' + organizers.collect {|record| record.user.author_name }.join(', ') if organizers.size > 0
+  end
+
+  def date(format=:month_and_year)
+    if !year.nil? and !month.nil?
+      I18n.localize(Date.new(year, month, 1), :format => format).downcase
+    elsif !year.nil?
+      year
+    end
+  end
 end
