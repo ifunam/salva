53eb6d43681f5bd51d6da396fc582930be0c6cd4
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-19 11:59:08 -0600

    Removing duplicated code from newspaperarticle and using meta_user_association extension

--- app/models/newspaperarticle.rb
+++ app/models/newspaperarticle.rb
@@ -11,6 +11,7 @@ class Newspaperarticle < ActiveRecord::Base
   has_many :user_newspaperarticles
   has_many :users, :through => :user_newspaperarticles
   accepts_nested_attributes_for :user_newspaperarticles
+  user_association_methods_for :user_newspaperarticles
 
   default_scope :order => 'newsdate DESC, authors ASC, title ASC'
 
@@ -21,14 +22,6 @@ class Newspaperarticle < ActiveRecord::Base
   search_methods :user_id_eq, :user_id_not_eq, :year_eq
 
   def as_text
-    [authors, title, newspaper.name_and_country, pages, I18n.localize(newsdate, :format => :long_without_day) ].compact.join(', ')
-  end
-
-  def has_associated_users?
-     users.size > 0
-  end
-
-  def has_association_with_user?(user_id)
-     !user_newspaperarticles.where(:user_id => user_id).first.nil?
+    [authors, title, newspaper.name_and_country, pages, I18n.localize(newsdate, :format => :long_without_day)].compact.join(', ')
   end
 end
