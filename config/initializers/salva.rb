a1be20e0c8b028acced1906266e8664cc0cac81a
Developer at IFUNAM <dev@fisica.unam.mx>
2010-11-19 11:42:34 -0600

    Adding meta_user_association ActiveRecord Extension

--- config/initializers/salva.rb
+++ config/initializers/salva.rb
@@ -1,4 +1,4 @@
-%w(site_config routes meta_search_extension meta_date_extension).each do |file|
+%w(site_config routes meta_search_extension meta_date_extension meta_user_association).each do |file|
   file_path = Rails.root.to_s + '/lib/salva/' + file
   require file_path if File.exist? "#{file_path}.rb"
 end
---
+++ lib/salva/meta_user_association.rb
@@ -0,0 +1,24 @@
+module MetaUserAssociation
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def user_association_methods_for(association_name)
+      if (self.reflect_on_all_associations(:has_many).collect(&:name) & [association_name.to_sym, :users]).size == 2 
+        self.class_eval do
+          define_method 'has_users?' do 
+            users.size > 0
+          end
+          define_method 'has_user_id?' do |user_id|
+             !send("#{association_name.to_sym}").where(:user_id => user_id).first.nil?
+          end
+        end
+      else
+        warn "You must define has_many relation for :#{association_name} and has_many :users, :through => :#{association_name}"
+      end
+    end
+  end
+end
+ActiveRecord::Base.send :include, MetaUserAssociation
