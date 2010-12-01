915962a97399729b99a547b1535e4196103cbc41
Developer at IFUNAM <dev@fisica.unam.mx>
2010-12-01 11:58:06 -0600

    Adding as_attendee scope in seminary model

--- app/controllers/seminaries_controller.rb
+++ app/controllers/seminaries_controller.rb
@@ -1,3 +1,3 @@
 class SeminariesController < PublicationController
-  defaults :user_role_class => :user_seminaries, :role_class => :roleinseminary, :resource_class_scope => :not_attendee
+  defaults :user_role_class => :user_seminaries, :role_class => :roleinseminary, :resource_class_scope => :as_not_attendee
 end
--- app/models/seminary.rb
+++ app/models/seminary.rb
@@ -7,7 +7,7 @@ class Seminary < ActiveRecord::Base
   belongs_to :seminarytype
   belongs_to :registered_by, :class_name => 'User'
   belongs_to :modified_by, :class_name => 'User'
-  
+
   has_many :user_seminaries
   has_many :users, :through => :user_seminaries
   accepts_nested_attributes_for :user_seminaries
@@ -15,10 +15,10 @@ class Seminary < ActiveRecord::Base
 
   default_scope :order => 'year DESC, month DESC, instructors ASC, title ASC'
 
-  scope :not_attendee, joins(:user_seminaries).where('user_seminaries.roleinseminary_id != 1')
+  scope :as_not_attendee, joins(:user_seminaries).where('user_seminaries.roleinseminary_id != 1')
+  scope :as_attendee, joins(:user_seminaries).where('user_seminaries.roleinseminary_id != 1')
   scope :user_id_eq, lambda { |user_id| joins(:user_seminaries).where(:user_seminaries => {:user_id => user_id}) }
   scope :user_id_not_eq, lambda { |user_id|  where("seminaries.id IN (#{UserSeminary.select('DISTINCT(seminary_id) as seminary_id').where(["user_seminaries.user_id !=  ?", user_id]).to_sql}) AND seminaries.id  NOT IN (#{UserSeminary.select('DISTINCT(seminary_id) as seminary_id').where(["user_seminaries.user_id =  ?", user_id]).to_sql})") }
-
   search_methods :user_id_eq, :user_id_not_eq
 
   def as_text
