5fd1d54dd5d6200b06639688e7d67c06192ecc79
Developer at IFUNAM <dev@fisica.unam.mx>
2010-12-13 01:19:58 -0600

    Disabling tls in mail initializer and minor change

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,6 +2,7 @@ if Rails.env.to_s != 'test'
   Salva::Application.configure do
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = { 
+      :enable_starttls_auto => false,
       :address => 'fenix.fisica.unam.mx',
       :port => 25,
       :domain => 'fisica.unam.mx',
