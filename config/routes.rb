1accf052f3d0fd299b519d918ee83ed9894490a9
Ramón Martínez Olvera <martinezo@developer-vi.(none)>
2010-12-28 14:50:52 -0600

    Adding MVC admin/thesismodalities based on SuperCatalogController class

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Salva::Application.routes.draw do
                       :roleincourses, :roles, :roleinjobpositions, :roleinjournals, :roleinjuries,
                       :roleinprojects, :roleinregularcourses, :roleinseminaries, :roleintheses,
                       :roleproceedings, :stimulustypes, :stimuluslevels, :studentroles, :talktypes,
-                      :techproductstatuses, :titlemodalities, :thesisstatuses
+                      :techproductstatuses, :titlemodalities, :thesisstatuses, :thesismodalities
   end	
 
   namespace :academic_secretary do
