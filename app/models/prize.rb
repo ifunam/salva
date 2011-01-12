609030800ca4cb25161e55fe7a8109521c94a51f
Developer at IFUNAM <dev@fisica.unam.mx>
2011-01-12 11:50:25 -0600

    Adding MVC for user_prizes

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -1,9 +1,16 @@
  class Prize < ActiveRecord::Base
-  validates_numericality_of :id, :allow_nil => true,  :greater_than =>0, :only_integer => true
-  validates_numericality_of :id, :allow_nil => false,  :greater_than =>0, :only_integer =>true
+  validates_numericality_of :id, :allow_nil => true,  :greater_than => 0, :only_integer => true
   validates_presence_of :name
+
   belongs_to :prizetype
+  belongs_to :registered_by, :class_name => 'User', :foreign_key => 'registered_by_id'
+  belongs_to :modified_by, :class_name => 'User', :foreign_key => 'modified_by_id'
   belongs_to :institution
 
-  validates_associated :prizetype
+  has_many :user_prizes
+  has_many :users, :through => :user_prizes
+
+  def as_text
+    ["#{prizetype.name}: #{name}", "Otorgado por: #{institution.name}"].join(', ')
+  end
 end
--- app/models/user_prize.rb
+++ app/models/user_prize.rb
@@ -1,11 +1,17 @@
 class UserPrize < ActiveRecord::Base
   validates_presence_of  :year
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
-  validates_numericality_of :user_id, :year,:greater_than => 0, :only_integer => true
-  #validates_uniqueness_of :user_id, :scope => [:prize_id, :year]
+  validates_numericality_of :user_id, :year, :greater_than => 0, :only_integer => true
 
   belongs_to :prize
-  belongs_to :user
+  accepts_nested_attributes_for :prize
 
-  validates_associated :prize
+  belongs_to :user
+  belongs_to :registered_by, :class_name => 'User', :foreign_key => 'registered_by_id'
+  belongs_to :modified_by, :class_name => 'User', :foreign_key => 'modified_by_id'
+  default_scope :order => 'year DESC, month DESC'
+
+  def as_text
+    [prize.as_text, date].join(', ')
+  end
 end
