a0063b7da7377276f9d8c75ab9522ce07cffcd7f
Developer at IFUNAM <dev@fisica.unam.mx>
2011-01-21 10:24:38 -0600

    Migration to move institutioncareer records to education and career tables

--- app/models/education.rb
+++ app/models/education.rb
@@ -6,13 +6,15 @@ class Education < ActiveRecord::Base
   validates_inclusion_of :is_titleholder, :in => [true, false], :if => lambda { |record| record.is_studying_this == false}
   validates_inclusion_of :is_studying_this, :in => [true, false], :if => lambda { |record| record.is_titleholder == false}
 
-  belongs_to :institutioncareer
   belongs_to :career
   accepts_nested_attributes_for :career
   belongs_to :user
   belongs_to :registered_by, :class_name => 'User', :foreign_key => 'registered_by_id'
   belongs_to :modified_by, :class_name => 'User', :foreign_key => 'modified_by_id'
 
+  belongs_to :institutioncareer # FIX IT: Remove this relationship for next release
+                                # because we need institutioncareers table to support
+                                # migrations of previous versions of salva databases.
   default_scope :order => 'endyear DESC, startyear DESC'
 
   def as_text
