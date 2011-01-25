108b7b55e221d292e6d25584bde4b08aa2182333
Developer at IFUNAM <dev@fisica.unam.mx>
2011-01-25 13:10:39 -0600

    Adding role_and_period method for user_regularcourse model

--- app/models/user_regularcourse.rb
+++ app/models/user_regularcourse.rb
@@ -3,16 +3,18 @@ class UserRegularcourse < ActiveRecord::Base
   validates_numericality_of :id, :allow_nil => true, :only_integer => true
   validates_numericality_of :period_id, :roleinregularcourse_id, :greater_than => 0, :only_integer => true
   validates_numericality_of :hoursxweek, :allow_nil => true, :greater_than => 0 , :only_integer => true
-  validates_inclusion_of :hoursxweek, :in => 1..40, :allow_nil => true
-
+  validates_inclusion_of    :hoursxweek, :in => 1..40, :allow_nil => true
 
   belongs_to :regularcourse
-  accepts_nested_attributes_for :regularcourse
   belongs_to :period
   belongs_to :roleinregularcourse
   belongs_to :user
 
   def as_text
     ["#{roleinregularcourse.name}: #{regularcourse.as_text}", "Horas por semana: #{hoursxweek}", "Periodo escolar: #{period.title}"].join(', ')
+  end
+
+  def role_and_period
+    ["Rol: #{roleinregularcourse.name}", "Periodo escolar: #{period.title}"].join(', ')
   end
 end
