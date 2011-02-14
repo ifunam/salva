c578a41b4ab25d1daa37dece80eff53c57e4eff0
Developer at IFUNAM <dev@fisica.unam.mx>
2011-02-14 11:53:31 -0600

    Enabling attribute_normalizers and meta_search extensions after SALVA has finished initializing

--- config/initializers/attribute_normalizer.rb
+++ config/initializers/attribute_normalizer.rb
@@ -1,30 +1,34 @@
-AttributeNormalizer.configure do |config|
-  config.normalizers[:currency] = lambda do |value, options|
-    value.is_a?(String) ? value.gsub(/[^0-9\.]+/, '') : value
-  end
+Salva::Application.configure do
+  config.after_initialize do
+    AttributeNormalizer.configure do |attr_config|
+      attr_config.normalizers[:currency] = lambda do |value, options|
+        value.is_a?(String) ? value.gsub(/[^0-9\.]+/, '') : value
+      end
 
-  config.normalizers[:truncate] = lambda do |text, options|
-    if text.is_a?(String)
-      options.reverse_merge!(:length => 30, :omission => "...")
-      l = options[:length] - options[:omission].mb_chars.length
-      chars = text.mb_chars
-      (chars.length > options[:length] ? chars[0...l] + options[:omission] : text).to_s
-    else
-      text
+      attr_config.normalizers[:truncate] = lambda do |text, options|
+        if text.is_a?(String)
+          options.reverse_merge!(:length => 30, :omission => "...")
+          l = options[:length] - options[:omission].mb_chars.length
+          chars = text.mb_chars
+          (chars.length > options[:length] ? chars[0...l] + options[:omission] : text).to_s
+        else
+          text
+        end
+      end
+
+      attr_config.normalizers[:text_line] = lambda do |value, options|
+        value.is_a?(String) ? value.tr("\n",'').tr("\r",'').sub(/^('|"|`|\s)+/, '').sub(/('|"|`|\s|\.|,|;)+$/, '').sub(/\s{1}+/, ' ') : value
+      end
+
+      attr_config.normalizers[:paragraph] = lambda do |value, options|
+        value.is_a?(String) ? value.sub(/\n{1}+/,"\n").sub(/\r{1}+/,"\r").sub(/^('|"|`|\s)+/, '').sub(/('|"|`|\s|,|;)+$/, '').sub(/\s{1}+/, ' ') : value
+      end
+
+      attr_config.default_normalizers = :text_line, :strip, :blank
+
+      attr_config.default_attributes = :name, :title, :abbrev, :authors, :pages, :vol, :num
+      attr_config.add_default_attribute :description, :with => :paragraph
+      attr_config.add_default_attribute :descr, :with => :paragraph
     end
   end
-
-  config.normalizers[:text_line] = lambda do |value, options|
-    value.is_a?(String) ? value.tr("\n",'').tr("\r",'').sub(/^('|"|`|\s)+/, '').sub(/('|"|`|\s|\.|,|;)+$/, '').sub(/\s{1}+/, ' ') : value
-  end
-
-  config.normalizers[:paragraph] = lambda do |value, options|
-    value.is_a?(String) ? value.sub(/\n{1}+/,"\n").sub(/\r{1}+/,"\r").sub(/^('|"|`|\s)+/, '').sub(/('|"|`|\s|,|;)+$/, '').sub(/\s{1}+/, ' ') : value
-  end
-
-  config.default_normalizers = :text_line, :strip, :blank
-
-  config.default_attributes = :name, :title, :abbrev, :authors, :pages, :vol, :num
-  config.add_default_attribute :description, :with => :paragraph
-  config.add_default_attribute :descr, :with => :paragraph
 end
--- config/initializers/salva.rb
+++ config/initializers/salva.rb
@@ -1,5 +1,9 @@
-%w(site_config routes meta_search_extension meta_date_extension meta_user_association).each do |file|
-  file_path = Rails.root.to_s + '/lib/salva/' + file
-  require file_path if File.exist? "#{file_path}.rb"
+Salva::Application.configure do
+  config.after_initialize do
+    %w(site_config routes meta_search_extension meta_date_extension meta_user_association).each do |file|
+      file_path = Rails.root.to_s + '/lib/salva/' + file
+      require file_path if File.exist? "#{file_path}.rb"
+    end
+    MetaWhere.operator_overload!
+  end
 end
-MetaWhere.operator_overload!
