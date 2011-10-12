1f97a2e406eeae4a1ab86b84b2c816ccae70f42c
Developer at IFUNAM <dev@fisica.unam.mx>
2011-10-12 11:31:37 -0500

    config/mail.yml file added for config/initializers/mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,13 @@
 if Rails.env.to_s != 'test'
   Salva::Application.configure do
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = { 
-      :enable_starttls_auto => false,
-      :address => 'fenix.fisica.unam.mx',
-      :port => 25,
-      :domain => 'fisica.unam.mx',
-      :location => '/usr/sbin/sendmail',
-      :arguments => '-i -t -f noreply@fisica.unam.mx'
-    }
+    conf_path = File.join(Rails.root.to_s, 'config', 'mail.yml')
+    if File.exists? conf_path
+      settings = YAML.load_file(conf_path)
+      settings.merge!(:arguments => "-i -t -f noreply@#{settings[:domain]}")
+      config.action_mailer.smtp_settings = settings
+    else
+      warn "You must configure #{conf_path}, copy the config/mail.yml.example file"
+    end
   end
 end
