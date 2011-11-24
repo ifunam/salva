456bb5ec4cd06d746fc8d8204d2c20c660c24469
Developer at IFUNAM <dev@fisica.unam.mx>
2011-11-24 17:27:16 -0600

    Refactored views for book_collaborations MVC. Closes #58

--- app/models/bookedition_roleinbook.rb
+++ app/models/bookedition_roleinbook.rb
@@ -2,6 +2,9 @@ class BookeditionRoleinbook < ActiveRecord::Base
   belongs_to :bookedition
   belongs_to :roleinbook
   belongs_to :user
+  belongs_to :registered_by, :class_name => 'User'
+  belongs_to :updated_by, :class_name => 'User'
+
   scope :authors, where("roleinbook_id = 1 OR roleinbook_id = 2")
   scope :collaborators, where("roleinbook_id != 1 AND roleinbook_id != 2")
 
