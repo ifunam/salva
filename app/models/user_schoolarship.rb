f82dd7480895fe556a00a6086703d62c9763f902
Developer at IFUNAM <dev@fisica.unam.mx>
2012-01-09 11:51:08 -0600

    Enforcing validation for amount in user_schoolarship model

--- app/models/user_schoolarship.rb
+++ app/models/user_schoolarship.rb
@@ -2,6 +2,7 @@ class UserSchoolarship < ActiveRecord::Base
   validates_presence_of :schoolarship_id
   validates_numericality_of :schoolarship_id, :allow_nil => false,  :greater_than => 0, :only_integer => true
   validates_numericality_of :id, :allow_nil => true,  :greater_than => 0, :only_integer => true
+  validates :amount, :numericality => { :allow_nil => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 400000 }
 
   belongs_to :schoolarship
   belongs_to :registered_by, :class_name => 'User'
