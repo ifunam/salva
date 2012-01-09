c14cbe2923573fce649a9a86c27de7e127dda506
Developer at IFUNAM <dev@fisica.unam.mx>
2012-01-09 12:17:27 -0600

    Fixing migration troubles with memberships table

--- db/migrate/20110111183022_add_registered_by_to_memberships.rb
+++ db/migrate/20110111183022_add_registered_by_to_memberships.rb
@@ -1,11 +1,24 @@
 class AddRegisteredByToMemberships < ActiveRecord::Migration
   def self.up
-    rename_column :memberships, :moduser_id, :registered_by_id
-    add_column    :memberships, :created_by_id, :integer
+    if column_exists? :memberships, :moduser_id
+      rename_column :memberships, :moduser_id, :registered_by_id
+    else
+      add_column    :memberships, :registered_by_id, :integer
+    end
+    add_column :memberships, :modified_by_id, :integer
   end
 
   def self.down
-    rename_column :memberships, :registered_by_id, :moduser_id
-    remove_column :memberships, :created_by_id
+    if column_exists? :memberships, :moduser_id
+      remove_column :memberships, :moduser_id
+    end
+
+    if column_exists? :memberships, :registered_by_id
+      rename_column :memberships, :registered_by_id, :moduser_id
+    end
+
+    if column_exists? :memberships, :modified_by_id
+      remove_column :memberships, :modified_by_id
+    end
   end
 end
