b6cb7722efd8d8f1462f43ae2e8b9905ea784c28
Developer at IFUNAM <dev@fisica.unam.mx>
2012-01-30 11:35:35 -0600

    Abbrev attribute added to the xml rendering in the actions api/adscriptions controller

--- app/controllers/api/adscriptions_controller.rb
+++ app/controllers/api/adscriptions_controller.rb
@@ -1,11 +1,11 @@
 class Api::AdscriptionsController < Api::BaseController
 
   def index
-    respond_with(@adscriptions = Adscription.enabled, :only => [:name, :id])
+    respond_with(@adscriptions = Adscription.enabled, :only => [:name, :abbrev, :id])
   end
 
   def show
-    respond_with(@adscription = Adscription.find(params[:id]), :only => [:name, :id])
+    respond_with(@adscription = Adscription.find(params[:id]), :only => [:name, :abbrev, :id])
   end
 
 end
