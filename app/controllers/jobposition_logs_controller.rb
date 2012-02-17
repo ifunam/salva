87fd442a7f5ec2b6034693f0a04f998944e8e003
Developer at IFUNAM <dev@fisica.unam.mx>
2012-02-16 18:50:36 -0600

    Adding new and create actions to jobposition_logs_controller. Closes #137

--- app/controllers/jobposition_logs_controller.rb
+++ app/controllers/jobposition_logs_controller.rb
@@ -1,11 +1,22 @@
 class JobpositionLogsController < ApplicationController
   layout 'user_resources'
+  def new
+    respond_with(@jobposition_log = JobpositionLog.new)
+  end
+
   def show
     respond_with(@jobposition_log = JobpositionLog.find_by_user_id(current_user.id))
   end
 
   def edit
     respond_with(@jobposition_log = JobpositionLog.find_by_user_id(current_user.id))
+  end
+
+  def create
+    @jobposition_log = JobpositionLog.new(params[:jobposition_log])
+    @jobposition_log.user_id = current_user.id
+    @jobposition_log.registered_by_id = current_user.id
+    respond_with(@jobposition_log.save, :status => :updated, :location => jobposition_log_path)
   end
 
   def update
