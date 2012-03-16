cc75470b93d0fc471e3f394274981f942b1c6efb
Developer at IFUNAM <dev@fisica.unam.mx>
2012-03-15 18:13:09 -0600

    Minor changes

--- db/migrate/20120223234813_add_homepage_resume_and_homepage_resume_en_to_users.rb
+++ db/migrate/20120223234813_add_homepage_resume_and_homepage_resume_en_to_users.rb
@@ -1,6 +1,11 @@
 class AddHomepageResumeAndHomepageResumeEnToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :homepage_resume, :text
-    add_column :users, :homepage_resume_en, :text
+    unless column_exists? :users, :homepage_resume
+      add_column :users, :homepage_resume, :text
+    end
+
+    unless column_exists? :users, :homepage_resume_en
+      add_column :users, :homepage_resume_en, :text
+    end
   end
 end
