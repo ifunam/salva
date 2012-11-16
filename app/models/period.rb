59963e7d90c2efddd6a4de98b75e09f36b4eed29
Developer at IFUNAM <dev@fisica.unam.mx>
2012-11-16 17:59:58 -0600

    Enabling accessible attributes in documenttype and period models

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,5 +1,6 @@
 class Period < ActiveRecord::Base
   validates_presence_of :title, :startdate, :enddate
+  attr_accessible :title, :startdate, :enddate
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_uniqueness_of :title, :scope => [:startdate, :enddate]
 
