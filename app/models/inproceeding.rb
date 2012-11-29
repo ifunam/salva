b754d2a0636c14ca4cb57bcafb7d9f47c1de0ea4
Developer at IFUNAM <dev@fisica.unam.mx>
2012-11-29 17:54:14 -0600

    ordered_by_year scope added to inproceeding model

--- app/models/inproceeding.rb
+++ app/models/inproceeding.rb
@@ -15,16 +15,18 @@ class Inproceeding < ActiveRecord::Base
 
   has_paper_trail
 
-  default_scope order("proceedings.year DESC, inproceedings.authors ASC, inproceedings.title ASC")
-  scope :refereed, joins("INNER JOIN proceedings ON proceedings.isrefereed = 't' AND inproceedings.proceeding_id = proceedings.id")
-  scope :unrefereed, joins("INNER JOIN proceedings ON proceedings.isrefereed = 'f' AND inproceedings.proceeding_id = proceedings.id")
+  default_scope order("inproceedings.authors ASC, inproceedings.title ASC")
+
+  scope :ordered_by_year, order("proceedings.year DESC, inproceedings.authors  ASC, inproceedings.title ASC")
+  scope :refereed, joins("INNER JOIN proceedings ON proceedings.isrefereed = 't' AND inproceedings.proceeding_id = proceedings.id").ordered_by_year
+  scope :unrefereed, joins("INNER JOIN proceedings ON proceedings.isrefereed = 'f' AND inproceedings.proceeding_id = proceedings.id").ordered_by_year
 
   scope :user_id_eq, lambda { |user_id| joins(:user_inproceedings).where(:user_inproceedings => {:user_id => user_id}) }
   scope :user_id_not_eq, lambda { |user_id|
       inproceeding_without_user_sql = UserInproceeding.select('DISTINCT(inproceeding_id) as inproceeding_id').where(["user_inproceedings.user_id != ?", user_id]).to_sql
       inproceeding_with_user_sql = UserInproceeding.select('DISTINCT(inproceeding_id) as inproceeding_id').where(["user_inproceedings.user_id = ?", user_id]).to_sql
       sql = "inproceedings.id IN (#{inproceeding_without_user_sql}) AND inproceedings.id NOT IN (#{inproceeding_with_user_sql})"
-      where(sql)
+      where sql
   }
 
   scope :since, lambda { |year| includes(:proceeding).where(["proceedings.year >= ?", year])}
