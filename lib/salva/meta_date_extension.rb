35878bb70f67c62511c61f872dc4b56f9e94f3e2
Developer at IFUNAM <dev@fisica.unam.mx>
2012-12-04 12:47:26 -0600

    Fixing wrong queries to build the annual report for the user

--- lib/salva/meta_date_extension.rb
+++ lib/salva/meta_date_extension.rb
@@ -47,9 +47,9 @@ module MetaDateExtension
          scope :among, lambda { |start_year, start_month, end_year, end_month|
           where{
             ({:startyear.gteq => start_year, :startmonth.gteq => start_month, :endyear.lteq => end_year, :endmonth.lteq => end_month}) |
-            ({:startyear.gteq => start_year, :startmonth.gteq => start_month, :endyear.gteq => start_year }) |
-            ({:startyear.gteq => start_year, :startmonth.gteq => start_month, :endyear => nil}) |
-            ({:startyear.lteq => end_year, :startmonth.gteq => start_month, :endyear.gteq => end_year, :endmonth.lteq => end_month})
+            ({:startyear.lt => start_year, :endyear.gt => start_year }) |
+            ({:startyear.lteq => start_year, :endyear => nil}) |
+            ({:startyear.lt => start_year, :endyear.lteq => end_year, :endyear.gteq => start_year})
           }
         }
         search_methods :among, :splat_param => true, :type => [:integer, :integer, :integer, :integer]
@@ -74,9 +74,9 @@ module MetaDateExtension
       unless respond_to? :among
         scope :among, lambda { |start_date, end_date|
           where{
-            ({:start_date.gteq => start_date} & {:end_date.gteq => start_date}) |
-            ({:start_date.gteq => start_date} & {:end_date.lteq => start_date}) |
-            ({:start_date.lteq => end_date} & {:end_date.gteq => end_date})
+            ({:start_date.gteq => start_date} & {:end_date.lteq => end_date}) |
+            ({:start_date.lt => start_date} & {:end_date.gt => end_date}) |
+            ({:start_date.lt => start_date} & {:end_date.lteq => end_date} & {:end_date.gteq => start_date})
           }
         }
         search_methods :among, :splat_param => true, :type => [:date, :date]
@@ -101,8 +101,9 @@ module MetaDateExtension
         scope :among, lambda { |start_year, end_year| 
           where{
             ({:startyear.gteq => start_year, :endyear.lteq => end_year}) |
-            ({:startyear.gteq => start_year, :endyear.gteq => start_year}) |
-            ({:startyear.gteq => start_year, :endyear => nil})
+            ({:startyear.lt => start_year, :endyear.gt => end_year}) |
+            ({:startyear.lt => start_year, :endyear.lteq => end_year, :endyear.gteq => start_year}) |
+            ({:startyear.lteq => start_year, :endyear => nil})
           }
         }
         search_methods :among, :splat_param => true, :type => [:integer, :integer]
