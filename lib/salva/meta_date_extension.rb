f88e0a8f127181643c71ef65266fa61164df72e6
Developer at IFUNAM <dev@fisica.unam.mx>
2012-12-05 13:52:39 -0600

    Missed queries added for the annual report

--- lib/salva/meta_date_extension.rb
+++ lib/salva/meta_date_extension.rb
@@ -78,7 +78,10 @@ module MetaDateExtension
             ({:start_date.lteq => start_date, :end_date.lteq => end_date, :end_date.gteq => start_date}) |
             ({:start_date.gteq => start_date, :end_date.gteq => end_date, :start_date.lteq => end_date})
             ({:start_date.gteq => start_date, :end_date => nil, :start_date.lteq => end_date}) |
-            ({:end_date.lteq => end_date, :start_date => nil, :end_date.gteq => start_date})
+            ({:end_date.lteq => end_date, :start_date => nil, :end_date.gteq => start_date}) |
+            ({:start_date.gteq => start_date, :end_date.lteq => end_date, :start_date.lt => end_date}) |
+            ({:end_date.lteq => end_date, :end_date.gteq => start_date, :start_date.lt => end_date}) 
+
           }
         }
         search_methods :among, :splat_param => true, :type => [:date, :date]
