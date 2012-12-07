ef7d39696196b6917f3a292c9e97f6052086151a
Ramón Martínez Olvera <ramonmtzol@gmail.com>
2012-12-07 09:59:45 -0600

    Adding scope to as_non_author to phd, mastery, degree, bachelor and technician theses scopes

--- app/models/thesis.rb
+++ app/models/thesis.rb
@@ -38,11 +38,6 @@ class Thesis < ActiveRecord::Base
   }
 
   scope :roleinthesis_id_eq, lambda { |roleinthesis_id| joins(:user_theses).where(:user_theses => { :roleinthesis_id => roleinthesis_id }) }
-  scope :phd_theses, joins(:career).where('degree_id = 6')
-  scope :mastery_theses, joins(:career).where('degree_id = 5 or degree_id = 4')
-  scope :degree_theses, joins(:career).where('degree_id = 3')
-  scope :technician_theses, joins(:career).where('degree_id = 2')
-  scope :bachelor_theses, joins(:career).where('degree_id = 1')
 
   scope :as_author, where(:user_theses => {:roleinthesis_id => 1})
   scope :roleinthesis_id_not_eq, lambda { |roleinthesis_id| joins(:user_theses).where{{:user_theses => { :roleinthesis_id.not_eq => roleinthesis_id }}} }
@@ -50,6 +45,12 @@ class Thesis < ActiveRecord::Base
   scope :as_non_author, roleinthesis_id_not_eq(1)
   scope :finished, where(:thesisstatus_id => 3).as_non_author
   scope :inprogress, where("thesisstatus_id != 3").as_non_author
+
+  scope :phd_theses, joins(:career).where('degree_id = 6').as_non_author
+  scope :mastery_theses, joins(:career).where('degree_id = 5 or degree_id = 4').as_non_author
+  scope :degree_theses, joins(:career).where('degree_id = 3').as_non_author
+  scope :technician_theses, joins(:career).where('degree_id = 2').as_non_author
+  scope :bachelor_theses, joins(:career).where('degree_id = 1').as_non_author
 
   search_methods :user_id_eq, :user_id_not_eq, :roleinthesis_id_eq
 
