6031108b9f3f35116a424a8ace470d500cedae0c
Ramón Martínez Olvera <ramonmtzol@gmail.com>
2012-12-07 17:56:36 -0600

    Refactoring for date_range_scoped method in meta_date_extension. It works for me, but it is necesary to review with Alejando Juarez

--- lib/salva/meta_date_extension.rb
+++ lib/salva/meta_date_extension.rb
@@ -73,14 +73,17 @@ module MetaDateExtension
       unless respond_to? :among
         scope :among, lambda { |start_date, end_date|
           where{
-            ({:start_date.gteq => start_date, :end_date.lteq => end_date}) |
-            ({:start_date.lt => start_date, :end_date.gt => end_date}) |
-            ({:start_date.lteq => start_date, :end_date.lteq => end_date, :end_date.gteq => start_date}) |
-            ({:start_date.gteq => start_date, :end_date.gteq => end_date, :start_date.lteq => end_date})
-            ({:start_date.gteq => start_date, :end_date => nil, :start_date.lteq => end_date}) |
-            ({:end_date.lteq => end_date, :start_date => nil, :end_date.gteq => start_date}) |
-            ({:start_date.gteq => start_date, :end_date.lteq => end_date, :start_date.lt => end_date}) |
-            ({:end_date.lteq => end_date, :end_date.gteq => start_date, :start_date.lt => end_date}) 
+            ({:start_date.gteq => start_date, :start_date.lteq => end_date }) |
+            ({:end_date.gteq => start_date, :end_date.lteq => end_date }) |
+            ({:start_date.lt => start_date, :end_date.gt => end_date } | {:end_date => nil})
+            #({:start_date.gteq => start_date, :end_date.lteq => end_date}) |             da
+            #({:start_date.lt => start_date, :end_date.gt => end_date}) |
+            #({:start_date.lteq => start_date, :end_date.lteq => end_date, :end_date.gteq => start_date}) |
+            #({:start_date.gteq => start_date, :end_date.gteq => end_date, :start_date.lteq => end_date})
+            #({:start_date.gteq => start_date, :end_date => nil, :start_date.lteq => end_date}) |
+            #({:end_date.lteq => end_date, :start_date => nil, :end_date.gteq => start_date}) |
+            #({:start_date.gteq => start_date, :end_date.lteq => end_date, :start_date.lt => end_date}) |
+            #({:end_date.lteq => end_date, :end_date.gteq => start_date, :start_date.lt => end_date})
 
           }
         }
