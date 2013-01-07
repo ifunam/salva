24a8bde30416cc99837df669a7fbe532ba2e65ca
Developer at IFUNAM <dev@fisica.unam.mx>
2013-01-07 11:19:00 -0600

    Upgrading to rails 3.2.10 and forcing to the salva users to define their own secret token in the config/site.yml file to secure the rails session

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Salva::Application.config.secret_token = '52d737e54b7c19af1c06c7101e405f842645b5c9446acd9cfa70bbdebf6aada443bb35460a5af20e3f93d567e59999fd6bd1d78ccc865b961c84318b4ebbf308'
+Salva::Application.config.secret_token = Salva::SiteConfig.app('secret_token')
