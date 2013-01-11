5907144c3deef9c41a1daa19824ba0e0e1c50241
Ramón Martínez Olvera <ramonmtzol@gmail.com>
2013-01-11 08:15:33 -0600

    Adding not_as_author scope to defaults of controller

--- app/controllers/theses_controller.rb
+++ app/controllers/theses_controller.rb
@@ -1,5 +1,5 @@
 class ThesesController < PublicationController
-  defaults :resource_class => Thesis, :collection_name => 'theses', :instance_name => 'thesis',
+  defaults :resource_class => Thesis.not_as_author, :collection_name => 'theses', :instance_name => 'thesis',
            :user_role_class => :user_theses, :role_class => :roleintheses
 
   def update
