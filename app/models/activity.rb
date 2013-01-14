9640d504221bcf89c7d14db15619d1748200dcbb
Developer at IFUNAM <dev@fisica.unam.mx>
2013-01-14 10:20:04 -0600

    The valitation rules to verify uniqueness of record was updated in the Activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,7 @@ class Activity < ActiveRecord::Base
   validates_presence_of :name, :activitytype_id, :year
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_numericality_of :user_id, :activitytype_id, :greater_than => 0, :only_integer => true
-  validates_uniqueness_of :name, :scope => [:user_id, :month, :year]
+  validates_uniqueness_of :name, :scope => [:user_id, :month, :year, :descr]
 
   belongs_to :activitytype
   belongs_to :user
