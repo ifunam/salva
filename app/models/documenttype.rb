3d04c29bf5bae0b67e2faaf6397156d818d4890d
Developer at IFUNAM <dev@fisica.unam.mx>
2013-05-22 15:59:22 -0500

    Fixing minor bugs related to the database migrations

--- app/models/documenttype.rb
+++ app/models/documenttype.rb
@@ -7,10 +7,12 @@ class Documenttype < ActiveRecord::Base
   default_scope :order => 'start_date DESC, end_date DESC'
   scope :annual_reports, where("name LIKE 'Informe anual de actividades%'")
   scope :annual_plans, where("name LIKE 'Plan de trabajo%'")
-  scope :active, where(:status => true)
+  scope :active, where(:status => true) if column_names.include? 'status'
   has_many :documents
 
   def self.year_for_annual_report
-   Documenttype.annual_reports.active.first.nil? ? Date.today.year : Documenttype.annual_reports.active.first.year
+    if column_names.include? 'status'
+      Documenttype.annual_reports.active.first.nil? ? Date.today.year : Documenttype.annual_reports.active.first.year
+    end
   end
 end
--- db/migrate/20101212175712_insert_records_to_documenttypes.rb
+++ db/migrate/20101212175712_insert_records_to_documenttypes.rb
@@ -1,12 +1,12 @@
 class InsertRecordsToDocumenttypes < ActiveRecord::Migration
   def self.up
 
-    @dt1 = Documenttype.new(:name => 'Informe anual de actividades - 2010', :year => 2010, :status => true,
-                          :start_date => '2010-12-13', :end_date => '2011-01-14')
+    @dt1 = Documenttype.new(:name => 'Informe anual de actividades - 2010', :year => 2010,
+                           :start_date => '2010-12-13', :end_date => '2011-01-14')
     @dt1.save
 
-    @dt2 = Documenttype.new(:name => 'Plan de trabajo - 2010', :year => 2010, :status => true,
-                        :start_date => '2010-12-13', :end_date => '2011-01-14')                        
+    @dt2 = Documenttype.new(:name => 'Plan de trabajo - 2010', :year => 2010,
+                            :start_date => '2010-12-13', :end_date => '2011-01-14')
     @dt2.save
   end
 
