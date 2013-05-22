5b174dfff2d3bcc7faa990c4951434eea7d8aeab
Developer at IFUNAM <dev@fisica.unam.mx>
2013-05-22 11:10:19 -0500

    enabling writting for name attributes in several models

--- app/models/document_type.rb
+++ app/models/document_type.rb
@@ -1,5 +1,6 @@
 class DocumentType < ActiveRecord::Base
   validates_presence_of :name
+  attr_accessible :name
   validates_uniqueness_of :name
   has_many :documents
   default_scope :order => 'name ASC'
