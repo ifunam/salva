d895bdb4825e04cc6b5e8e6d78e2f06fecd81c37
Developer at IFUNAM <dev@fisica.unam.mx>
2013-05-22 15:40:09 -0500

    Minor change

--- db/migrate/20121109221333_seed_thesismodalities.rb
+++ db/migrate/20121109221333_seed_thesismodalities.rb
@@ -2,20 +2,25 @@
 class SeedThesismodalities < ActiveRecord::Migration
   def up
     @degree = Degree.find_by_name('Licenciatura')
-
-    Thesismodality.all.each do |record|
-      record.update_attribute(:degree_id, @degree.id)
+    unless @degree.nil?
+      Thesismodality.all.each do |record|
+        record.update_attribute(:degree_id, @degree.id)
+      end
+      Thesismodality.create(:name => 'Otra modalidad', :degree_id => @degree.id)
     end
-    Thesismodality.create(:name => 'Otra modalidad', :degree_id => @degree.id)
 
-    @degree = Degree.find_by_name('Maestría')
-    ['Tesis', 'Por artículo de investigación', 'Por proyecto de investigación', 'Otra modalidad'].each do |name|
-      Thesismodality.create(:name => name, :degree_id => 5)
+    unless @degree.nil?
+      @degree = Degree.find_by_name('Maestría')
+      ['Tesis', 'Por artículo de investigación', 'Por proyecto de investigación', 'Otra modalidad'].each do |name|
+        Thesismodality.create(:name => name, :degree_id => 5)
+      end
     end
 
     @degree = Degree.find_by_name('Doctorado')
-    ['Obtención de grado', 'Candidatura a grado de doctor', 'Otra modalidad'].each do |name|
-      Thesismodality.create(:name => name, :degree_id => @degree.id)
+    unless @degree.nil?
+      ['Obtención de grado', 'Candidatura a grado de doctor', 'Otra modalidad'].each do |name|
+        Thesismodality.create(:name => name, :degree_id => @degree.id)
+      end
     end
 
   end
