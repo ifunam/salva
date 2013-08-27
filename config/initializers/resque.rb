c33e4f8cd42ad46fd78d8a8137b29f95393062bd
Developer at IFUNAM <dev@fisica.unam.mx>
2013-08-27 18:09:07 -0500

    Removing Resque::Status from the Resque initializer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -7,7 +7,6 @@ Salva::Application.configure do
      if File.exist? conf_path
        resque_config = YAML.load_file(conf_path)
        Resque.redis = resque_config[Rails.env.to_s]
-       Resque::Status.expire_in = (24 * 60 * 60) # 24hrs in seconds
       end
    end
 end
