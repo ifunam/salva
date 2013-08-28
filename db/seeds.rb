f97c02a178f1e965297f9f201efbc73925a1141a
Developer at IFUNAM <dev@fisica.unam.mx>
2013-08-28 15:59:28 -0500

    Minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,15 @@ data_dir = Rails.root.join('db', 'data')
 [Userstatus, Group, Articlestatus, Bookchaptertype, Booktype, Citizenmodality, Conferencescope, Conferencetype,
  Contracttype, Courseduration, Coursegrouptype, Credittype, DocumentType, Editionstatus, Genericworkstatus,
  Groupmodality, Idtype, Migratorystatus].each do |class_name|
-   class_name.destroy_all
    table_name = ActiveSupport::Inflector.tableize(class_name)
    puts "Inserting data into the table: #{table_name}..."
-   CSV.foreach(File.join(data_dir, "#{table_name}.csv"), :headers => false) do |row|
-     class_name.create name: row[0].to_s.strip
+   CSV.foreach(File.join(data_dir, "#{table_name}.csv"), headers: false) do |row|
+     class_name.where(name: row[0].to_s.strip).first_or_create
    end
 end
 
-Degree.destroy_all
 puts "Inserting data into the table: degrees..."
-CSV.foreach(File.join(data_dir, "degrees.csv"), :headers => false) do |row|
-  Degree.create name: row[0].to_s.strip, level: row[1].to_i
+CSV.foreach(File.join(data_dir, "degrees.csv"), headers: false) do |row|
+  Degree.where(name: row[0].to_s.strip, level: row[1].to_i).first_or_create
 end
 
