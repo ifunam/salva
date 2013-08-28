de9365b11c771412b22b195910c0da7d39d3d47f
Developer at IFUNAM <dev@fisica.unam.mx>
2013-08-28 13:14:28 -0500

    The Group table and its data was added to the db/seeds.rb script

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@
 require 'csv'
 data_dir = Rails.root.join('db', 'data')
 
-Userstatus.destroy_all
-CSV.foreach("#{data_dir}/userstatuses.csv", :headers => false) do |row|
-  Userstatus.create name: row[0].to_s.strip
+[Userstatus, Group].each do |class_name|
+  class_name.destroy_all
+  CSV.foreach(File.join(data_dir, "#{ActiveSupport::Inflector.tableize(class_name)}.csv"), :headers => false) do |row|
+    class_name.create name: row[0].to_s.strip
+  end
 end
 
