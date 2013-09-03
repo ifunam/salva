64d823f64d367d78250075ffbcd0700c7f91fdc7
Developer at IFUNAM <dev@fisica.unam.mx>
2013-09-03 16:35:27 -0500

    The chzn-select plugin for jquery was added into the ActiveAdmin assets

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -16,12 +16,12 @@ ActiveAdmin.register User do
 
   filter :fullname, :as => :string
   filter :login
-  filter :adscription, :collection => proc { Adscription.enabled.all }, :as => :select
-  filter :jobpositioncategory, :collection => proc { Jobpositioncategory.for_researching }, :as => :select
-  filter :schoolarship, :collection => proc { Schoolarship.posdoctoral_scholar.collect {|record|[ record.to_s,  record.id]} }, :as => :select
-  filter :userstatus
-  filter :jobposition_start_date_year, :collection => (Date.today.year - 100 .. Date.today.year + 1).to_a.reverse, :as => :select
-  filter :jobposition_end_date_year, :collection => (Date.today.year - 100 .. Date.today.year + 1).to_a.reverse, :as => :select
+  filter :adscription, :collection => proc { Adscription.enabled.all }, :as => :select, :input_html => { :class => "chosen-select" } 
+  filter :jobpositioncategory, :collection => proc { Jobpositioncategory.for_researching }, :as => :select, :input_html => { :class => "chosen-select" } 
+  filter :schoolarship, :collection => proc { Schoolarship.posdoctoral_scholar.collect {|record|[ record.to_s,  record.id]} }, :as => :select, :input_html => { :class => "chosen-select" } 
+  filter :userstatus, :input_html => { :class => "chosen-select" } 
+  filter :jobposition_start_date_year, :collection => (Date.today.year - 100 .. Date.today.year + 1).to_a.reverse, :as => :select, :input_html => { :class => "chosen-select" } 
+  filter :jobposition_end_date_year, :collection => (Date.today.year - 100 .. Date.today.year + 1).to_a.reverse, :as => :select, :input_html => { :class => "chosen-select" } 
 
 
   form do |f|                         
@@ -31,7 +31,7 @@ ActiveAdmin.register User do
       f.input :userstatus, :as => :radio
       f.input :password, :as => :password
       f.input :password_confirmation, :as => :password
-      f.input :user_incharge, :collection => User.activated.collect {|record| [record.to_s, record.id] }, :as => :select
+      f.input :user_incharge, :collection => User.activated.collect {|record| [record.to_s, record.id] }, :as => :select, :input_html => {:class => "chosen-select"}
       f.inputs "Group", :for => [:user_group, f.object.user_group || UserGroup.new] do |ug_form|
         ug_form.input :group, :as => :radio
       end
@@ -44,10 +44,10 @@ ActiveAdmin.register User do
         p_form.input :title_en, :as => :string
         p_form.input :dateofbirth
         p_form.input :gender
-        p_form.input :maritalstatus, :as => :select
-        p_form.input :country, :as => :select
-        p_form.input :state, :as => :select
-        p_form.input :city, :as => :select
+        p_form.input :maritalstatus, :as => :select, :input_html => { :class => "chosen-select" }
+        p_form.input :country, :as => :select, :input_html => { :class => "chosen-select" }
+        p_form.input :state, :as => :select, :input_html => { :class => "chosen-select" }
+        p_form.input :city, :as => :select, :input_html => { :class => "chosen-select" }
       end
 
       f.inputs "Professional address", :for => [:address, f.object.address || Address.new] do |a_form|
@@ -59,26 +59,26 @@ ActiveAdmin.register User do
       
       f.object.user_identifications << UserIdentification.new if f.object.new_record?
       f.has_many :user_identifications do |item|
-        item.input :idtype, :as => :select
+        item.input :idtype, :as => :select, :input_html => { :class => "chosen-select" }
         item.input :descr, :as => :string
       end
 
       f.inputs "Jobposition", :for => [:jobposition, f.object.jobposition || Jobposition.new] do |p_form|
-        p_form.input :jobpositioncategory, :collection => Jobpositioncategory.for_researching,  :as => :select
-        p_form.input :contracttype
+        p_form.input :jobpositioncategory, :collection => Jobpositioncategory.for_researching,  :as => :select, :input_html => { :class => "chosen-select" } 
+        p_form.input :contracttype, :input_html => { :class => "chosen-select" } 
         p_form.input :institution
         p_form.input :start_date
         p_form.input :end_date
       end
 
       f.has_many :user_schoolarships do |item|
-        item.input :schoolarship, :collection => Schoolarship.posdoctoral_scholar.collect {|record|[ record.to_s,  record.id]}, :as => :select
+        item.input :schoolarship, :collection => Schoolarship.posdoctoral_scholar.collect {|record|[ record.to_s,  record.id]}, :as => :select, :input_html => { :class => "chosen-select" } 
         item.input :start_date
         item.input :end_date
       end
 
       f.has_many :documents do |item|
-        item.input :document_type, :collection => DocumentType.all, :as => :select
+        item.input :document_type, :collection => DocumentType.all, :as => :select, :input_html => { :class => "chosen-select" } 
         item.input :file
       end
 
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,6 @@
 //= require active_admin/base
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require chosen.jquery.min
+//= require document
