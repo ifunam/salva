380df145b5629b4e685e83ed24561015d5cc01ca
Developer at IFUNAM <dev@fisica.unam.mx>
2013-09-04 12:10:50 -0500

    Improvements for the default and the for_researching scopes of the jobpositioncategory model

--- app/models/jobpositioncategory.rb
+++ app/models/jobpositioncategory.rb
@@ -15,9 +15,9 @@ class Jobpositioncategory < ActiveRecord::Base
   validates_associated :jobpositionlevel
 
 
- # scope :find_by_jobpositiontype_id,  where("jobpositioncategories.jobpositiontype_id = 1").order('roleinjobpositions.name ASC, jobpositionlevels.name ASC')
- default_scope joins(:roleinjobposition, :jobpositionlevel).order('roleinjobpositions.name ASC, jobpositionlevels.name ASC')
- scope :for_researching, where("jobpositioncategories.jobpositiontype_id = 1")
+  # scope :find_by_jobpositiontype_id,  where("jobpositioncategories.jobpositiontype_id = 1").order('roleinjobpositions.name ASC, jobpositionlevels.name ASC')
+  default_scope includes(:roleinjobposition, :jobpositionlevel).order('roleinjobpositions.name ASC, jobpositionlevels.name ASC')
+  scope :for_researching, where("jobpositioncategories.jobpositiontype_id = 1 OR jobpositioncategories.roleinjobposition_id = 38 OR jobpositioncategories.roleinjobposition_id = 110 OR jobpositioncategories.roleinjobposition_id = 112")
   def name
     unless self.new_record?
       [(roleinjobposition.nil? ? nil : roleinjobposition.name), (self.jobpositionlevel_id.nil? ? nil : jobpositionlevel.name) ].compact.join(' ')
