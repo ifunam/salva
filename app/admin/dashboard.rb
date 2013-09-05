4f15cd53ccfcdee553321923ff28ca714b09b79f
Developer at IFUNAM <dev@fisica.unam.mx>
2013-09-05 12:49:25 -0500

    Initial dashboard implementation for Admin::User was implemented

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,12 +4,24 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
     end
 
+    columns do
+      column do
+        panel "Usuarios recientes" do 
+          ul do
+            User.order("created_on DESC").limit(5).all.map do |user|
+              li link_to(user.to_s, admin_user_path(user))
+            end
+          end
+        end
+      end
+      column do
+        panel "Info" do
+          para "Welcome"
+        end
+      end
+    end
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
@@ -18,7 +30,7 @@ ActiveAdmin.register_page "Dashboard" do
     #       ul do
     #         Post.recent(5).map do |post|
     #           li link_to(post.title, admin_post_path(post))
-    #         end
+    #          end
     #       end
     #     end
     #   end
