fe2e3d23c219126c2ce938118a185e8e9f3d817f
Developer at IFUNAM <dev@fisica.unam.mx>
2013-09-05 14:30:50 -0500

    The MVC for Admin::Documenttype was added

--- app/models/documenttype.rb
+++ app/models/documenttype.rb
@@ -2,7 +2,7 @@ class Documenttype < ActiveRecord::Base
   validates_presence_of :name
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_uniqueness_of :name
-  attr_accessible :name, :start_date, :end_date, :status, :year
+  attr_accessible :name, :start_date, :end_date, :status, :year, :descr
 
   default_scope :order => 'start_date DESC, end_date DESC'
   scope :annual_reports, where("name LIKE 'Informe anual de actividades%'")
