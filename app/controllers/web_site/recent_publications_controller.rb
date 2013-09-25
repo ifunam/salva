10a8f89ed325ac2f4858cbadcf8e481793614ebd
Developer at IFUNAM <dev@fisica.unam.mx>
2013-09-25 10:26:59 -0500

    The recent publication list was restricted to include the records of the current year

--- app/controllers/web_site/recent_publications_controller.rb
+++ app/controllers/web_site/recent_publications_controller.rb
@@ -5,7 +5,7 @@ class WebSite::RecentPublicationsController < WebSite::ApplicationController
 
   def index
     @collections = [
-                    { :title => :published_articles, :collection => Article.published.verified.recent }
+                    { :title => :published_articles, :collection => Article.where(:year => Date.today.year).published.verified }
                     ]
     respond_with @collections
   end
