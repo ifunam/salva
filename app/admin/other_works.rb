df29c07b9507e8806bc96e3db8ff8e4fe2d26047
Developer at IFUNAM <dev@fisica.unam.mx>
2015-04-23 18:05:03 -0500

    The MVC Admin::OtherWorks and Admin::PopularScienceWorks were added

---
+++ app/admin/other_works.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+ActiveAdmin.register UserGenericwork, :as => "OtherWork"  do
+  menu :parent => 'Reportes', :label => 'Otros trabajos'
+
+  controller do
+    def scoped_collection
+      UserGenericwork.other_works
+    end
+  end
+
+  index :title => 'Otros trabajos' do
+    column :id
+    column(:title) { |record| record.genericwork.title }
+    column(:authors) { |record| record.genericwork.authors }
+    column(:institution)  { |record| record.genericwork.institution_name }
+    column(:country)  { |record| record.genericwork.institution_country }
+    column(:fullname) { |record| record.user.fullname_or_email }
+    column(:worker_key) { |record| record.user.worker_key_or_login }
+    column :userrole
+    column(:year) { |record| record.genericwork.year}
+    column(:month) { |record| record.genericwork.month }
+    column(:genericworktype) { |record| record.genericwork.genericworktype.name }
+    column(:genericworkstatus) { |record| record.genericwork.genericworkstatus.name }
+  end
+
+  filter :user, :label => 'Académico'
+  filter :userrole, :label => 'Rol'
+  filter :year, :collection => (Date.today.year - 100 .. Date.today.year + 1).to_a.reverse, :as => :select
+  filter :genericworkstatus_id, :collection => proc { Genericworkstatus.all }, :label => 'Status', :as => :select
+  filter :genericworktype_id, :collection => proc { Genericworktype.other_works }, :label => 'Tipo', :as => :select
+
+  csv do
+    column(:title) { |record| record.genericwork.title }
+    column(:authors) { |record| record.genericwork.authors }
+    column(:institution)  { |record| record.genericwork.institution_name }
+    column(:country)  { |record| record.genericwork.institution_country }
+    column(:fullname) { |record| record.user.fullname_or_email }
+    column(:worker_key) { |record| record.user.worker_key_or_login }
+    column(:userrole) { |record| record.userrole.name }
+    column(:year) { |record| record.genericwork.year}
+    column(:month) { |record| record.genericwork.month }
+    column(:genericworktype) { |record| record.genericwork.genericworktype.name }
+    column(:genericworkstatus) { |record| record.genericwork.genericworkstatus.name }
+  end
+end
---
+++ app/admin/popular_science_works.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+ActiveAdmin.register UserGenericwork, :as => "PopularScienceWork"  do
+  menu :parent => 'Reportes', :label => 'Trabajos de divulgación'
+
+  controller do
+    def scoped_collection
+      UserGenericwork.popular_science
+    end
+  end
+
+  index :title => 'Trabajos de divulgación' do
+    column :id
+    column(:title) { |record| record.genericwork.title }
+    column(:authors) { |record| record.genericwork.authors }
+    column(:institution)  { |record| record.genericwork.institution_name }
+    column(:country)  { |record| record.genericwork.institution_country }
+    column(:publisher) { |record| record.genericwork.publisher_name }
+    column(:fullname) { |record| record.user.fullname_or_email }
+    column(:worker_key) { |record| record.user.worker_key_or_login }
+    column :userrole
+    column(:year) { |record| record.genericwork.year}
+    column(:month) { |record| record.genericwork.month }
+    column(:vol) { |record| record.genericwork.vol }
+    column(:pages) { |record| record.genericwork.pages }
+    column(:genericworktype) { |record| record.genericwork.genericworktype.name }
+    column(:genericworkstatus) { |record| record.genericwork.genericworkstatus.name }
+  end
+
+  filter :user, :label => 'Académico'
+  filter :userrole, :label => 'Rol'
+  filter :year, :collection => (Date.today.year - 100 .. Date.today.year + 1).to_a.reverse, :as => :select
+  filter :genericworkstatus_id, :collection => proc { Genericworkstatus.all }, :label => 'Status', :as => :select
+  filter :genericworktype_id, :collection => proc { Genericworktype.popular_science }, :label => 'Tipo', :as => :select
+
+  csv do
+    column(:title) { |record| record.genericwork.title }
+    column(:authors) { |record| record.genericwork.authors }
+    column(:institution)  { |record| record.genericwork.institution_name }
+    column(:country)  { |record| record.genericwork.institution_country }
+    column(:publisher) { |record| record.genericwork.publisher_name }
+    column(:fullname) { |record| record.user.fullname_or_email }
+    column(:worker_key) { |record| record.user.worker_key_or_login }
+    column(:userrole) { |record| record.userrole.name }
+    column(:year) { |record| record.genericwork.year}
+    column(:month) { |record| record.genericwork.month }
+    column(:vol) { |record| record.genericwork.vol }
+    column(:pages) { |record| record.genericwork.pages }
+    column(:genericworktype) { |record| record.genericwork.genericworktype.name }
+    column(:genericworkstatus) { |record| record.genericwork.genericworkstatus.name }
+  end
+end
