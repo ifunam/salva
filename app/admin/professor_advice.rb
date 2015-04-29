c4e8d1a45bf6fe48cdb1927c1e1497d19b6f2697
Developer at IFUNAM <dev@fisica.unam.mx>
2015-04-28 19:04:35 -0500

    Admin::ProfessorAdvice MVC added

---
+++ app/admin/professor_advice.rb
@@ -0,0 +1,56 @@
+# encoding: utf-8
+ActiveAdmin.register Indivadvice, :as => 'ProfessorAdvice' do
+  menu :parent => 'Vinculación', :label => 'Asesoría de personal académico'
+
+  controller do
+    def scoped_collection
+      Indivadvice.professors
+    end
+  end
+
+  controller do
+    def index
+      if params.has_key? :q and params[:q].has_key? :among and !params[:q][:among].is_a? Array
+        params[:q][:among] = params[:q][:among].split(',').collect(&:to_i)
+      end
+      index!
+    end
+  end
+
+  index :title => 'Asesoría de personal académico' do
+    column :id
+
+    column :indivname
+    column :hours
+    column :startyear
+    column :startmonth
+    column :endyear
+    column :endmonth
+    column(:institution) { |record| record.institution.name unless record.career_id.nil? }
+    column(:country) { |record| record.institution.country_name unless record.career_id.nil? }
+
+    column(:fullname) { |record| record.user.fullname_or_email }
+    column(:adscription) { |record| record.user.adscription_name }
+    column(:worker_key) { |record| record.user.worker_key_or_login }
+  end
+
+  filter :indivname
+  filter :user, :label => 'Académico'
+  filter :among, :collection => (Date.today.year - 100 .. Date.today.year + 1).to_a.reverse.collect {|y| [y, [y,1, y, 12].join(',') ]}, :label => 'Año', :as => :select
+  filter :adscription_id, :collection => proc { Adscription.enabled }, :label => 'Adscripción', :as => :select
+
+  csv do
+    column :indivname
+    column :hours
+    column :startyear
+    column :startmonth
+    column :endyear
+    column :endmonth
+    column(:institution) { |record| record.institution.name unless record.career_id.nil? }
+    column(:country) { |record| record.institution.country_name unless record.career_id.nil? }
+
+    column(:fullname) { |record| record.user.fullname_or_email }
+    column(:adscription) { |record| record.user.adscription_name }
+    column(:worker_key) { |record| record.user.worker_key_or_login }
+  end
+end
--- app/admin/user_outreach_works.rb
+++ app/admin/user_outreach_works.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 ActiveAdmin.register UserGenericwork, :as => "UserOutreachWork" do
-  menu :parent => 'Reportes', :label => 'Trabajos de vinculación'
+  menu :parent => 'Vinculación', :label => 'Trabajos de vinculación'
   controller do
     def scoped_collection
       UserGenericwork.outreach_works
